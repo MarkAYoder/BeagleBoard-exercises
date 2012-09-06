3332c8a6f31eb90b3a409adf4b7e2ca9d81b85c5
Mark A. Yoder <yoder@yoder-dell.(none)>
2012-09-06 10:01:12 -0400

    Added git clone and cleaned things up a bit

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
+#!/usr/bash
 # Here are the extra things I install
 # --Mark
 # 6-Jul-2012
+cd ~
 opkg update
 opkg install openssh-keygen
-cp .bashrc ~
-cd
+git clone git@github.com:MarkAYoder/BeagleBoard-exercises.git exercises
+cp exercises/.bashrc .
 ln -s /var/lib/cloud9 .
-cd /var/lib/cloud9
+cd exercises/pinMux
 ln -s $PWD/pinMux.html /var/lib/cloud9/bone101
 cd cloud9
 git pull
