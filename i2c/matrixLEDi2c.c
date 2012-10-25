26fce2ca19c634abda30409b76af31e5d241689a
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2012-10-25 21:20:55 +0000

    More cleaning

--- i2c/matrixLEDi2c.c
+++ i2c/matrixLEDi2c.c
@@ -54,42 +54,10 @@ static int check_funcs(int file, int size)
 		return -1;
 	}
 
-	switch (size) {
-	case I2C_SMBUS_BYTE:
-		if (!(funcs & I2C_FUNC_SMBUS_WRITE_BYTE)) {
-			fprintf(stderr, MISSING_FUNC_FMT, "SMBus send byte");
-			return -1;
-		}
-		break;
-
-	case I2C_SMBUS_BYTE_DATA:
-		if (!(funcs & I2C_FUNC_SMBUS_WRITE_BYTE_DATA)) {
-			fprintf(stderr, MISSING_FUNC_FMT, "SMBus write byte");
-			return -1;
-		}
-		break;
-
-	case I2C_SMBUS_WORD_DATA:
-		if (!(funcs & I2C_FUNC_SMBUS_WRITE_WORD_DATA)) {
-			fprintf(stderr, MISSING_FUNC_FMT, "SMBus write word");
-			return -1;
-		}
-		break;
-
-	case I2C_SMBUS_BLOCK_DATA:
-		if (!(funcs & I2C_FUNC_SMBUS_WRITE_BLOCK_DATA)) {
-			fprintf(stderr, MISSING_FUNC_FMT, "SMBus block write");
-			return -1;
-		}
-		break;
-	case I2C_SMBUS_I2C_BLOCK_DATA:
-		if (!(funcs & I2C_FUNC_SMBUS_WRITE_I2C_BLOCK)) {
-			fprintf(stderr, MISSING_FUNC_FMT, "I2C block write");
-			return -1;
-		}
-		break;
+	if (!(funcs & I2C_FUNC_SMBUS_WRITE_BYTE)) {
+		fprintf(stderr, MISSING_FUNC_FMT, "SMBus send byte");
+		return -1;
 	}
-
 	return 0;
 }
 
@@ -127,96 +95,77 @@ int main(int argc, char *argv[])
 	 || set_slave_addr(file, address, force))
 		exit(1);
 
+	// Start oscillator (page 10)
+	res = i2c_smbus_write_byte(file, 0x21);
 
-	switch (size) {
-	case I2C_SMBUS_BYTE:
-		daddress = 0x21;	// Start oscillator (page 10)
-		printf("writing: 0x%02x\n", daddress);
-		res = i2c_smbus_write_byte(file, daddress);
+	// Display on, blinking off (page 11)
+	res = i2c_smbus_write_byte(file, 0x81);
 
-		daddress = 0x81;	// Display on, blinking off (page 11)
-		printf("writing: 0x%02x\n", daddress);
-		res = i2c_smbus_write_byte(file, daddress);
+	// Full brightness (page 15)
+	res = i2c_smbus_write_byte(file, 0xe7);
 
-		daddress = 0xe7;	// Full brightness (page 15)
-		printf("writing: 0x%02x\n", daddress);
-		res = i2c_smbus_write_byte(file, daddress);
-
-		daddress = 0x00;	// Start writing to address 0 (page 13)
-		printf("writing: 0x%02x\n", daddress);
-		res = i2c_smbus_write_byte(file, daddress);
 
 		int i;
 static __u16 smile_bmp[]={0x3C, 0x42, 0x95, 0xA1, 0xA1, 0x95, 0x42, 0x3C};
-static __u16 frown_bmp[]={0x3C, 0x42, 0xA5, 0x91, 0x91, 0xA5, 0x42, 0x3C};
-static __u16 neutral_bmp[]={0x3c3C, 0x0042, 0x9595, 0x0091, 0x9191, 0x0095, 0x4242, 0x003C};
+static __u16 frown_bmp[]={0x3C00, 0x4200, 0xA500, 0x9100, 0x9100, 0xA500, 0x4200, 0x3C00};
+static __u16 neutral_bmp[]={0x3c3C, 0x4242, 0x9595, 0x9191, 0x9191, 0x9595, 0x4242, 0x3c3C};
 
 /*
  * For some reason the display is rotated one column, so pre-unrotate the
  * data.
  */
-		for(i=0; i<8; i++) {
-			block[i]   =    (smile_bmp[i]&0xfe) >>1 | 
-					(smile_bmp[i]&0x01) << 7;
-		}
-		res = i2c_smbus_write_i2c_block_data(file, daddress, 16, 
-			(__u8 *)block);
+	// Start writing to address 0 (page 13)
+//	res = i2c_smbus_write_byte(file, 0x00);
+	for(i=0; i<8; i++) {
+		block[i]   =    (smile_bmp[i]&0xfe) >>1 | 
+				(smile_bmp[i]&0x01) << 7;
+	}
+	res = i2c_smbus_write_i2c_block_data(file, 0x00, 16, 
+		(__u8 *)block);
 
-		sleep(0);
+	sleep(2);
 
 // Display a new picture
-		for(i=0; i<8; i++) {
-			block[i]   =    (frown_bmp[i]&0xfe) >>1 | 
-					(frown_bmp[i]&0x01) << 7;
-		}
-		daddress = 0x00;
-		printf("writing: 0x%02x\n", daddress);
-		res = i2c_smbus_write_byte(file, daddress);
-
-		res = i2c_smbus_write_i2c_block_data(file, daddress, 16, 
-			(__u8 *)block);
-
-		sleep(0);
-
-// Display yet another picture
-		for(i=0; i<8; i++) {
+	for(i=0; i<8; i++) {
 #ifdef BICOLOR
-			block[i]   =    neutral_bmp[i];
+		block[i] = frown_bmp[i];
 
 #else
-			block[i]   =    (neutral_bmp[i]&0xfe) >>1 | 
-					(neutral_bmp[i]&0x01) << 7;
+		block[i]   =    (frown_bmp[i]&0xfe) >>1 | 
+				(frown_bmp[i]&0x01) << 7;
 #endif
-		}
-		daddress = 0x00;
-		printf("writing: 0x%02x\n", daddress);
-		res = i2c_smbus_write_byte(file, daddress);
-
-		res = i2c_smbus_write_i2c_block_data(file, daddress, 16, 
-			(__u8 *)block);
-
-		for(daddress = 0xef; daddress >= 0xe0; daddress--) {
-		    printf("writing: 0x%02x\n", daddress);
-		    res = i2c_smbus_write_byte(file, daddress);
-
-		    usleep(100000);	// Sleep 0.5 seconds
-		}
-
-		break;
-
-	case I2C_SMBUS_WORD_DATA:
-		res = i2c_smbus_write_word_data(file, daddress, value);
-		break;
-	case I2C_SMBUS_BLOCK_DATA:
-		res = i2c_smbus_write_block_data(file, daddress, len, (const __u8 *)block);
-		break;
-	case I2C_SMBUS_I2C_BLOCK_DATA:
-		res = i2c_smbus_write_i2c_block_data(file, daddress, len, (const __u8 *)block);
-		break;
-	default: /* I2C_SMBUS_BYTE_DATA */
-		res = i2c_smbus_write_byte_data(file, daddress, value);
-		break;
 	}
+	daddress = 0x00;
+	printf("writing: 0x%02x\n", daddress);
+//	res = i2c_smbus_write_byte(file, daddress);
+
+	res = i2c_smbus_write_i2c_block_data(file, daddress, 16, 
+		(__u8 *)block);
+
+	sleep(1);
+
+// Display yet another picture
+	for(i=0; i<8; i++) {
+#ifdef BICOLOR
+		block[i] = neutral_bmp[i];
+
+#else
+		block[i]   =    (neutral_bmp[i]&0xfe) >>1 | 
+				(neutral_bmp[i]&0x01) << 7;
+#endif
+	}
+//	res = i2c_smbus_write_byte(file, 0x00);
+
+	res = i2c_smbus_write_i2c_block_data(file, daddress, 16, 
+		(__u8 *)block);
+
+	for(daddress = 0xef; daddress >= 0xe0; daddress--) {
+//	    printf("writing: 0x%02x\n", daddress);
+	    res = i2c_smbus_write_byte(file, daddress);
+
+	    usleep(100000);	// Sleep 0.5 seconds
+	}
+
 	if (res < 0) {
 		fprintf(stderr, "Error: Write failed\n");
 		close(file);
@@ -228,17 +177,9 @@ static __u16 neutral_bmp[]={0x3c3C, 0x0042, 0x9595, 0x0091, 0x9191, 0x0095, 0x42
 		exit(0);
 	}
 
-	switch (size) {
-	case I2C_SMBUS_BYTE:
-		res = i2c_smbus_read_byte(file);
-		value = daddress;
-		break;
-	case I2C_SMBUS_WORD_DATA:
-		res = i2c_smbus_read_word_data(file, daddress);
-		break;
-	default: /* I2C_SMBUS_BYTE_DATA */
-		res = i2c_smbus_read_byte_data(file, daddress);
-	}
+/*
+	res = i2c_smbus_read_byte(file);
+	value = daddress;
 	close(file);
 
 	if (res < 0) {
@@ -253,6 +194,6 @@ static __u16 neutral_bmp[]={0x3c3C, 0x0042, 0x9595, 0x0091, 0x9191, 0x0095, 0x42
 		printf("Value 0x%0*x written, readback matched\n",
 		       size == I2C_SMBUS_WORD_DATA ? 4 : 2, value);
 	}
-
+*/
 	exit(0);
 }
