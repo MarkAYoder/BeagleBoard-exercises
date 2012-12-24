a69a928bc83c9490f655252e69493822780be4f5
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2012-12-24 17:01:54 +0000

    Removed i2c handler

--- LEDstring/tree.js
+++ LEDstring/tree.js
@@ -110,19 +110,6 @@ io.sockets.on('connection', function (socket) {
         });
     });
 
-    socket.on('i2c', function (i2cNum) {
-//        console.log('Got i2c request:' + i2cNum);
-        exec('i2cget -y 3 ' + i2cNum + ' 0 w',
-            function (error, stdout, stderr) {
-//		The TMP102 returns a 12 bit value with the digits swapped
-                stdout = '0x' + stdout.substring(4,6) + stdout.substring(2,4);
-//                console.log('i2cget: "' + stdout + '"');
-                if(error) { console.log('error: ' + error); }
-                if(stderr) {console.log('stderr: ' + stderr); }
-                socket.emit('i2c', stdout);
-            });
-    });
-
     socket.on('led', function (ledNum) {
         var ledPath = "/sys/class/leds/beaglebone::usr" + ledNum + "/brightness";
 //        console.log('LED: ' + ledPath);
