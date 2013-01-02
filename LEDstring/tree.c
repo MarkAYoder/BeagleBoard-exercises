7206fc2404398249d3ad3996cdf3f800fe897615
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-01-02 02:36:00 +0000

    Switched to new indexed interface.  Much, much faster

--- LEDstring/tree.c
+++ LEDstring/tree.c
@@ -15,17 +15,17 @@ int running=1;
 void clear() {
   int i;
   for (i=0; i<STRAND_LEN; i++) {
-    fprintf(grb_fp, "0 0 0\n");
+    fprintf(grb_fp, "0 0 0 %d\n", i);
   }
 }
 
 void display() {
-	    fprintf(grb_fp, "\n");
+  fprintf(grb_fp, "\n");
 }
 
-void rgb(int red, int green, int blue, int us) {
-    fprintf(grb_fp, "%d %d %d", green, red, blue);
-//    printf("sending %d %d %d for %d", red, green, blue, us);
+void rgb(int red, int green, int blue, int index, int us) {
+    fprintf(grb_fp, "%d %d %d %d", green, red, blue, index);
+    // printf("sending %d %d %d %d for %d\n", red, green, blue, index, us);
     if(us) {
       display();
     }
@@ -35,57 +35,40 @@ void rgb(int red, int green, int blue, int us) {
 // pattern4 matches the static LEDs on the tree.
 void pattern4() {
   int i;
-  for(i=0; i<STRAND_LEN; i+=12) {
-    rgb(MAX,   0,   0, 0);
-    rgb(  0,   0,   0, 0);  
-    rgb(  0,   0,   0, 0);  
-    rgb(  0, MAX,   0, 0);  
-    rgb(  0,   0,   0, 0);  
-    rgb(  0,   0,   0, 0);  
-    rgb(  0,   0, MAX, 0);  
-    rgb(  0,   0,   0, 0);  
-    rgb(  0,   0,   0, 0);  
-    rgb(MAX, MAX/4, 0, 0);  
-    rgb(  0,   0,   0, 0);  
-    rgb(  0,   0,   0, 0);  
-    rgb(MAX, MAX,   0, 0);  
-    rgb(  0,   0,   0, 0);  
-    rgb(  0,   0,   0, 0);  
+  for(i=0; i<STRAND_LEN; i+=15) {
+    rgb(MAX,   0,   0, i+ 00, 0);
+    rgb(  0, MAX,   0, i+ 03, 0);  
+    rgb(  0,   0, MAX, i+ 06, 0);  
+    rgb(MAX, MAX/4, 0, i+  9, 0);  
+    rgb(MAX, MAX,   0, i+ 12, 0);  
   }
   display();
   while(running) {
-    rgb(MAX,   0,   0, DELAY);
-    rgb(  0,   0,   0, DELAY);  
-    rgb(  0,   0,   0, DELAY);  
-    rgb(  0, MAX,   0, DELAY);  
-    rgb(  0,   0,   0, DELAY);  
-    rgb(  0,   0,   0, DELAY);  
-    rgb(  0,   0, MAX, DELAY);  
-    rgb(  0,   0,   0, DELAY);  
-    rgb(  0,   0,   0, DELAY);  
-    rgb(MAX, MAX/4, 0, DELAY);  
-    rgb(  0,   0,   0, DELAY);  
-    rgb(  0,   0,   0, DELAY);  
-    rgb(MAX, MAX,   0, DELAY);  
-    rgb(  0,   0,   0, DELAY);  
-    rgb(  0,   0,   0, DELAY);  
+    rgb(MAX,   0,   0, 0, DELAY);
+    rgb(  0,   0,   0, 0, DELAY);  
+    rgb(  0,   0,   0, 0, DELAY);  
+    rgb(  0, MAX,   0, 0, DELAY);  
+    rgb(  0,   0,   0, 0, DELAY);  
+    rgb(  0,   0,   0, 0, DELAY);  
+    rgb(  0,   0, MAX, 0, DELAY);  
+    rgb(  0,   0,   0, 0, DELAY);  
+    rgb(  0,   0,   0, 0, DELAY);  
+    rgb(MAX, MAX/4, 0, 0, DELAY);  
+    rgb(  0,   0,   0, 0, DELAY);  
+    rgb(  0,   0,   0, 0, DELAY);  
+    rgb(MAX, MAX,   0, 0, DELAY);  
+    rgb(  0,   0,   0, 0, DELAY);  
+    rgb(  0,   0,   0, 0, DELAY);  
   }
 }
 
 // Pattern 3 is a single LED running backward.
 void pattern3() {
-  int i, j;
+  int i;
 
-  for(j=0; j<STRAND_LEN; j++) {
-    for(i=0; i<STRAND_LEN; i++) {
-      if(i==j) {
-        rgb(0, 0, 127, 0);
-      } else {
-        rgb(0, 0, 0, 0);
-      }
-    }
-    display();
-//  usleep(100);
+  for(i=0; i<STRAND_LEN-1; i++) {
+      rgb(0,  0, 0, i, 0);
+      rgb(0, 10, 0, i+1, 10000);
   }
 }
 
@@ -94,7 +77,7 @@ void pattern1() {
   int i;
 
   for (i = 0; i < STRAND_LEN; i++) {
-    rgb(0, i%127, 0, 20000);
+    rgb(0, i%127, 0, i, 20000);
   }
 }
 
@@ -114,7 +97,7 @@ void pattern2() {
       data[j+2] = b;
     }
     for (j = 0; j < STRAND_LEN * 3; j += 3) {
-      fprintf(grb_fp, "%d %d %d\n", data[j], data[j+1], data[j+2]);
+      fprintf(grb_fp, "%d %d %d %d\n", data[j], data[j+1], data[j+2], i/3);
     }
     usleep(200000);
   }
