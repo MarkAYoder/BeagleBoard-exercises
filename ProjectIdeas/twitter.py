6495c502fccdce4192db6b069b6ca3f76075a7db
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-05-16 21:39:18 +0000

    Haven't figured out the encoding error

--- ProjectIdeas/twitter.py
+++ ProjectIdeas/twitter.py
@@ -1,11 +1,14 @@
 import urllib
 import simplejson
 
+# From http://glowingpython.blogspot.de/2011/04/how-to-use-twitter-search-api.html
+
 def searchTweets(query):
  search = urllib.urlopen("http://search.twitter.com/search.json?q="+query)
  dict = simplejson.loads(search.read())
+ print dict["results"]
  for result in dict["results"]: # result is a list of dictionaries
-  print "*", result["text"], "\n"
+  print "*", result["text"].decode()
 
 # we will search tweets about "fc liverpool" football team
-searchTweets("fc+liverpool")
+searchTweets("yoder")
