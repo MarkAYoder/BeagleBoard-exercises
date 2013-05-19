19c12fe3066c6ce3b1af68b6886e0c2bbcaa1b83
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-05-18 21:27:20 -0400

    Commented out some debugging prints

--- ProjectIdeas/twitter.py
+++ ProjectIdeas/twitter.py
@@ -6,9 +6,9 @@ import simplejson
 def searchTweets(query):
  search = urllib.urlopen("http://search.twitter.com/search.json?q="+query)
  dict = simplejson.loads(search.read())
- print dict["results"]
+# print dict["results"]
  for result in dict["results"]: # result is a list of dictionaries
-  print "*", result["text"].decode()
+  print "*", result["text"]
 
 # we will search tweets about "fc liverpool" football team
 searchTweets("yoder")
