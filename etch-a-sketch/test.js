5a3cad62c0ab0d030c6a873fd9cf68a373e0d9b5
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-06-04 11:19:41 -0400

    test.js now does a simple etch-a-sketch with keyboard input and text output.

--- etch-a-sketch/test.js
+++ etch-a-sketch/test.js
@@ -1,25 +1,30 @@
-// I'm testing out 2D arrays
+// Simple etch-a-sketch framework.
 
 var util = require('util');
 
-var grid = new Array(8);
-var x=0,
+var xMax = 8,
+    yMax = 8;
+var grid = new Array(yMax);
+var x=0,	// Current position
     y=0;
 
+// Initialize th grid to all blanks
 for(var i=0; i<grid.length; i++) {
-    grid[i] = new Array(8);
+    grid[i] = new Array(xMax);
     for(var j=0; j<grid[i].length; j++) {
 	grid[i][j] = ' ';
     }
 }
 
-grid[y][x] = '*';
+// Show a + at the current location
+grid[y][x] = '+';
 
+// Print the grid
 function printGrid(grid) {
   util.print('   0 1 2 3 4 5 6 7\n');
-  for(var i=0; i<8; i++) {
+  for(var i=0; i<grid.length; i++) {
     util.print(util.format("%d: ",i));
-    for(var j=0; j<8; j++) {
+    for(var j=0; j<grid[i].length; j++) {
       util.print(util.format("%s ", grid[i][j]));
     }
   util.print("\n");
@@ -28,32 +33,42 @@ function printGrid(grid) {
 
 printGrid(grid);
 
+// This set up the handlers to read user input.
 var readline = require('readline'),
     rl = readline.createInterface(process.stdin, process.stdout);
 
 rl.setPrompt('Direction> ');
 rl.prompt();
 
+// This function is called whenever a newline is entered
 rl.on('line', function(line) {
+  grid[y][x] = '*';
   switch(line.trim()) {
     case 'u':
       console.log('Up!');
+      y--;
+      if(y<0) {y=grid.length-1;}
       break;
     case 'd':
       console.log('Down!');
       y++;
+      if(y >= grid.length) {y=0;}
       break;
     case 'l':
       console.log('Left!');
+      x--;
+      if(x<0) {x = grid[y].length-1;}
       break;
     case 'r':
       console.log('Right!');
+      x++;
+      if(x >= grid[y].length) {x=0;}
       break;
     default:
       console.log('Say what? I might have heard `' + line.trim() + '`');
       break;
   }
-  grid[y][x] = '*';
+  grid[y][x] = '+';
   printGrid(grid);
   rl.prompt();
 }).on('close', function() {
