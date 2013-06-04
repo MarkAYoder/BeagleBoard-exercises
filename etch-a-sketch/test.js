b5c5e17f7e92509abbe5039e3b55dd60b0da07d5
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-06-04 08:12:25 -0400

    Added input loop

--- etch-a-sketch/test.js
+++ etch-a-sketch/test.js
@@ -3,21 +3,61 @@
 var util = require('util');
 
 var grid = new Array(8);
+var x=0,
+    y=0;
 
 for(var i=0; i<grid.length; i++) {
     grid[i] = new Array(8);
     for(var j=0; j<grid[i].length; j++) {
-	grid[i][j] = '*';
+	grid[i][j] = ' ';
     }
 }
 
-//grid[0][0] = 0;
+grid[y][x] = '*';
 
-for(var i=0; i<8; i++) {
+function printGrid(grid) {
+  util.print('   0 1 2 3 4 5 6 7\n');
+  for(var i=0; i<8; i++) {
     util.print(util.format("%d: ",i));
     for(var j=0; j<8; j++) {
-	util.print(util.format("%s ", grid[i][j]));
+      util.print(util.format("%s ", grid[i][j]));
     }
-    util.print("\n");
+  util.print("\n");
+  }
 }
+
+printGrid(grid);
+
+var readline = require('readline'),
+    rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.setPrompt('Direction> ');
+rl.prompt();
+
+rl.on('line', function(line) {
+  switch(line.trim()) {
+    case 'u':
+      console.log('Up!');
+      break;
+    case 'd':
+      console.log('Down!');
+      y++;
+      break;
+    case 'l':
+      console.log('Left!');
+      break;
+    case 'r':
+      console.log('Right!');
+      break;
+    default:
+      console.log('Say what? I might have heard `' + line.trim() + '`');
+      break;
+  }
+  grid[y][x] = '*';
+  printGrid(grid);
+  rl.prompt();
+}).on('close', function() {
+  console.log('Have a great day!');
+  process.exit(0);
+});
 
