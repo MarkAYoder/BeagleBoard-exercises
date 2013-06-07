61d477687bf2ad30f2f8c710add5ee0ebc3b8ff5
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-06-07 13:08:38 -0400

    Finished cpSD2eMMC.sh

--- setup/cpSD2eMMC.sh
+++ setup/cpSD2eMMC.sh
@@ -8,6 +8,27 @@
 
 # First mount the eMMC
 eMMC=/media/eMMCp2
+MODULES=3.8.13-bone20.2
 mkdir -p $eMMC
 mount /dev/mmcblk1p2 $eMMC
+
+echo Copying firmware
+cd $eMMC/lib
+mv firmware firmware.orig
+cp /lib/firmware .
+
+echo Copying modules
+cp /lib/modules/$MODULES $eMMC/lib/modules
+
+echo Copying uImage
+cd $eMMC/boot
+cp /boot/uImage-$MODULES $eMMC/boot
+cp uImage uImage.bak
+ln -s uImage-$MODULES uImage
+
+echo Cleaning up
+cd
+sync
+umount $eMMc
+
 
