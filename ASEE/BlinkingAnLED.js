fa18d2108b64809c540507338c1104d2f60c4d4c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-06-17 16:57:59 +0000

    Added ASEE and updated setup.sh to copy overlay to /lib/firmware

---
+++ ASEE/BlinkingAnLED.js
@@ -0,0 +1,23 @@
+//******************************************************
+var b = require('bonescript');
+
+//Old bonescript defines ’bone’ globally
+var pins = b.bone.pins;
+var ledPin = pins.P9_12;
+var ledPin2 = pins.USR3;
+
+b.pinMode(ledPin, b.OUTPUT);
+b.pinMode(ledPin2, b.OUTPUT);
+
+var state = b.LOW;
+b.digitalWrite(ledPin, state);
+b.digitalWrite(ledPin2, state);
+setInterval(toggle, 100);
+
+function toggle() {
+   if(state == b.LOW) state = b.HIGH;
+     else state = b.LOW;
+   b.digitalWrite(ledPin, state);
+   b.digitalWrite(ledPin2, state);
+}
+//******************************************************
--- LEDstring/setup.sh
+++ LEDstring/setup.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
 # This sets things up for runing the LEDstring
 # 1. enables SPI0 via device tree
 # 2. inserts the lpd8806 kernel module
 
 # Enable SPI0
+if [ ! -e /lib/firmware/BB-SPI0-01-00A0.dtbo ] 
+then
+	echo Copying
+	cp driver/BB-SPI0-01-00A0.dtbo /lib/firmware
+fi
 echo BB-SPI0-01 > /sys/devices/bone_capemgr.*/slots
 
 # Insert module
