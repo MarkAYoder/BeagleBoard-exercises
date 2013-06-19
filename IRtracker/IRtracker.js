c80f7325c45ae0903034da7396b4e55cd8cfbd5d
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-06-19 17:41:06 +0000

    Motor is working

--- IRtracker/IRtracker.js
+++ IRtracker/IRtracker.js
@@ -4,32 +4,30 @@
 
 var b = require('bonescript');
 
-var controller = ["P9_11", "P9_13", "P9_15", "P9_17"];
+var controller = ["P9_11", "P9_13", "P9_15", "P9_16"];
 var i;
 
 for(i=0; i<controller.length; i++) {
     b.pinMode(controller[i], b.OUTPUT);
 }
 
-b.pinMode(ledPin, b.OUTPUT);
-b.pinMode(ledPin2, b.OUTPUT);
-
-var state = [b.LOW, bHIGH, b.HIGH, b.LOW];
+var state = [b.LOW, b.HIGH, b.HIGH, b.LOW];
 for(i=0; i<controller.length; i++) {
     b.digitalWrite(controller[i], state[i]);
 }
 
-b.digitalWrite(ledPin, state);
-b.digitalWrite(ledPin2, state);
+setInterval(CWrotate, 50);
 
-setInterval(toggle, 100);
+function CWrotate() {
+	state = [state[1], state[2], state[3], state[0]];
+	for (i = 0; i < controller.length; i++) {
+		b.digitalWrite(controller[i], state[i]);
+	}
+}
 
-function toggle() {
-    if (state == b.LOW) state = b.HIGH;
-	else state = b.LOW;
-	b.digitalWrite(ledPin, state);
-	b.digitalWrite(ledPin2, state);
-    for (i = 0; i < controller.length; i++) {
-    	b.digitalWrite(controller[i], state);
-    }
+function CCWrotate() {
+    state = [state[3], state[0], state[1], state[2]];
+	for (i = 0; i < controller.length; i++) {
+		b.digitalWrite(controller[i], state[i]);
+	}
 }
