3b569c45fdf09b8a595adf72d54e7f22e8e6ed42
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-06-20 00:06:25 +0000

    Added more instructions to updateBonescript.sh

--- setup/updateBonescript.sh
+++ setup/updateBonescript.sh
@@ -11,3 +11,11 @@ ssh root@192.168.7.2 systemctl stop bonescript
 ssh root@192.168.7.2 opkg install bone101_1.0-r2.3_all.ipk
 ssh root@192.168.7.2 opkg install bonescript_1.0-r21.4_armv7a-vfp-neon.ipk 
 
+# Now, follow the instructions at http://beagleboard.org/support/BoneScript/wired_basic_test/ to confirm a good installation.
+#  You should also check out
+# http://192.168.7.2/Support/BoneScript/readTextFile/ and, when run,
+# it should give you something like:
+#  Bonescript: initialized
+#  x.data = Cloud9 GNOME Image 2013.05.08
+
+#  x.err = null
