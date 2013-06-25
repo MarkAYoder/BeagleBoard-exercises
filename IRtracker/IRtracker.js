3487d7d21391f4c58e4601c045a4529e3653affd
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-06-25 01:55:09 +0000

    Motor control working

--- IRtracker/IRtracker.js
+++ IRtracker/IRtracker.js
@@ -5,11 +5,12 @@
 var b = require('bonescript');
 
 var controller = ["P9_11", "P9_13", "P9_15", "P9_16"];  // Motor is attachedhere
-var PT         = ["P9_33", "P9_35"];                    // Phototransistor inputs
+var PT         = ["P9_37", "P9_35"];                    // Phototransistor inputs
 var buttons    = ["P9_41", "P9_42"];                    // Pushbuttons
 var state = [b.LOW, b.HIGH, b.HIGH, b.LOW]; // Pulse sequence for motor
+var off   = [b.LOW, b.LOW,  b.LOW,  b.LOW]; // Turn motor off
 var steps = 20;                             // 20 steps is one turn
-var rotateDelay = 50;   // ms delay when turning motor
+var rotateDelay = 200;   // ms delay when turning motor
 var CW  = 0;
 var CCW = 1;
 
@@ -23,22 +24,31 @@ for(i=0; i<controller.length; i++) {
 for(i=0; i<buttons.length; i++) {
     b.pinMode(buttons[i], b.INPUT);
 }
-// Put the motor into a know state
-updateState(controller, state, steps, rotateDelay);
+// Put the motor into a known state
+updateState(state);
+rotate(CW, 20, function() {rotate(CCW, 20);});
 
 // One button goes CW the other CCW
-readButtons();
-b.attachInterrupt(buttons[0], true, b.RISING, rotateCW);
-b.attachInterrupt(buttons[1], true, b.RISING, rotateCCW);
+//readButtons();
+//b.attachInterrupt(buttons[0], true, b.RISING, rotateCW);
+//b.attachInterrupt(buttons[1], true, b.RISING, rotateCCW);
 
 //readPT();
 //setInterval(readPT, 500);
 
 // Write the current input state to the controller
-function updateState() {
+function updateState(state) {
 	for (i = 0; i < controller.length; i++) {
 		b.digitalWrite(controller[i], state[i]);
 	}
+}
+
+function rotateNew(direction, count) {
+    var i;
+    rotate(direction, 0);
+    for(i=1; i<count; i++) {
+        
+    }
 }
 
 // These rotate just one step CW or CCW
@@ -49,23 +59,44 @@ function rotateCCW() {
     rotate(CCW, 1);
 }
 // This the the general rotate funtion
-function rotate(direction, count) {
-	console.log("rotate(%d,%d)", direction, count);
+function rotate(direction, count, next) {
+//	console.log("rotate(%d,%d)", direction, count);
 	count--;
     // Rotate the state acording to the direction of rotation
-	if (direction === 0) {
+	if (direction === CW) {
 		state = [state[1], state[2], state[3], state[0]];
 	}
 	else {
 		state = [state[3], state[0], state[1], state[2]];
 	}
-	updateState();
+	updateState(state);
     // Schedule next rotation
-	if (count > 0) {
+    if (count === 0) {
+        direction = 1-direction;    //change direction
+        count = 20;
+    }
+    setTimeout(function() {
+    	rotate(direction, count, next);
+    }, rotateDelay);
+    
+/*	if (count > 0) {
 		setTimeout(function() {
-			rotate(direction, count);
+			rotate(direction, count, next);
 		}, rotateDelay);
+	} else {
+//        console.log("Count = " + count + " next = " + next);
+
+
+        if(next === undefined) {
+            updateState(off);
+            } else {
+//            console.log("next !== undefined");
+            next();
+        }
+
 	}
+*/
+    return "Done with rotate";
 }
 
 function readButtons() {
