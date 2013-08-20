429fc91e7b6269bd31fc23ea24e8cc2c6957400f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-08-20 13:58:02 -0400

    Fixed some typos

--- setup/install.sh
+++ setup/install.sh
@@ -6,8 +6,8 @@ set -e
 BONE=192.168.7.2
 BONE_NAME=yoder-black-bone
 
-if [ 1 == 0 ] ; then
-sh-copy-id root@$BONE
+if [ 1 == 1 ] ; then
+ssh-copy-id root@$BONE
 DATE=`date`
 ssh root@$BONE "date -s \"$DATE\""
 ssh root@$BONE "echo $BONE_NAME > /etc/hostname"
@@ -15,11 +15,11 @@ ssh root@$BONE "opkg update"
 
 ssh root@$BONE "git config --global user.name \"Mark A. Yoder\""
 ssh root@$BONE "git config --global user.email Mark.A.Yoder@Rose-Hulman.edu"
-scp -r .ssh root@BONE:.
+scp -r .ssh root@$BONE:.
 ssh root@$BONE "git clone git@github.com:MarkAYoder/BeagleBoard-exercises.git exercises"
 
 ssh root@$BONE "cp exercises/.bashrc ."
-ssh root@$BONE "ln -s /var/lib/cloud9 ."
+ssh root@$BONE "cd /var/lib/cloud9; ln -s ~/exercises ."
 fi
 
 ssh root@$BONE "rm /etc/localtime"
