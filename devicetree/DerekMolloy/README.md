9cdb42cfdddc246aeef2413940c21d05e0db11a4
root <root@yoder-black-bone.dhcp.rose-hulman.edu>
2013-08-27 09:31:32 -0400

    CLeaned up moved device tree stuff

--- devicetree/DerekMolloy/README.md
+++
@@ -1,4 +0,0 @@
-boneDeviceTree
-==============
-
-Working Project on the DeviceTree (note: files 3.8.11 Angstrom distro)
--- devicetree/setup.sh
+++
@@ -1,4 +0,0 @@
-# Taken from "Bad to the Bone" page 189
-export SLOTS=/sys/devices/bone_capemgr.*/slots
-export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
-export PINGROUS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pingroups
