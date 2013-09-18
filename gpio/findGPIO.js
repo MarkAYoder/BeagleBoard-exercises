da307206f64355f424644f4fe44bf57ee6f1bf3f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-09-18 09:34:42 -0400

    Takes multiple pin numbers

--- gpio/findGPIO.js
+++ gpio/findGPIO.js
@@ -2,6 +2,8 @@
 // Program to test looking up information in /usr/share/bone101/static/bone.js
 // Usage:  findGPIO.js 7     # Look up info for gpio7 (internal pin number)
 //         findGPIO.js P9_12 # Look up using header pin number (external)
+//         findGPIO.js P9_12 P9_13 ...  # Look up multiple pins and use one line
+//                                      # output for each.
 // Returns current pin mux
 
 var PINS = "/sys/kernel/debug/pinctrl/44e10800.pinmux/pins",
@@ -15,7 +17,7 @@ process.argv.forEach(function(val, index, array) {
 });
 */
 
-function pinMux(gpio) {
+function pinMux(gpio, flag) {
     var addr = '(' + (0x44e10800 + 
                     parseInt(gpio.muxRegOffset, 16)).toString(16) + ')';
     
@@ -30,9 +32,10 @@ function pinMux(gpio) {
                 if(stderr) { console.log('stderr: ' + stderr); }
                 
                 stdout = stdout.substring(0,stdout.length-1);  // Get rid of extra \n
-                console.log(stdout);
+                // console.log(stdout);
                 mux = parseInt(stdout.split(" ")[3], 16);  // Get the mux field
-                out = "Mode: " + (mux & 0x7) + " (" + gpio.options[mux & 0x7] + ")";
+                out = gpio.key + ' (gpio ' + gpio.gpio + ") mode: " + (mux & 0x7) + 
+                        " (" + gpio.options[mux & 0x7] + ")";
                 if(!(mux & 0x8)) {   // Pullup or down is enabled
                     if(mux & 0x10) {
                         dir = 'up';
@@ -47,27 +50,39 @@ function pinMux(gpio) {
                 }
                 console.log(out);
             });
-
-    exec('grep "' + addr + '" ' + PINMUX,
+    if(flag) {
+        exec('grep "' + addr + '" ' + PINMUX,
             function (error, stdout, stderr) {
 
                 if(error) { console.log('error: ' + error); }
                 if(stderr) {console.log('stderr: ' + stderr); }
                 
+                stdout = stdout.substring(0,stdout.length-1);  // Get rid of extra \n
                 console.log(stdout);
             });
+    }
 }
 
-var gpio = process.argv[2].toUpperCase();
-if (gpio[0] === 'P') {
-    console.log(b.bone.pins[gpio]);
-    pinMux(b.bone.pins[gpio]);
-} else {
-	console.log("Looking for gpio " + gpio);
-	for (var i in b.bone.pins) {
-		if (b.bone.pins[i].gpio === parseInt(gpio,10)) {
-			console.log(b.bone.pins[i]);
-            pinMux(b.bone.pins[i]);
-		}
-	}
+var gpio,
+    flag = process.argv.length < 4,
+    i;
+
+for(i=2; i<process.argv.length; i++) {
+    gpio = process.argv[i].toUpperCase();
+    if (gpio[0] === 'P') {
+        if(flag) {
+            console.log(b.bone.pins[gpio]);
+        }
+        pinMux(b.bone.pins[gpio], flag);
+    } else {
+        console.log("Looking for gpio " + gpio);
+        for (var i in b.bone.pins) {
+            if (b.bone.pins[i].gpio === parseInt(gpio,10)) {
+                if(flag) {
+                    console.log(b.bone.pins[i]);
+                }
+                pinMux(b.bone.pins[i], flag);
+            }
+        }
+    }
 }
