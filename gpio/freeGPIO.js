ff402a3658a98a768fe1d7c89c3f2e3235f7adaa
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-09-18 07:29:32 -0400

    Added Approach comments

--- gpio/freeGPIO.js
+++ gpio/freeGPIO.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
 // Lists all gpio pins with MUX UNCLAIMED and GPIO UNCLAIMED in $PINMUX
 // Usage:  freeGPIO.js    # List free GPIOs using P8 or P9 pin number
+//
+// Approach:
+//  1. search PINMUX for all entries with "(MUX UNCLAIMED) (GPIO UNCLAIMED)"
+//  2. An entry looks like "pin 8 (44e10820): (MUX UNCLAIMED) (GPIO UNCLAIMED)"
+//  3. Extract the address (44e10820) and subtract 0x44e10800 to ge the offset
+//  4. Format the offset as 0x020, with leading 0's to give 3 digits
+//  5. Search for the address in the muxRegOffset field in b.bone.pins
+//  6. Print the matching key
 
-var PINS = "/sys/kernel/debug/pinctrl/44e10800.pinmux/pins",
-    PINMUX = "/sys/kernel/debug/pinctrl/44e10800.pinmux/pinmux-pins",
+var PINMUX = "/sys/kernel/debug/pinctrl/44e10800.pinmux/pinmux-pins",
     b = require('bonescript'),
     exec = require('child_process').exec,
     addr;
