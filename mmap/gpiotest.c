fdd8ca7a766e3dda7ff596593d37660f98579e40
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-09-26 14:01:47 -0400

    Added mmap example

---
+++ mmap/gpiotest.c
@@ -0,0 +1,56 @@
+// From : http://stackoverflow.com/questions/13124271/driving-beaglebone-gpio-through-dev-mem
+//
+// Toggles a gpio pin using mmap.
+// Be sure to set -O3 when compiling.
+// Modified by Mark A. Yoder  26-Sept-2013
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h> 
+#include "beaglebone_gpio.h"
+
+int main(int argc, char *argv[]) {
+    volatile void *gpio_addr = NULL;
+    volatile unsigned int *gpio_oe_addr = NULL;
+    volatile unsigned int *gpio_setdataout_addr = NULL;
+    volatile unsigned int *gpio_cleardataout_addr = NULL;
+    unsigned int reg;
+    int fd = open("/dev/mem", O_RDWR);
+
+    printf("Mapping %X - %X (size: %X)\n", GPIO1_START_ADDR, GPIO1_END_ADDR, GPIO1_SIZE);
+
+    gpio_addr = mmap(0, GPIO1_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO1_START_ADDR);
+
+    gpio_oe_addr = gpio_addr + GPIO_OE;
+    gpio_setdataout_addr = gpio_addr + GPIO_SETDATAOUT;
+    gpio_cleardataout_addr = gpio_addr + GPIO_CLEARDATAOUT;
+
+    if(gpio_addr == MAP_FAILED) {
+        printf("Unable to map GPIO\n");
+        exit(1);
+    }
+    printf("GPIO mapped to %p\n", gpio_addr);
+    printf("GPIO OE mapped to %p\n", gpio_oe_addr);
+    printf("GPIO SETDATAOUTADDR mapped to %p\n", gpio_setdataout_addr);
+    printf("GPIO CLEARDATAOUT mapped to %p\n", gpio_cleardataout_addr);
+
+    reg = *gpio_oe_addr;
+    printf("GPIO1 configuration: %X\n", reg);
+    reg = reg & (0xFFFFFFFF - USR1_LED);
+    *gpio_oe_addr = reg;
+    printf("GPIO1 configuration: %X\n", reg);
+
+    printf("Start blinking LED USR1\n");
+    while(1) {
+        // printf("ON\n");
+        *gpio_setdataout_addr= GPIO_03;
+        // usleep(1);
+        // printf("OFF\n");
+        *gpio_cleardataout_addr = GPIO_03;
+        // usleep(1);
+    }
+
+    close(fd);
+    return 0;
+}
