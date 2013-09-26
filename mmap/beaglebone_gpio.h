0edfa757bb92410e86d201f625a828f927e37f75
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-09-26 17:15:06 -0400

    Reading a gpio pin and sending out to another pin

--- mmap/beaglebone_gpio.h
+++ mmap/beaglebone_gpio.h
@@ -1,5 +1,5 @@
 // From : http://stackoverflow.com/questions/13124271/driving-beaglebone-gpio-through-dev-mem
-//#ifndef _BEAGLEBONE_GPIO_H_
+#ifndef _BEAGLEBONE_GPIO_H_
 #define _BEAGLEBONE_GPIO_H_
 
 //#define GPIO1_START_ADDR 0x4804C000
@@ -7,7 +7,7 @@
 #define GPIO1_END_ADDR 0x4804DFFF
 #define GPIO1_SIZE (GPIO1_END_ADDR - GPIO1_START_ADDR)
 #define GPIO_OE 0x134
-#define GPIO_DATAIN 0x138GPIO_DATAIN
+#define GPIO_DATAIN 0x138
 #define GPIO_SETDATAOUT 0x194
 #define GPIO_CLEARDATAOUT 0x190
 
@@ -15,7 +15,8 @@
 #define USR1_LED (1<<22)
 #define USR2_LED (1<<23)
 #define USR3_LED (1<<24)
-#define GPIO_60  (1<<28)
 #define GPIO_03  (1<<3)
+#define GPIO_07  (1<<7)
+#define GPIO_60  (1<<28)
 
 #endif
