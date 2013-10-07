4d498c4d6d1914f22f2f0aa5eb55d86e4020133d
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-10-07 14:10:37 -0400

    Added status line

--- realtime/ballAndCube.js
+++ realtime/ballAndCube.js
@@ -1,11 +1,42 @@
 // Extended example from https://github.com/mrdoob/three.js/
 // http://www.aerotwist.com/tutorials/getting-started-with-three-js/
+var socket,
+    firstconnect = true;
 var camera, scene, renderer;
-var geometry, material, mesh;
+var geometry, material, mesh, mesh2;
 var direction = 1;
 
 init();
 animate();
+connect();
+
+    function connect() {
+      if(firstconnect) {
+        socket = io.connect(null);
+
+        socket.on('message', function(data)
+            { status_update("Received: message " + data);});
+        socket.on('connect', function()
+            { status_update("Connected to Server"); });
+        socket.on('disconnect', function()
+            { status_update("Disconnected from Server"); });
+        socket.on('reconnect', function()
+            { status_update("Reconnected to Server"); });
+        socket.on('reconnecting', function( nextRetry )
+            { status_update("Reconnecting in " + nextRetry/1000 + " s"); });
+        socket.on('reconnect_failed', function()
+            { status_update("Reconnect Failed"); });
+
+        socket.on('ain',  ain);
+
+        firstconnect = false;
+      } else {
+          }
+      }
+
+    function status_update(txt){
+      document.getElementById('status').innerHTML = txt;
+    }
 
 function init() {
         // set the scene size
@@ -13,7 +44,7 @@ function init() {
 	    HEIGHT = 300;
 
 	// set some camera attributes
-	var VIEW_ANGLE = 45,
+	var VIEW_ANGLE = 75,
 	    ASPECT = WIDTH / HEIGHT,
 	    NEAR = 0.1,
 	    FAR = 10000;
@@ -21,9 +52,6 @@ function init() {
 	// get the DOM element to attach to
 	// - assume we've got jQuery to hand
 	var $container = $('#container');
-
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
-    camera.position.z = 300;
 
     scene = new THREE.Scene();
 
@@ -37,6 +65,10 @@ function init() {
     scene.add(mesh);
     console.log(mesh);
     
+    camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+    camera.position.z = 300;
+	scene.add(camera)
+    
     geometry = new THREE.SphereGeometry(100, 16, 16);
     material = new THREE.MeshBasicMaterial({
         color: 0xff0000,
@@ -48,7 +80,7 @@ function init() {
 
     renderer = new THREE.CanvasRenderer();
 //    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setSize(400, 300);
+    renderer.setSize(WIDTH, HEIGHT);
     
     // attach the render-supplied DOM element
 	$container.append(renderer.domElement);
@@ -65,13 +97,13 @@ function animate() {
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.02;
 
-    if(mesh2.position.x > 100) {
+    if(mesh2.position.y > 100) {
         direction = -1;
     }
-    if(mesh2.position.x < -100) {
+    if(mesh2.position.y < -100) {
         direction = 1;
     }
-    mesh2.position.x += direction * 1;
+    mesh2.position.y += direction * 1;
 
 //    mesh2.position.x += 1;
     renderer.render(scene, camera);
