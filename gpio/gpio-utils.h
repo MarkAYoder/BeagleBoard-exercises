21014ccb1f4e4285ed8ad6f2eb7c76058ca2caaf
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-10-07 11:38:14 -0400

    Added ain_get_value Removed gpioThru from Makefile

--- gpio/gpio-utils.h
+++ gpio/gpio-utils.h
@@ -11,3 +11,7 @@ int gpio_set_edge(unsigned int gpio, const char *edge);
 int gpio_fd_open(unsigned int gpio, unsigned int dir);
 int gpio_fd_close(int fd);
 
+// Analog in
+#define ADC_BUF 1024
+#define SYSFS_AIN_DIR "/sys/devices/ocp.2/helper.11"
+int ain_get_value(unsigned int ain, unsigned int *value);
