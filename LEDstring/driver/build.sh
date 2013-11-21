0260cb7e4f6b70c81e0ecc1b9de3c478aa27a3ba
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-11-20 19:14:39 -0500

    Doesn't rebuild all modules.  Much faster.

---
+++ LEDstring/driver/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# From page 224 of Embedded Linux Primer
+# Be sure to source ~/crossCompileEnv.sh
+
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabi-
+DRIVER_PATH=~/BeagleBoard/linux-dev/KERNEL/drivers/char
+
+cp lpd8806.c $DRIVER_PATH
+pushd $DRIVER_PATH
+make -C ~/BeagleBoard/linux-dev/KERNEL SUBDIRS=$PWD modules
+
+popd
+cp $DRIVER_PATH/lpd8806.ko .
+
+scp *.ko bone:.
