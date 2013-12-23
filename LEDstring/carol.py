6a95c3576895ab929d61ebaa778e8fb2564b6924
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-12-22 19:38:06 -0500

    Playing with python

---
+++ LEDstring/carol.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+from time import sleep
+import math
+
+len = 320
+amp = 25
+f = 25
+shift = 3
+time = 0
+max = 128
+
+# Open a file
+fo = open("/sys/firmware/lpd8806/device/rgb", "w", 0)
+
+while True:
+    for i in range(0, len):
+        r = (amp * (math.sin(2*math.pi*f*(i+time-0*shift)/len) + 1)) + 1;
+        g = 4#(amp * (math.sin(2*math.pi*f*(i-time-1*shift)/len) + 1)) + 1;
+        b = 1;#(amp * (math.sin(2*math.pi*f*(i-time-2*shift)/len) + 1)) + 1;
+        fo.write("%d %d %d %d" % (r%max, g%max, b%max, i))
+        # print("0 0 127 %d" % (i))
+
+    fo.write("\n");
+    time = time + 1
+    sleep(0.100)
+
+# Close opened file
+fo.close()
---
+++ LEDstring/play.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+from time import sleep
+import math
+
+len = 320
+max = 30
+
+# Open a file
+fo = open("/sys/firmware/lpd8806/device/rgb", "w", 0)
+
+# Turn whole string off
+for i in range(0, len):
+    fo.write("%d %d %d %d" % (max, 0, 0, i))
+
+while True:
+    for i in range(0, len-1):
+        fo.write("%d %d %d %d" % (0, 0, 0, i))
+        fo.write("%d %d %d %d" % (max, 0, 0, i+1))
+        fo.write("\n");
+        sleep(0.05)
+    
+    for i in range(len, 1, -1):
+        fo.write("%d %d %d %d" % (0, 0, 0, i))
+        fo.write("%d %d %d %d" % (0, 0, max, i-1))
+        fo.write("\n");
+        sleep(0.02)
+        
+# Close opened file
+fo.close()
