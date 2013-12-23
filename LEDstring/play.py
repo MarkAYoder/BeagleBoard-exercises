9d83484c5a2014a46385e95f7fc84556a594281b
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-12-22 20:02:44 -0500

    Moved things to functions

--- LEDstring/play.py
+++ LEDstring/play.py
@@ -9,10 +9,11 @@ max = 30
 fo = open("/sys/firmware/lpd8806/device/rgb", "w", 0)
 
 # Turn whole string off
-for i in range(0, len):
-    fo.write("%d %d %d %d" % (max, 0, 0, i))
-
-while True:
+def clear(r,g,b):
+    for i in range(0, len):
+        fo.write("%d %d %d %d" % (r, g, b, i))
+        
+def skiUpDown():
     for i in range(0, len-1):
         fo.write("%d %d %d %d" % (0, 0, 0, i))
         fo.write("%d %d %d %d" % (max, 0, 0, i+1))
@@ -24,6 +25,11 @@ while True:
         fo.write("%d %d %d %d" % (0, 0, max, i-1))
         fo.write("\n");
         sleep(0.02)
+    
+clear(0, 20, 0)
+
+while True:
+    skiUpDown()
         
 # Close opened file
 fo.close()
