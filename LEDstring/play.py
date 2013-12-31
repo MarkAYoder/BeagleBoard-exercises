20f9e99618ffc108d6ea86381a3749cec9a86a97
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-12-31 16:56:01 -0500

    Switched to a master update thread

--- LEDstring/play.py
+++ LEDstring/play.py
@@ -9,6 +9,14 @@ max = 30
 # Open a file
 fo = open("/sys/firmware/lpd8806/device/rgb", "w", 0)
 
+# Update the thread at a regular interval
+def keepDisplaying(delay):
+    print "keepDisplaying called with delay = %f" % delay
+    
+    while True:
+        fo.write("\n")
+        sleep(delay)
+
 # Turn whole string off
 def clear(r,g,b):
     for i in range(0, len):
@@ -19,16 +27,18 @@ def skiUpDown():
         for i in range(0, len-1):
             fo.write("%d %d %d %d" % (0, 0, 0, i))
             fo.write("%d %d %d %d" % (max, 0, 0, i+1))
-            fo.write("\n")
+#            fo.write("\n")
             sleep(0.05)
         
         for i in range(len, 1, -1):
             fo.write("%d %d %d %d" % (0, 0, 0, i))
             fo.write("%d %d %d %d" % (0, 0, max, i-1))
-            fo.write("\n")
+#            fo.write("\n")
             sleep(0.02)
     
 clear(0, 2, 0)
+
+threading.Thread(target=keepDisplaying, args=(0.01,)).start()
 
 for i in range(10):
     threading.Thread(target=skiUpDown).start()
