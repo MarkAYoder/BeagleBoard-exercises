7e1166a392bb5e2a63d7749fbfbbab3ddbc96579
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2013-12-31 17:18:20 -0500

    Added signal handler

--- LEDstring/play.py
+++ LEDstring/play.py
@@ -2,18 +2,27 @@
 from time import sleep
 import math
 import threading
+import signal
+import sys
 
 len = 320
 max = 30
+keepgoing = True
 
-# Open a file
-fo = open("/sys/firmware/lpd8806/device/rgb", "w", 0)
+def signal_handler(signal, frame):
+    global keepgoing
+    print 'You pressed Ctrl+C!'
+    keepgoing = False
+#    fo.close()
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
 # Update the thread at a regular interval
 def keepDisplaying(delay):
+    global keepgoing
     print "keepDisplaying called with delay = %f" % delay
     
-    while True:
+    while keepgoing:
         fo.write("\n")
         sleep(delay)
 
@@ -23,19 +32,21 @@ def clear(r,g,b):
         fo.write("%d %d %d %d" % (r, g, b, i))
         
 def skiUpDown():
-    while True:
+    global keepgoing
+    while keepgoing:
         for i in range(0, len-1):
             fo.write("%d %d %d %d" % (0, 0, 0, i))
             fo.write("%d %d %d %d" % (max, 0, 0, i+1))
-#            fo.write("\n")
             sleep(0.05)
         
         for i in range(len, 1, -1):
             fo.write("%d %d %d %d" % (0, 0, 0, i))
             fo.write("%d %d %d %d" % (0, 0, max, i-1))
-#            fo.write("\n")
             sleep(0.02)
     
+# Open a file
+fo = open("/sys/firmware/lpd8806/device/rgb", "w", 0)
+
 clear(0, 2, 0)
 
 threading.Thread(target=keepDisplaying, args=(0.01,)).start()
