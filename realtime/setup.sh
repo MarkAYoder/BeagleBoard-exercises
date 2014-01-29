8e8e736dd076fd6bab39bbe9cf09def3a9e8e7db
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-01-29 17:31:58 -0500

    Adding automatic loading of jquery files

--- realtime/setup.sh
+++ realtime/setup.sh
@@ -1,1 +1,12 @@
-npm install -g socket.io
+#!/bin/bash
+# Loads files needed to run boneServer.js
+
+# npm install -g socket.io
+
+# wget http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+# wget http://jqueryui.com/resources/download/jquery-ui-1.10.4.zip
+# wget http://www.flotcharts.org/downloads/flot-0.8.2.zip
+
+# unzip jquery-ui-1.10.4.zip
+mv jquery-ui-1.10.4/ui/minified/jquery-ui.min.js .
+mv jquery-ui-1.10.4/themes/base/jquery-ui.css .
