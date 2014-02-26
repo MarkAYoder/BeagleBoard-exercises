20d758c86895adc48efbb2b106176930a0ef87d1
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-02-26 07:42:29 -0500

    Looks up DNS used by host and passes it on to the Beagle

--- setup/host.setDNS.sh
+++ setup/host.setDNS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# These are the commands to run on the host to setup IP masquerading so the Beagle
+# These are the commands to run on the host so the Beagle
 #  can access the Internet through the USB connection.
 # Run host.ipForward.sh the first time. It will set up the host.
 # Run this script if the host is already set up.
@@ -10,30 +10,19 @@ beagleAddr=192.168.7.2
 
 # Save the /etc/resolv.conf on the Beagle in case we mess things up.
 ssh root@$beagleAddr "mv -n /etc/resolv.conf /etc/resolv.conf.orig"
-# Copy the resolv.conf file to the Beagle.  Now the Beagle will use the
-# same name servers as the host.
+# Create our own resolv.conf
 cat - << EOF > /tmp/resolv.conf
 # This is installed by host.setDNS.sh on the host
-# Mark A. Yoder, 25-Aug-2013
+# Mark A. Yoder, 25-FEB-2014
 search rose-hulman.edu dhcp.rose-hulman.edu wlan.rose-hulman.edu
 
 EOF
 
-# Use the campus name servers if on compus or is in a VM,
-# otherwise use the Google name servers
-if ifconfig | egrep "addr:137.112.|addr:192.168."; then
-cat - << EOF >> /tmp/resolv.conf
-nameserver 137.112.18.59
-nameserver 137.112.5.28
-nameserver 137.112.4.196
-EOF
-else
-cat - << EOF >> /tmp/resolv.conf
-nameserver 8.8.8.8
-nameserver 8.8.4.4
-EOF
-fi
+# Look up the nameserver of the host and add it to our resolv.conf
+# From: http://askubuntu.com/questions/197036/how-to-know-what-dns-am-i-using-in-ubuntu-12-04
+nmcli dev list | grep IP4.DNS | sed 's/IP4.DNS\[.\]:/nameserver/' >> /tmp/resolv.conf
 scp /tmp/resolv.conf root@$beagleAddr:/etc
+
 # Tell the beagle to use the host as the gateway.
 ssh root@$beagleAddr "/sbin/route add default gw $hostAddr" || true
 
