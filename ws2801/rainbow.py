5e1f76907f8b45b74cac6e3e7dfd2ae13af30e3e
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-05-14 23:08:38 -0400

    Turned off last two rows since they were distracting

--- ws2801/rainbow.py
+++ ws2801/rainbow.py
@@ -4,8 +4,8 @@ import math
 import sys
 from LedStrip_WS2801 import LedStrip_WS2801
 
-amp = 15
-f = 25
+amp = 10
+f = 10
 shift = 3
 
 def fillAll(ledStrip, color, sleep):
@@ -15,8 +15,12 @@ def fillAll(ledStrip, color, sleep):
 
 def rainbow(ledStrip, nrOfleds):
     phase = 0
+    skip = 60
+    for i in range(nrOfleds-skip, nrOfleds):
+        ledStrip.setPixel(i, [0, 0, 0])
+
     while True:
-        for i in range(0, nrOfleds):
+        for i in range(0, nrOfleds-skip):
             r = int((amp * (math.sin(2*math.pi*f*(i-phase-0*shift)/nrOfleds) + 1)) + 1)
             g = int((amp * (math.sin(2*math.pi*f*(i-phase-1*shift)/nrOfleds) + 1)) + 1)
             b = int((amp * (math.sin(2*math.pi*f*(i-phase-2*shift)/nrOfleds) + 1)) + 1)
@@ -24,8 +28,8 @@ def rainbow(ledStrip, nrOfleds):
     
         ledStrip.update()
 
-        phase = phase + 1
-        time.sleep(0.100)
+        phase = phase + 0.5
+        time.sleep(0.050)
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
