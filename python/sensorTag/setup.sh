e9b702139552fd7bd72f2cbec91ac8129bc3d738
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-06-12 10:42:16 -0400

    Adding more detailed instructions

--- python/sensorTag/setup.sh
+++ python/sensorTag/setup.sh
@@ -1,3 +1,22 @@
-export BLE=90:59:AF:0B:84:57
+apt-get install bluez
+# This appears to be an old version of bluez
+# Here's how to get the current one
+wget http://www.kernel.org/pub/linux/bluetooth/bluez-5.19.tar.xz
+tar xvf bluez-5.19.tar.xz
+apt-get install libusb-dev libdbus-1-dev libglib2.0-dev automake libudev-dev libical-dev libreadline-dev
+./configure --disable-systemd
+make
+make install
 
+hcitool lescan
+export BLE=90:59:AF:0B:84:57
+hcitool lecc $BLE
 hciconfig hci0 reset
+gatttool -b $BLE --interactive
+
+connect
+char-read-hnd
+exit
+
+pip install pexpect
+
