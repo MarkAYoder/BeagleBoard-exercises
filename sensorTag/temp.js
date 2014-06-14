49bd9e0d6cfcd0b54037f4248d7cfee01664d2fd
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-06-14 18:59:05 -0400

    Able to control light table demo

--- sensorTag/temp.js
+++ sensorTag/temp.js
@@ -15,8 +15,11 @@ var async = require('async');
 
 var SensorTag = require('sensortag');
 
+var fs = require('fs');
+
 var demoNumber = 0;
 var maxDemo = 6;
+cntFile = "../ws2801/demo.txt"
 
 SensorTag.discover(function(sensorTag) {
   console.log('sensorTag = ' + sensorTag);
@@ -34,62 +37,63 @@ SensorTag.discover(function(sensorTag) {
         console.log('discoverServicesAndCharacteristics');
         sensorTag.discoverServicesAndCharacteristics(callback);
       },
-      function(callback) {
-        console.log('enableIrTemperature');
-        sensorTag.enableIrTemperature(callback);
-      },
-      function(callback) {
-        setTimeout(callback, 1000);
-      },
-      function(callback) {
-        console.log('readIrTemperature');
-        sensorTag.readIrTemperature(function(objectTemperature, ambientTemperature) {
-          console.log('\tobject temperature = %d °C', objectTemperature.toFixed(1));
-          console.log('\tambient temperature = %d °C', ambientTemperature.toFixed(1));
+      // function(callback) {
+      //   console.log('enableIrTemperature');
+      //   sensorTag.enableIrTemperature(callback);
+      // },
+      // function(callback) {
+      //   setTimeout(callback, 1000);
+      // },
+      // function(callback) {
+      //   console.log('readIrTemperature');
+      //   sensorTag.readIrTemperature(function(objectTemperature, ambientTemperature) {
+      //     console.log('\tobject temperature = %d °C', objectTemperature.toFixed(1));
+      //     console.log('\tambient temperature = %d °C', ambientTemperature.toFixed(1));
 
-          callback();
-        });
+      //     callback();
+      //   });
 
-        // sensorTag.on('irTemperatureChange', function(objectTemperature, ambientTemperature) {
-        //   console.log('\tobject temperature = %d °C', objectTemperature.toFixed(1));
-        //   console.log('\tambient temperature = %d °C', ambientTemperature.toFixed(1))
-        // });
+      //   // sensorTag.on('irTemperatureChange', function(objectTemperature, ambientTemperature) {
+      //   //   console.log('\tobject temperature = %d °C', objectTemperature.toFixed(1));
+      //   //   console.log('\tambient temperature = %d °C', ambientTemperature.toFixed(1))
+      //   // });
 
-        // sensorTag.notifyIrTemperature(function() {
-        //   console.log('notifyIrTemperature');
-        // });
-      },
-      function(callback) {
-        console.log('disableIrTemperature');
-        sensorTag.disableAccelerometer(callback);
-      },
-      function(callback) {
-        console.log('enableAccelerometer');
-        sensorTag.enableAccelerometer(callback);
-      },
-      function(callback) {
-        setTimeout(callback, 1000);
-      },
-      function(callback) {
-        console.log('readAccelerometer');
-        sensorTag.readAccelerometer(function(x, y, z) {
-          console.log('\tx = %d G', x.toFixed(1));
-          console.log('\ty = %d G', y.toFixed(1));
-          console.log('\tz = %d G', z.toFixed(1));
+      //   // sensorTag.notifyIrTemperature(function() {
+      //   //   console.log('notifyIrTemperature');
+      //   // });
+      // },
+      // function(callback) {
+      //   console.log('disableIrTemperature');
+      //   sensorTag.disableAccelerometer(callback);
+      // },
+      
+      // function(callback) {
+      //   console.log('enableAccelerometer');
+      //   sensorTag.enableAccelerometer(callback);
+      // },
+      // function(callback) {
+      //   setTimeout(callback, 10);
+      // },
+      // function(callback) {
+      //   console.log('readAccelerometer');
+      //   sensorTag.readAccelerometer(function(x, y, z) {
+      //     console.log('\tx = %d G', x.toFixed(1));
+      //     console.log('\ty = %d G', y.toFixed(1));
+      //     console.log('\tz = %d G', z.toFixed(1));
 
-          callback();
-        });
+      //     callback();
+      //   });
 
-        sensorTag.on('accelerometerChange', function(x, y, z) {
-          console.log('\tx = %d G', x.toFixed(1));
-          console.log('\ty = %d G', y.toFixed(1));
-          console.log('\tz = %d G', z.toFixed(1));
-        });
+      //   sensorTag.on('accelerometerChange', function(x, y, z) {
+      //     console.log('\tx = %d G', x.toFixed(1));
+      //     console.log('\ty = %d G', y.toFixed(1));
+      //     console.log('\tz = %d G', z.toFixed(1));
+      //   });
 
-        sensorTag.notifyAccelerometer(function() {
+      //   sensorTag.notifyAccelerometer(function() {
 
-        });
-      },
+      //   });
+      // },
       function(callback) {
         console.log('readSimpleRead');
         sensorTag.on('simpleKeyChange', function(left, right) {
@@ -109,6 +113,10 @@ SensorTag.discover(function(sensorTag) {
             }
           }
           console.log('demoNumber = ' + demoNumber);
+          fs.writeFile(cntFile, demoNumber, function (err) {
+            if (err) throw err;
+            console.log('It\'s saved!');
+          });
           if (left && right) {
             sensorTag.notifySimpleKey(callback);
           }
