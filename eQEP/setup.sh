b9b6d2ce2c8ea9e0a7b83dc95f93496008b37187
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-06-23 16:16:22 -0400

    Added github path

--- eQEP/setup.sh
+++ eQEP/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# From: https://github.com/Teknoman117/beaglebot
 # dtc -O dtb -o bone_eqep2b-00A0.dtbo -b 0 -@ bone_eqep2b.dts 
 cp bone_eqep2b-00A0.dtbo /lib/firmware
 echo bone_eqep2b > $SLOTS 
