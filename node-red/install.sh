6221f55bc142cb7c44f3cdd68c6a96850b8a69e4
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-07-09 13:44:44 -0400

    Added install insturctions of sensorTag

--- node-red/install.sh
+++ node-red/install.sh
@@ -1,11 +1,15 @@
 # From: http://nodered.org/docs/getting-started/installation.html
 git clone https://github.com/node-red/node-red.git
 cd node-red/
-npm install --production
+npm install --production	# 4 minutes
 cd nodes
-time git clone https://github.com/node-red/node-red-nodes.git
+git clone https://github.com/node-red/node-red-nodes.git # 2 seconds
 
 # To run
 cd node-red
 node red.js
 
+# To use sensorTag
+# From: https://github.com/sandeepmistry/node-sensortag
+apt-get install libbluetooth-dev
+npm install -g sensortag
