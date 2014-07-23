816816d1e447840db87a09c4fc4c8d4e8501b9c9
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-07-23 16:55:25 -0400

    Commented out run statements

--- node-red/install.sh
+++ node-red/install.sh
@@ -1,4 +1,5 @@
 # From: http://nodered.org/docs/getting-started/installation.html
+cd
 git clone https://github.com/node-red/node-red.git
 cd node-red/
 npm install --production	# 4 minutes
@@ -6,12 +7,12 @@ cd nodes
 git clone https://github.com/node-red/node-red-nodes.git # 2 seconds
 
 # To run
-cd node-red
-node red.js
+# cd ~/node-red
+# node red.js
 
 # More modules, 
 # From https://learn.adafruit.com/raspberry-pi-hosting-node-red/installing-further-plugins
-npm install badwords ntwitter oauth sentiment wordpos xml2js firmata fs.notify serialport feedparser pushbullet irc simple-xmpp redis mongodb
+npm install -g badwords ntwitter oauth sentiment wordpos xml2js firmata fs.notify serialport feedparser pushbullet irc simple-xmpp redis mongodb
 
 # To use sensorTag
 # From: https://github.com/sandeepmistry/node-sensortag
