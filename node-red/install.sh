f95c596f708be26c22cfe5843e957f59a98c2926
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-07-23 22:39:05 -0400

    Added npm packages that were missing, but something is causing it to fail

--- node-red/install.sh
+++ node-red/install.sh
@@ -12,8 +12,13 @@ git clone https://github.com/node-red/node-red-nodes.git # 2 seconds
 
 # More modules, 
 # From https://learn.adafruit.com/raspberry-pi-hosting-node-red/installing-further-plugins
-npm install -g badwords ntwitter oauth sentiment wordpos xml2js firmata fs.notify serialport feedparser pushbullet irc simple-xmpp redis mongodb
+npm install -g badwords ntwitter oauth sentiment wordpos xml2js firmata fs.notify serialport feedparser pushbullet irc simple-xmpp redis mongodb node-stringprep
 
+npm install -g imap js2xmlparser nodemailer arduino-firmata heatmiser wemo 
+npm install -g stomp-client wake_on_lan node-dweetio komponist nma growl 
+npm install -g node-prowl pusher pushover-notifications snapchat twilio aws-sdk 
+npm install -g node-hue-api level mysql sqlite3 suncalc
+# Failed: node-blink1 blinkstick node-hid hidstream noble pg
 # To use sensorTag
 # From: https://github.com/sandeepmistry/node-sensortag
 apt-get install libbluetooth-dev
