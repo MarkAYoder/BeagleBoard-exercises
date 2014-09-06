67c3a15fd720541e7fd68f56a0302c9a954d8b28
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-09-06 16:50:53 -0400

    Added nodemailer

---
+++ email/nodemailer-test.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'Mark.A.Yoder@gmail.com',
+        pass: ''
+    }
+});
+
+// NB! No need to recreate the transporter object. You can use
+// the same transporter object for all e-mails
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: 'Mark A. Yoder ✔ <Mark.A.Yoder@Rose-Hulman.edu>', // sender address
+    to: 'yoder@rose-hulman.edu, Mark.A.Yoder@gmail.com', // list of receivers
+    subject: 'Test of nodemail ✔', // Subject line
+    text: 'Hello world ✔', // plaintext body
+    html: '<b>Hello world ✔</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Message sent: ' + info.response);
+    }
+});
