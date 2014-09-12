e866eb3e12ea0711853a27eea75d379fd9c533a1
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-09-12 17:40:04 -0400

    Each LED is individually controlled

--- gpio/fourLEDs.js
+++ gpio/fourLEDs.js
@@ -2,6 +2,7 @@
 var b = require('bonescript');
 var leds =    ["P9_11", "P9_13", "P9_15", "P9_17"];
 var buttons = ["P9_12", "P9_14", "P9_16", "P9_18"];
+var map = {"P9_12": "P9_11", "P9_14": "P9_13", "P9_16": "P9_15", "P9_18": "P9_17", };
 var state = b.HIGH;
 
 for(var i in leds) {
@@ -17,18 +18,16 @@ for(var i in leds) {
 }
 
 // setInterval(toggle, 1000);
-for(var i in leds) {
+for (var i in leds) {
+    console.log("buttons[" + i + "] = " + buttons[i]);
     b.attachInterrupt(buttons[i], true, b.CHANGE, toggle);
 }
 
 console.log("Ready to go");
 
-function toggle(led) {
-    state = ~state;
-    for(var i in leds) {
-        b.digitalWrite(leds[i], state);
+function toggle(x) {
+        state = ~state;
+        b.digitalWrite(map[x.pin.key], state);
+        console.log(x.pin.key);
+        console.log(map[x.pin.key]);
     }
-}
-
-
-// setInterval(blink, 250)
