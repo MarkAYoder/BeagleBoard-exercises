557068de8a64df8d24d27a993213817cfd272db2
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-09-12 17:16:58 -0400

    Every switch toggles all LEDs

--- fourLEDs.js
+++ fourLEDs.js
@@ -1,16 +1,34 @@
 #!/usr/bin/env node
 var b = require('bonescript');
-var LED = 'P9_11';
-var button = 'P9_12';
+var leds =    ["P9_11", "P9_13", "P9_15", "P9_17"];
+var buttons = ["P9_12", "P9_14", "P9_16", "P9_18"];
 var state = b.HIGH;
 
-b.pinMode(button, b.INPUT, 7, 'pulldown');
-b.pinMode(LED, b.OUTPUT);
-
-// b.attachInterrupt(button, true, b.CHANGE, blink);
-setInterval(blink, 250)
-
-function blink(){
-    b.digitalWrite(LED, state);
-    state = ~state;
+for(var i in leds) {
+    b.pinMode(leds[i], b.OUTPUT);
 }
+for(var i in buttons) {
+    b.pinMode(buttons[i], b.INPUT, 7, 'pulldown');
+}
+
+var state = b.LOW;
+for(var i in leds) {
+    b.digitalWrite(leds[i], state);
+}
+
+// setInterval(toggle, 1000);
+for(var i in leds) {
+    b.attachInterrupt(buttons[i], true, b.CHANGE, toggle);
+}
+
+console.log("Ready to go");
+
+function toggle(led) {
+    state = ~state;
+    for(var i in leds) {
+        b.digitalWrite(leds[i], state);
+    }
+}
+
+
+// setInterval(blink, 250)
