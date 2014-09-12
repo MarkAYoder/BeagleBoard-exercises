c6538265972a7d649310d0ab0c885334242e508c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-09-12 17:53:35 -0400

    Removed state variable and use x.value

--- gpio/fourLEDs.js
+++ gpio/fourLEDs.js
@@ -3,7 +3,6 @@ var b = require('bonescript');
 var leds =    ["P9_11", "P9_13", "P9_15", "P9_17"];
 var buttons = ["P9_12", "P9_14", "P9_16", "P9_18"];
 var map = {"P9_12": "P9_11", "P9_14": "P9_13", "P9_16": "P9_15", "P9_18": "P9_17", };
-var state = b.HIGH;
 
 for(var i in leds) {
     b.pinMode(leds[i], b.OUTPUT);
@@ -12,12 +11,10 @@ for(var i in buttons) {
     b.pinMode(buttons[i], b.INPUT, 7, 'pulldown');
 }
 
-var state = b.LOW;
 for(var i in leds) {
-    b.digitalWrite(leds[i], state);
+    b.digitalWrite(leds[i], 0);
 }
 
-// setInterval(toggle, 1000);
 for (var i in leds) {
     console.log("buttons[" + i + "] = " + buttons[i]);
     b.attachInterrupt(buttons[i], true, b.CHANGE, toggle);
@@ -26,8 +23,7 @@ for (var i in leds) {
 console.log("Ready to go");
 
 function toggle(x) {
-        state = ~state;
-        b.digitalWrite(map[x.pin.key], state);
+        b.digitalWrite(map[x.pin.key], x.value);
         console.log(x.pin.key);
         console.log(map[x.pin.key]);
     }
