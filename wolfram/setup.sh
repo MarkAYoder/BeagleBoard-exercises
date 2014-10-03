31d58b5f87e8cbd90ffa61afd5928d462cf91288
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-10-03 14:53:42 -0400

    Added Wolfram Alpha example

---
+++ wolfram/setup.sh
@@ -0,0 +1,2 @@
+# Get from https://developer.wolframalpha.com/portal/myapps/
+export WOLFRAM_APPID=KGYX54-WXVKWUEGG
---
+++ wolfram/test.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+console.log(process.env.WOLFRAM_APPID);
+
+var wolfram = require('wolfram').createClient(process.env.WOLFRAM_APPID)
+
+wolfram.query("integrate 2x^3 + 3x^2", function(err, result) {
+  if(err) throw err
+  console.log("Result: %j", result)
+})
