4e9a21036ea7e8c043d1af6051522daa25206fda
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-10-04 19:44:42 -0400

    Added installCLoud.sh

---
+++ setup/installCloud.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Here are the things I install on cloud machines
+# --Mark
+# 4-Oct-2014
+set -e
+BONE=ec2
+BONE_NAME=yoder-aws-cloud
+
+ssh $BONE "
+
+# Set up github
+git config --global user.name \"Mark A. Yoder\"
+git config --global user.email Mark.A.Yoder@Rose-Hulman.edu
+git config --global color.ui true
+
+# Get Beagle things
+git clone git@github.com:MarkAYoder/BeagleBoard-exercises.git exercises
+
+# Copy the .bashrc and .x11vncrc files from github so bash and x11vnc will use them
+ln -s --backup=numbered exercises/setup/bashrc .bashrc
+
+"
