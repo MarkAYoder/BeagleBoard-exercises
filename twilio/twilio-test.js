e18fd43bd9bc96c119e78de5103d8d27a1045960
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-10-04 22:03:09 -0400

    Adding command line input

--- twilio/twilio-test.js
+++ twilio/twilio-test.js
@@ -5,6 +5,13 @@
 // Twilio Credentials 
 var accountSid = 'AC407ab27aab63fa995dbc24c43a18d204'; 
 var authToken = '99e44f31bc8c7981c4ae6d6cf9c65ed'; 
+
+// The message
+var message="This is a test message.";
+
+console.log(process.argv);
+//if(process.argv.length > 2) 
+//process.exit(0);
  
 //require the Twilio module and create a REST client 
 var client = require('twilio')(accountSid, authToken); 
@@ -12,7 +19,7 @@ var client = require('twilio')(accountSid, authToken);
 client.messages.create({ 
 	to: "8122333219", 
 	from: "+18122333826", 
-	body: "This is a test for my class",   
+	body: message,
 }, function(err, message) { 
 	console.log(message.sid); 
 });
