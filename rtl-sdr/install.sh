a5c7f85c591f5c571a3ed6c79bb0d3eeecd5bf6a
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-10-23 19:37:19 -0400

    Added commands for tracking aircraft and listening to fm

--- rtl-sdr/install.sh
+++ rtl-sdr/install.sh
@@ -12,3 +12,7 @@ git clone https://github.com/MalcolmRobb/dump1090 MR-dump1090
 cd MR-dump1090
 make
 ./dump1090 --interactive --net --net-http-port 8081
+# Or on the bone
+# rtl_fm -f 103.7M -M wbfm -r 48000 - | netcat -l -p8082
+# On the host
+# nc bone 8082 | aplay -r 24k -f S16_LE
