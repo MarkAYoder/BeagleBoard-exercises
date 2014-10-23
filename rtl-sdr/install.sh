d092231f703602450238099c781aa504d9b62a96
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-10-23 18:38:30 -0400

    Added rtl-sdr

---
+++ rtl-sdr/install.sh
@@ -0,0 +1,14 @@
+# From: http://makezine.com/projects/tracking-planes-with-rtl-sdr/
+
+apt-get install cmake
+apt-get install libusb-1.0-0-dev
+git clone git://git.osmocom.org/rtl-sdr.git
+cd rtl-sdr
+cmake ./ -DINSTALL_UDEV_RULES=ON
+make
+make install
+cd ..
+git clone https://github.com/MalcolmRobb/dump1090 MR-dump1090
+cd MR-dump1090
+make
+./dump1090 --interactive --net --net-http-port 8081
