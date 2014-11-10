99d86a71187f5724d7701396742451fa390ec458
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-11-10 08:05:24 -0500

    Completely synchronous now

--- i2c/matrixLEDi2c.js
+++ i2c/matrixLEDi2c.js
@@ -8,7 +8,6 @@ var TMP102 = 0x70;
 var time = 1000000; // Delay between images in us
 
 // The first btye is GREEN, the second is RED.
-// The single color display responds only to the lower byte
 var smile =
 	[0x00, 0x3c, 0x00, 0x42, 0x28, 0x89, 0x04, 0x85, 
 	 0x04, 0x85, 0x28, 0x89, 0x00, 0x42, 0x00, 0x3c];
@@ -20,34 +19,26 @@ var neutral =
 	 0x89, 0x89, 0xa9, 0xa9, 0x42, 0x42, 0x3c, 0x3c];
 var blank = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-b.i2cOpen(port, TMP102, {}, onI2C);
+b.i2cOpen(port, TMP102);
 
-function onI2C(x) {
-    var bytes = [2, 7];
-    if (x.event == 'return') {
-        b.i2cWriteByte(port,  0x21); // Start oscillator (p10)
-    	b.i2cWriteByte(port,  0x81); // Disp on, blink off (p11)
-    	b.i2cWriteByte(port,  0xe7); // Full brightness (page 15)
+b.i2cWriteByte(port,  0x21); // Start oscillator (p10)
+b.i2cWriteByte(port,  0x81); // Disp on, blink off (p11)
+b.i2cWriteByte(port,  0xe7); // Full brightness (page 15)
 
-        b.i2cWriteBytes(port, 0x00, frown);
-        sleep.usleep(time);
-        
-        b.i2cWriteBytes(port, 0x00, neutral);
-        sleep.usleep(time);
-        
-        b.i2cWriteBytes(port, 0x00, smile);
-    // Fade the display
-    	var fade;
-    	for(fade = 0xef; fade >= 0xe0; fade--) {
-    //	    printf("writing: 0x%02x\n", fade);
-    	    b.i2cWriteByte(port,  fade);
-    	    sleep.usleep(100000);	// Sleep 0.1 seconds
-    	}
-    	for(fade = 0xe1; fade <= 0xef; fade++) {
-    //	    printf("writing: 0x%02x\n", fade);
-    	    b.i2cWriteByte(port,  fade);
-    	    sleep.usleep(100000);	// Sleep 0.1 seconds
-    	}
+b.i2cWriteBytes(port, 0x00, frown);
+sleep.usleep(time);
 
-    }
+b.i2cWriteBytes(port, 0x00, neutral);
+sleep.usleep(time);
+
+b.i2cWriteBytes(port, 0x00, smile);
+// Fade the display
+var fade;
+for(fade = 0xef; fade >= 0xe0; fade--) {
+    b.i2cWriteByte(port,  fade);
+    sleep.usleep(time/10);
+}
+for(fade = 0xe1; fade <= 0xef; fade++) {
+    b.i2cWriteByte(port,  fade);
+    sleep.usleep(time/10);
 }
