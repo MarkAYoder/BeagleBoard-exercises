b067549773dc15b1e6b607240f7d8b29b47e66ad
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-11-10 07:44:13 -0500

    Getting ready to add fading via sleep()

--- i2c/matrixLEDi2c.js
+++ i2c/matrixLEDi2c.js
@@ -2,12 +2,12 @@
 // npm install -g i2c
 
 var b = require('bonescript');
-// var i2c = require('i2c');
+var sleep = require('sleep');
 var port = '/dev/i2c-2'
 var TMP102 = 0x70;
-var time = 500; // Delay between images.
+var time = 1000; // Delay between images.
 
-// The first btye is RED, the second is GREEN.
+// The first btye is GREEN, the second is RED.
 // The single color display responds only to the lower byte
 var smile =
 	[0x00, 0x3c, 0x00, 0x42, 0x28, 0x89, 0x04, 0x85, 
@@ -26,10 +26,25 @@ function onI2C(x) {
     var bytes = [2, 7];
     if (x.event == 'return') {
         // b.i2cReadByte(port, onReadByte);
-        b.i2cWriteBytes(port, 0x00, neutral);
+        b.i2cWriteByte(port,  0x21); // Start oscillator (p10)
+    	b.i2cWriteByte(port,  0x81); // Disp on, blink off (p11)
+    	b.i2cWriteByte(port,  0xe7); // Full brightness (page 15)
+
+        b.i2cWriteBytes(port, 0x00, frown);
+        setTimeout(function() {
+            b.i2cWriteBytes(port, 0x00, neutral);
+        }, time);
         setTimeout(function() {
             b.i2cWriteBytes(port, 0x00, smile);
-        }, time);
+        }, 2*time);
+        // Fade the display
+    // 	int daddress;
+    // 	for(daddress = 0xef; daddress >= 0xe0; daddress--) {
+    // //	    printf("writing: 0x%02x\n", daddress);
+    // 	    res = i2c_smbus_write_byte(file, daddress);
+    // 	    usleep(100000);	// Sleep 0.1 seconds
+    // 	}
+
     }
 }
 
