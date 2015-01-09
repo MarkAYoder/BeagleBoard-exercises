230588140886fb60820081c18e53b619c6c75846
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-01-09 09:57:58 -0500

    Added grabber.c

---
+++ videoThru/install.h
@@ -0,0 +1,7 @@
+# From: http://www.egr.msu.edu/classes/ece480/capstone/fall13/group04/docs/briapp.pdf
+apt-get install v4l-utils
+git clone https://github.com/derekmolloy/boneCV.git
+apt-get install libv4l-dev
+gcc -lv4l2 -o grabber grabber.c
+apt-get install graphicsmagick
+
