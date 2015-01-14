7a0f266df58e327e1f6d0ff20bfe8a3128825acb
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-01-14 09:06:35 -0500

    Looking into LIRC for an IR remote

---
+++ ir/install.sh
@@ -0,0 +1,7 @@
+# From: https://thenetworkcable.wordpress.com/2014/10/18/compiling-lirc_bbb-for-ir-control-on-a-beagle-bone-black/
+
+apt-get install lirc
+# apt-get install build-essential fakeroot
+# apt-get install bc ncurses-dev
+
+# http://www.instructables.com/id/Raspberry-Pi-Universal-Remote/
