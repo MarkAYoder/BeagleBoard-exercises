b37ce4905337a36b75ad697378f79f6668733e59
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-03-02 03:08:42 +0530

    Added esp8266 installation instructions

---
+++ esp8266/install.sh
@@ -0,0 +1,30 @@
+# From: https://github.com/esp8266/Arduino/issues/208
+# Scroll 75% the way down to instructions on changing platform.txt
+
+wget https://raw.githubusercontent.com/themadinventor/esptool/master/esptool.py
+dir=$PWD
+cd /usr/local/bin
+ln -s $dir/esptool.py .
+cd $dir
+
+# edit .arduino15/packages/esp8266/hardware/esp8266/1.6.5-947-g39819f0/platform.txt
+
+tools.esptool.cmd=esptool
+tools.esptool.cmd.windows=esptool.exe
+tools.esptool.path={runtime.ide.path}/hardware/tools/esp8266
+
+tools.esptool.upload.protocol=esp
+tools.esptool.upload.params.verbose=-vv
+tools.esptool.upload.params.quiet=
+tools.esptool.upload.pattern="{path}/{cmd}" {upload.verbose} -cd {upload.resetmethod} -cb {upload.speed} -cp "{serial.port}" -ca 0x00000 -cf "{build.path}/{build.project_name}_00000.bin" -ca 0x10000 -cf "{build.path}/{build.project_name}_10000.bin"
+
+to
+
+tools.esptool.cmd=esptool.py
+tools.esptool.cmd.windows=esptool.exe
+tools.esptool.path=/usr/local/bin/
+
+tools.esptool.upload.protocol=esp
+tools.esptool.upload.params.verbose=-vv
+tools.esptool.upload.params.quiet=
+tools.esptool.upload.pattern="{path}/{cmd}" -b {upload.speed} -p "{serial.port}" write_flash 0x00000  "{build.path}/{build.project_name}_00000.bin" 0x10000 "{build.path}/{build.project_name}_10000.bin"
