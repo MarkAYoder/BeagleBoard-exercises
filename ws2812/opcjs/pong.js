546c077a58e087ad8fd8ae1c52b608972aeb3711
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-03-02 06:17:22 +0530

    Added comments

--- ws2812/opcjs/pong.js
+++ ws2812/opcjs/pong.js
@@ -3,71 +3,61 @@
 var opc_client = require('open-pixel-control');
 var b = require('bonescript');
 
-// Size of grid
-var xlen = 10,
+var xlen = 10,    // Size of grid
     ylen = 10,
-    xpos = xlen/2,
-    ypos = ylen/2+1,
-    xdir = 1,
+    xdir = 1,     // Direction ball is traveling
     ydir = -1,
-    color = [127, 0, 0],
-    bgnd = [0, 0, 0],
-    grid = new Array(xlen*ylen),
-    ms   = 300,
-    pin  = 'P9_42';
+    color = [127, 0, 0],  // Color of ball (RGB)
+    bgnd = [0, 0, 0],     // Color of background
+    grid = new Array(xlen*ylen),  // Playing field
+    ms   = 100,   // Time (in ms) between moves
+    pin  = 'P9_42';   // Pin with the reverse button
+    zone = 2;     // Size of zone where reverse will occur
     
-var x = 0,
+var x = 0,        // Current location
     y = ylen/2;
     
-b.pinMode(pin, b.INPUT);
+b.pinMode(pin, b.INPUT);    // Set pin to be INPUT
 
-b.attachInterrupt(pin, true, b.RISING, reverse);
+b.attachInterrupt(pin, true, b.RISING, reverse);  // Call reverse() whenever button is pressed
 
 function reverse() {
-  console.log("reverse");;;;;;;;
-  
-  if(x<2 || x>xlen-2) {
+  if(x<zone || x>xlen-zone-1) {   // If in the zone, reverse
   xdir = -xdir;
   }
 }
 
-// Set up grid of LEDs
+// Initialize the grid of LEDs
 for(var i=0; i<grid.length; i++) {
   grid[i] = bgnd;
 }
 
+// Connect to opc client
 var client = new opc_client({
   address: 'localhost',
   port: 7890
 });
 
-client.on('connected', function(){
+client.on('connected', function() {   // Do this connected
   console.log("Connected");
   var strip = client.add_strip({
     length: xlen*ylen
   });
 
-  client.put_pixels(strip.id, grid);
-  
-  movePixel();
-  
-  function movePixel() {
-    // client.put_pixel(strip.id, x*xlen+y, bgnd);
-    grid[x*xlen+y] = bgnd;
-    x += xdir;
-    if(x<0 || x>=xlen) {
-      // xdir = -xdir;
-      // x += xdir;
-      x = xlen/2;
+// Here's how you move the ball
+  function moveBall() {
+    grid[x*xlen+y] = bgnd;    // Turn off ball in present location
+    x += xdir;                // Move in the current direction
+    if(x<0 || x>xlen-1) {     // See if you have fallen off either edge
+      x = xlen/2;             // If so, restart in middle
     }
-    // client.put_pixel(strip.id, x*xlen+y, color);
-    grid[x*xlen+y] = color;
-    client.put_pixels(strip.id, grid);
-    setTimeout(movePixel, ms);
+    grid[x*xlen+y] = color;   // Draw ball in new location
+    client.put_pixels(strip.id, grid);  // Display the grid
+    setTimeout(moveBall, ms); // Schedule the next ball move
   }
   
-  // client.disconnect();
-
+  moveBall(); // Start the ball moving
+  
 });
 
-client.connect();
+client.connect(); // Connect to the OPC server and start things moving
