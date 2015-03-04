e16909822921ac06c9f7d3ed17081137f429a275
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-03-04 07:46:48 +0530

    Added plotlyjs, a locally running version of plotly

--- plotly/install.sh
+++ plotly/install.sh
@@ -1,2 +1,9 @@
-# https://plot.ly/nodejs/getting-started/
-npm install plotly
+# This uses the free BASIC version of plotlyjs which runs locally.
+# From https://plot.ly/javascript/
+wget http://d14fo0winaifog.cloudfront.net/plotlyjs_basic.zip
+unzip plotlyjs_basic.zip
+
+# Link it where the browser can see it 
+here=$PWD
+cd /var/lib/cloud9
+ln -s $here .
