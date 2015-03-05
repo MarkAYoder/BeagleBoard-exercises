7fc9e98c4bef23ca5e8f1710432eb1ec63af3c13
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-03-05 10:55:47 +0530

    Cleaned up comments.  Fixed annotation

--- plotly/install.sh
+++ plotly/install.sh
@@ -1,7 +1,9 @@
 # This uses the free BASIC version of plotlyjs which runs locally.
 # From https://plot.ly/javascript/
 wget http://d14fo0winaifog.cloudfront.net/plotlyjs_basic.zip
-unzip plotlyjs_basic.zip
+unzip plotlyjs_basic.
+
+ln -s plotly_20150812a_basic weather    # Make path easier to remember
 
 # Link it where the browser can see it 
 here=$PWD
