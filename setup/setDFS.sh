f93676b6d712d8f03f746af5210b82387387d8e9
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-03-05 17:54:39 -0500

    Added /etc/fuse.conf instruction

--- setup/setDFS.sh
+++ setup/setDFS.sh
@@ -1,7 +1,9 @@
 # Mounts dfs files
 # sudo apt-get install sshfs
+# Uncomment allow_other in /etc/fuse.conf
 REMOTEUSER=yoder
 NAME=/mnt/dfs
+echo $USER
 if [ "$USER" != "root" ]
 then
     sudo mkdir -p $NAME
@@ -11,5 +13,5 @@ else    # On the Bone
     chown $USER:$USER $NAME
 fi
 chmod 777 $NAME
-sshfs $REMOTEUSER@dfs.rose-hulman.edu:/DFSRoot/MyDocs/$REMOTEUSER $NAME
+sshfs $REMOTEUSER@dfs.rose-hulman.edu:/DFSRoot/MyDocs/$REMOTEUSER $NAME -o allow_other
 echo "$NAME is mounted"
