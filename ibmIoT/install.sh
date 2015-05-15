7a94f33ecbfc6c589e087302667b2b2f2a85d547
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-05-15 13:13:42 -0400

    Added notes on used mqtt to intersept data

--- ibmIoT/install.sh
+++ ibmIoT/install.sh
@@ -1,1 +1,9 @@
+# From: https://developer.ibm.com/iot/recipes/ti-beaglebone-sensortag/
 git clone http://github.com/ibm-messaging/iot-beaglebone
+
+# Edit iot_sensortag.js and change:
+# var qs_host = "quickstart.messaging.internetofthings.ibmcloud.com";
+# // var qs_host = "192.168.7.2";
+
+# Then listen on
+# var pub_topic = "iot-2/evt/sample/fmt/json";
