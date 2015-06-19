c75296d17c6bf197023cb16cc77ad24ef48cf4e7
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-06-19 15:27:22 -0400

    Added parameters

--- ifttt/test.js
+++ ifttt/test.js
@@ -3,18 +3,17 @@ var request = require('request');
 var qs = require('querystring');
 var event = 'my_test';
 var key = 'yycMlao6V0SbGCk2iAhl9';
-var string = {Beagle: "Bone", value1: 'My', value2: 'Test'};
+var string = {value1: 'My', value2: 'Test', value3: 'BeagleBone'};
 
 var url = 'https://maker.ifttt.com/trigger/' + event + '/with/key/' + key + 
             '?' + qs.stringify(string);
-
-console.log(string);
-console.log(qs.stringify(string));
 
 console.log(url);
 
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body); // Show the HTML for the Google homepage. 
+    console.log(body); 
+  } else {
+    console.log("error=" + error + " response=" + JSON.stringify(response));
   }
 });
