f2116bfd0503800e152c16abad6ac355deaeb669
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-07-28 13:30:11 +0530

    Update proxy.sh to set global proxy via dconf

--- setup/proxy.sh
+++ setup/proxy.sh
@@ -1,2 +1,14 @@
-git config --global http.proxy http://10.8.0.1:8080
-git config --global https.proxy https://10.8.0.1:8080
+# Set proxy for IIT Mandi
+for proxy in ftp http https socks 
+do
+    echo $proxy 
+    dconf write /system/proxy/$proxy/host "'10.8.0.1'"
+    dconf write /system/proxy/$proxy/port "'8080'"
+done
+
+# I don't know if this is needed on the Bone, but it's needed on the host to reach the Bone
+dconf write /system/proxy/ignore-hosts "['localhost', '127.0.0.0/8', '192.168.7.0/8']"
+dconf dump /system/proxy/  
+
+# git config --global http.proxy http://10.8.0.1:8080
+# git config --global https.proxy https://10.8.0.1:8080
