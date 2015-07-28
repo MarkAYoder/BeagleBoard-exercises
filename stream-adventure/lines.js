74d86d58ed918962e3f97ca15b65c5040fb1c55e
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-07-28 14:38:22 +0530

    Cleaned up code

--- stream-adventure/lines.js
+++ stream-adventure/lines.js
@@ -2,13 +2,14 @@
 
 var fs=require('fs');
 var through = require('through2');
+var split = require('split');
+
 var count = 0;
 
-var split = require('split');
 process.stdin
     .pipe(split())
     .pipe(through(function(line, _, next) {
-        if(count++%2){
+        if(count++%2 === 0){
             this.push(line.toString().toUpperCase() + '\n');
         } else {
             this.push(line.toString().toLowerCase() + '\n');
