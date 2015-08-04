a83af392b66b2127eb6bd2fbe6bb326f1f73efa6
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-08-04 19:37:20 +0530

    Added wget's for jquery and jsapi

--- phant/install.sh
+++ phant/install.sh
@@ -3,10 +3,11 @@
 npm install -g phant
 phant &
 # Browse to http://14.139.34.32:8080/ and click "Create" and fill in the fields
-# Record the public/private/delete keys
-export phant_PUBLIC=8BqVYqVlerhpE2m1zBaXh0L8Nvz
-export phant_PRIVATE=ylDNWDNO7yFGDwORErVjCN84lmz
-export phant_DELETE=apqOGqOoB7sYRNPypWz3hRLYzAP
+# Save the keys as json.
+
+# Load jquery and jsapi
+wget https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
+wget https://www.google.com/jsapi
 
 # Log some data
 wget http://14.139.34.32:8080/input/$phant_PUBLIC?private_key=$ylDNWDNO7yFGDwORErVjCN84lmz\&amplitude=23.29\&timestamp=25.94
