9f027395157bf49b7bbdc3e4a0336f011cecb7e2
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-08-04 17:17:32 +0530

    Now pinging and logging one time

--- phant/recordPing.js
+++ phant/recordPing.js
@@ -8,26 +8,26 @@ var phant_PUBLIC  = "4X9aed2JXgUlEV1m1bBvu0JBmY0";
 var phant_PRIVATE = "qmLBjGrEmXCl3M7x7XPBc78zLR7";
 var phant_DELETE  = "yqvelNGxqPhPv8g6gkOwcVNa2rV";
 var site = "http://14.139.34.32:8080";
-var url = site + "/input/" + phant_PUBLIC + "?private_key=" + phant_PRIVATE + "&time=1234";
+var url = site + "/input/" + phant_PUBLIC + "?private_key=" + phant_PRIVATE + "&time=";
 
 
 child_process.exec(ping,
     function (error, stdout, stderr) {
         console.log('ping: ' + stdout);
-        var test = [0];
+        var time = [0];
         if(error || stderr) { 
             console.log('error: ' + error); 
             console.log('stderr: ' + stderr); 
         } else {    
-            test = stdout.match(/time=[0-9.]* /mg);
-            for(var i =0; i<test.length; i++) {
-                test[i] = test[i].substring(5);     // Strip off the leading time=
+            time = stdout.match(/time=[0-9.]* /mg);
+            for(var i =0; i<time.length; i++) {
+                time[i] = time[i].substring(5);     // Strip off the leading time=
             } 
         }
         
-        console.log("test: " + test);
+        console.log("time: " + time);
         console.log("url: " + url);
-        request(url, function (error, response, body) {
+        request(url+time[0], function (error, response, body) {
           if (!error && response.statusCode == 200) {
             console.log(body); 
           } else {
