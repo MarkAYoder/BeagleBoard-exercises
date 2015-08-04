a8ce6d731446a88a38ac77292699a4c632254cae
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-08-04 17:53:29 +0530

    Adding plots

--- phant/recordPing.js
+++ phant/recordPing.js
@@ -1,32 +1,40 @@
 #!/usr/bin/env node
+// Measure the time to send a ping and log it on phant
+// Go to http://14.139.34.32:8080/streams/make and create a new stream
+// Save the keys json file and copy to keys.json
+// Run this script
 
 var child_process = require('child_process');
-var request = require('request');
+var request       = require('request');
+var util          = require('util');
+var fs            = require('fs');
 
-var ping = "ping -c3 -i0.25 rose-hulman.edu";
-var phant_PUBLIC  = "4X9aed2JXgUlEV1m1bBvu0JBmY0";
-var phant_PRIVATE = "qmLBjGrEmXCl3M7x7XPBc78zLR7";
-var phant_DELETE  = "yqvelNGxqPhPv8g6gkOwcVNa2rV";
-var site = "http://14.139.34.32:8080";
-var url = site + "/input/" + phant_PUBLIC + "?private_key=" + phant_PRIVATE + "&time=";
+var ping = "ping -c1 -i0.25 rose-hulman.edu";
+var filename = "keys.json";
+var keys = JSON.parse(fs.readFileSync(filename));
+// console.log(util.inspect(keys));
 
+var url = keys.inputUrl + "/?private_key=" + keys.privateKey + "&time=";
 
+// Send off the ping command.
 child_process.exec(ping,
     function (error, stdout, stderr) {
-        console.log('ping: ' + stdout);
-        var time = [0];
+        // console.log('ping: ' + stdout);
+        var time = [0];             // Report time=0 if an error.
         if(error || stderr) { 
             console.log('error: ' + error); 
             console.log('stderr: ' + stderr); 
         } else {    
-            time = stdout.match(/time=[0-9.]* /mg);
+            time = stdout.match(/time=[0-9.]* /mg); //  Pull the time out of the return string
             for(var i =0; i<time.length; i++) {
                 time[i] = time[i].substring(5);     // Strip off the leading time=
             } 
         }
         
-        console.log("time: " + time);
-        console.log("url: " + url);
+        // console.log("time: " + time);
+        console.log("url: " + url+time[0]);
+        
+        // Send the time to the phant server.
         request(url+time[0], function (error, response, body) {
           if (!error && response.statusCode == 200) {
             console.log(body); 
