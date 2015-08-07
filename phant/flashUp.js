5c739a56d25b96e1a3eb2238839e1bad84998194
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-08-07 18:50:53 +0530

    Turns on RED LED when no network and GREEN when there is network

--- phant/flashUp.js
+++ phant/flashUp.js
@@ -5,21 +5,21 @@
 var child = require('child_process');
 var b = require('bonescript');
 
+var pingCmd = "ping -w1 google.com";
 var ms = 1500;  // Repeat time.
 var red   = 'P9_11';
-var green = 'P9_13';
-var blue  = 'P9_15';
+var green = 'P9_15';
+var blue  = 'P9_13';
 
 b.pinMode(red,   b.OUTPUT);
 b.pinMode(green, b.OUTPUT);
 b.pinMode(blue,  b.OUTPUT);
 
-b.digitalWrite(red,   1);
-b.digitalWrite(greeb, 0);
+b.digitalWrite(red,   0);
+b.digitalWrite(green, 0);
 b.digitalWrite(blue,  0);
 
 
-var pingCmd = "ping -w1 google.com";
 // console.log("process.argv.length: " + process.argv.length);
 if(process.argv.length === 3) {
     pingCmd = process.argv[2];
@@ -34,8 +34,12 @@ function ping  () {
             if(error || stderr) { 
                 console.log('error: ' + error); 
                 console.log('stderr: ' + stderr); 
+                b.digitalWrite(red,   1);
+                b.digitalWrite(green, 0);
             } else {
                 console.log('ping: ' + stdout);
+                b.digitalWrite(red,   0);
+                b.digitalWrite(green, 1);
             }
         }
     )
