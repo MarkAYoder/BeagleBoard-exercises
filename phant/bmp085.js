5a26b89a13f81defc8d9d923955bf3f83b4436a5
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-08-18 13:35:32 +0530

    Added wiring instructions

--- phant/bmp085.js
+++ phant/bmp085.js
@@ -2,6 +2,7 @@
 // Read BMP085 temperature and pressure
 // https://www.npmjs.com/package/bmp085
 // npm install -g bmp085
+// Wire SCL to P9_20 and SDA to P9_19
 
 var BMP085 = require('bmp085'),
     barometer = new BMP085();
