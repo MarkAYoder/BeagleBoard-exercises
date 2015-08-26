e4898b9e09ffdc3ea490a3599510cd52f3038a7f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-08-26 14:45:26 +0530

    Simple example to read analong and button and control LED via web page

---
+++ jsfiddle/jsfiddleDemoI.js
@@ -0,0 +1,49 @@
+// This uses the BeagleBone's remote procecdure call (RPC) to poll an
+// analog input and attache an interrupt to a switch.  
+// From: http://jsfiddle.net/2fnj3wLg/
+
+setTargetAddress('192.168.7.2', {
+    initialized: run
+});
+
+function run() {
+    b = require('bonescript');
+    var SLIDER = 'P9_36';
+    var BUTTON = 'P9_42';
+    var ms     = 200;           // Polling interval in ms
+    b.pinMode(BUTTON, b.INPUT);
+
+     setInterval(getSliderStatus, ms);
+     b.detachInterrupt(BUTTON);         // The detaches the interrupt from the previous run
+     b.attachInterrupt(BUTTON, true, b.CHANGE, getButtonStatus);
+
+    function getSliderStatus() {
+        b.analogRead(SLIDER, onSliderRead);     // <5>
+    }
+
+    function onSliderRead(x) {
+        if (!x.err) {                           // <6>
+            $('#sliderStatus').html(x.value.toFixed(3));
+        }
+    }
+
+    function getButtonStatus() {
+        b.digitalRead(BUTTON, onButtonRead);    // <8>
+    }
+
+    function onButtonRead(x) {
+        if (!x.err) {                           // <9>
+            $('#buttonStatus').html(x.value);
+        }
+        setTimeout(getSliderStatus, 200);        // <10>
+    }
+}
+
+var LED = 'P9_14';
+var toggle = 1;
+
+function led(x) {
+    console.log("led called with: %d", x);
+    b.digitalWrite(LED, toggle);
+    toggle = !toggle;
+}
