080b57589e0700bab1491647242a938dff8c95be
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-08-28 16:25:37 +0530

    Added slider to control fading of LED

--- jsfiddle/jsfiddleDemoI.js
+++ jsfiddle/jsfiddleDemoI.js
@@ -8,7 +8,8 @@ setTargetAddress('192.168.7.2', {
 
 var SLIDER = 'P9_36';
 var BUTTON = 'P9_42';
-var LED   = 'P9_14';
+var LED    = 'P9_14';
+var FADE   = 'P9_16';
 var toggle = true;          // State of LED
 
 var ms     = 200;           // Polling interval in ms
@@ -19,32 +20,48 @@ function led(x) {
     toggle = !toggle;
 }
 
+function fade(x) {
+    console.log("fade: %s", JSON.stringify(x))
+    b.analogWrite(FADE, x);
+}
+
 function run() {
     b = require('bonescript');
     b.pinMode(BUTTON, b.INPUT );
     b.pinMode(LED,    b.OUTPUT);
+    b.pinMode(FADE,   b.OUTPUT);
+    
+    initFade = 0.5;
+    b.analogWrite(FADE, initFade);
 
     setInterval(getSliderStatus, ms);
     b.detachInterrupt(BUTTON);         // The detaches the interrupt from the previous run
     b.attachInterrupt(BUTTON, true, b.CHANGE, getButtonStatus);
 
     function getSliderStatus() {
-        b.analogRead(SLIDER, onSliderRead);     // <5>
+        b.analogRead(SLIDER, onSliderRead);
     }
 
     function onSliderRead(x) {
-        if (!x.err) {                           // <6>
+        if (!x.err) {
             $('#sliderStatus').html(x.value.toFixed(3));
         }
     }
 
     function getButtonStatus() {
-        b.digitalRead(BUTTON, onButtonRead);    // <8>
+        b.digitalRead(BUTTON, onButtonRead);
     }
 
     function onButtonRead(x) {
-        if (!x.err) {                           // <9>
+        if (!x.err) {
             $('#buttonStatus').html(x.value);
         }
     }
+
+    $(function () {
+        $("#slider1").slider({min:0, max:100, value: 100*initFade, slide: function(event, ui) {
+    	   // console.log("slider: %d",  ui.value);
+    	    fade(ui.value/100);
+        }});
+    });
 }
