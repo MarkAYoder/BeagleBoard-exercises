58dff20099896040e492b38dbae5c8734127b950
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-08-31 11:01:59 +0530

    Using setTimeout to sequence images

--- i2c/matrixLEDi2c.js
+++ i2c/matrixLEDi2c.js
@@ -2,10 +2,10 @@
 // npm install -g sleep
 
 var b = require('bonescript');
-var sleep = require('sleep');
+// var sleep = require('sleep');
 var port = '/dev/i2c-2'
-var TMP102 = 0x70;
-var time = 1000000; // Delay between images in us
+var matrix = 0x70;
+var time = 1000; // Delay between images in ms
 
 // The first btye is GREEN, the second is RED.
 var smile =
@@ -19,26 +19,35 @@ var neutral =
 	 0x89, 0x89, 0xa9, 0xa9, 0x42, 0x42, 0x3c, 0x3c];
 var blank = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-b.i2cOpen(port, TMP102);
+b.i2cOpen(port, matrix);
 
 b.i2cWriteByte(port,  0x21); // Start oscillator (p10)
 b.i2cWriteByte(port,  0x81); // Disp on, blink off (p11)
 b.i2cWriteByte(port,  0xe7); // Full brightness (page 15)
 
-b.i2cWriteBytes(port, 0x00, frown);
-sleep.usleep(time);
+doFrown();
+function doFrown() {
+	b.i2cWriteBytes(port, 0x00, frown);
+}
+// sleep.usleep(time);
 
-b.i2cWriteBytes(port, 0x00, neutral);
-sleep.usleep(time);
+setTimeout(doNeutral, 1*time);
+function doNeutral() {
+	b.i2cWriteBytes(port, 0x00, neutral);
+}
+// sleep.usleep(time);
 
-b.i2cWriteBytes(port, 0x00, smile);
+setTimeout(doSmile, 2*time);
+function doSmile() {
+	b.i2cWriteBytes(port, 0x00, smile);
+}
 // Fade the display
 var fade;
 for(fade = 0xef; fade >= 0xe0; fade--) {
     b.i2cWriteByte(port,  fade);
-    sleep.usleep(time/10);
+    // sleep.usleep(time/10);
 }
 for(fade = 0xe1; fade <= 0xef; fade++) {
     b.i2cWriteByte(port,  fade);
-    sleep.usleep(time/10);
+    // sleep.usleep(time/10);
 }
