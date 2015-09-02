26013978705ab8a0954b21c2a99e232d9013d721
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-09-02 10:54:29 +0530

    Moved / from path to filename.  Cleaned up console.log

--- eQEP/rotaryEncoder.js
+++ eQEP/rotaryEncoder.js
@@ -7,22 +7,22 @@
 var b = require('bonescript'),
     fs = require('fs');
     
-var eQEP0 = "/sys/devices/ocp.3/48300000.epwmss/48300180.eqep/",
-    eQEP1 = "/sys/devices/ocp.3/48302000.epwmss/48302180.eqep/",
-    eQEP2 = "/sys/devices/ocp.3/48304000.epwmss/48304180.eqep/",
+var eQEP0 = "/sys/devices/ocp.3/48300000.epwmss/48300180.eqep",
+    eQEP1 = "/sys/devices/ocp.3/48302000.epwmss/48302180.eqep",
+    eQEP2 = "/sys/devices/ocp.3/48304000.epwmss/48304180.eqep",
     eQEP = eQEP2;
 	
 var oldData,			// pervious data read
     period = 100;	// in ms
 
 // Set the eEQP period, convert to ns.
-fs.writeFile(eQEP+'period', period*1000000, function(err) {
+fs.writeFile(eQEP+'/period', period*1000000, function(err) {
 	if (err) throw err;
 	console.log('Period updated to ' + period*1000000);
 })
 
 // Enable
-fs.writeFile(eQEP+'enabled', 1, function(err) {
+fs.writeFile(eQEP+'/enabled', 1, function(err) {
 	if (err) throw err;
 	console.log('Enabled');
 })
@@ -30,13 +30,13 @@ fs.writeFile(eQEP+'enabled', 1, function(err) {
 setInterval(readEncoder, period);    // Check state every 250 ms
 
 function readEncoder(x) {
-	fs.readFile(eQEP + 'position', {encoding: 'utf8'}, printValue);
+	fs.readFile(eQEP + '/position', {encoding: 'utf8'}, printValue);
 }
 
 function printValue(err, data) {
 	if (err) throw err;
 	if (oldData !== data) {
-		console.log('position: '+data+' speed: '+(oldData-data));
+		console.log('position: %d, speed: %d', data, oldData-data);
 		oldData = data;
 	}
 }
