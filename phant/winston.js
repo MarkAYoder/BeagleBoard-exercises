ab7a4085edf232f52ccc0bc50632e5f6f3e47452
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-09-06 03:04:12 +0530

    More playing

--- phant/winston.js
+++ phant/winston.js
@@ -2,7 +2,11 @@
 // Testing winston for logging
 var winston = require('winston');
 
-winston.add(winston.transports.File, { filename: 'somefile.log' });
+winston.add(winston.transports.File, { 
+    filename: 'somefile.log',
+    handleExceptions: true,
+    exitOnError: false
+});
 
 winston.log('info', 'Hello distributed log files!');
 winston.info('Hello again distributed logs');
