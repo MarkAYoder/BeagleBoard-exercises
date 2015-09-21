11ba8023bb77607b5c44264ae70b60041753878f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-09-21 16:49:40 +0530

    Adding PRU materials

---
+++ pru/install.sh
@@ -0,0 +1,8 @@
+# Get reference manuals
+# From Exploring BeagleBone Black
+# AM335x PRU-ICSS Reference Guide
+wget https://github.com/beagleboard/am335x_pru_package/raw/master/am335xPruReferenceGuide.pdf
+
+# TI PRU Wiki:  tiny.cc/ebb1308
+# PRU Linux Application Loader API Guide:   tiny.cc/ebb1309
+# PRU Debugger User Guide:  tiny.cc/ebb1310
