008f245db589db960512dd285fb8d173980e9cce
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-09-23 08:49:07 +0530

    Added more resources

--- pru/install.sh
+++ pru/install.sh
@@ -6,3 +6,11 @@ wget https://github.com/beagleboard/am335x_pru_package/raw/master/am335xPruRefer
 # TI PRU Wiki:  tiny.cc/ebb1308
 # PRU Linux Application Loader API Guide:   tiny.cc/ebb1309
 # PRU Debugger User Guide:  tiny.cc/ebb1310
+
+# From http://exploringbeaglebone.com/chapter13
+wget http://exploringbeaglebone.com/wp-content/uploads/2014/12/Instruction-Set-Sheet.pdf
+
+# uio_pruss.c  Used to set RAM buffer size
+wget http://lxr.free-electrons.com/source/drivers/uio/uio_pruss.c
+
+# PRU C compiler: tiny.cc/ebb1311
