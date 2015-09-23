b2cd03f59b2ae77ad3389d48743ab22725fc3ddc
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-09-23 09:15:34 +0530

    Added latest weather status

--- plotly/install.sh
+++ plotly/install.sh
@@ -1,7 +1,7 @@
 # This uses the free BASIC version of plotlyjs which runs locally.
 # From https://plot.ly/javascript/
 wget http://d14fo0winaifog.cloudfront.net/plotlyjs_basic.zip
-unzip plotlyjs_basic.
+unzip plotlyjs_basic.zip
 
 ln -s plotly_20150812a_basic weather    # Make path easier to remember
 
