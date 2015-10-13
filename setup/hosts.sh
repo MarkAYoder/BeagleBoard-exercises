ef1a61a4688faeb15f91db68d8f15407ca40ee9b
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-10-13 16:00:59 +0530

    Added install.apt-get and hosts.sh

---
+++ setup/hosts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Updates /etc/hosts
+cat - << EOF >> /etc/hosts
+
+# Mandi hosts
+192.168.7.1     host
+192.168.7.2     bone
+10.8.7.185      bone2
+14.139.34.32    think
+137.112.41.36   office
+
+EOF
