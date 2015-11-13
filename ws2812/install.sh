ecb8297a8cefc5a22a2e972f9968c2ad1ff9dc77
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-11-13 15:35:13 +0530

    Added details to install.sh

--- ws2812/install.sh
+++ ws2812/install.sh
@@ -11,7 +11,7 @@ node pinmap.js
 # I see channel 0 on P9_22
 # Start the opc_server
 cd ..
-./opc_server &
+./opc_server --config ws281x-may.json &
 
 # Start a pattern
 cd openpixelcontrol/python_clients
@@ -22,3 +22,16 @@ wget https://raw.githubusercontent.com/mens-amplio/mens-amplio/master/modeling/o
 
 # js version
 npm install -g open-pixel-control
+
+# Adafruit library,, not used at this time.q
+# https://learn.adafruit.com/adafruit-neopixel-uberguide
+# https://learn.adafruit.com/adafruit-neopixel-uberguide/neomatrix-library
+wget https://github.com/adafruit/Adafruit_NeoMatrix/archive/master.zip
+unzip master.zip
+rm master.zip
+wget https://github.com/adafruit/Adafruit-GFX-Library/archive/master.zip
+unzip master.zip
+rm master.zip
+
+wget https://raw.githubusercontent.com/arduino/Arduino/master/hardware/arduino/avr/cores/arduino/Arduino.h
+wget https://raw.githubusercontent.com/arduino/Arduino/master/hardware/arduino/sam/cores/arduino/Arduino.h
