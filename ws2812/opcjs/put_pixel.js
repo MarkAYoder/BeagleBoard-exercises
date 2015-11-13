2e772395aac29eb29594f47df80cd7a9a5cb322d
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-11-13 15:29:50 +0530

    Works for multiple panels

--- ws2812/opcjs/put_pixel.js
+++ ws2812/opcjs/put_pixel.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var opc_client = require('open-pixel-control');
+var util = require('util');
 
 var client = new opc_client({
   address: 'localhost',
@@ -8,18 +9,24 @@ var client = new opc_client({
 });
 
 client.on('connected', function() {
-  var strip = client.add_strip({
-    length: 100
-  });
+  var strip = 
+      client.add_strip({
+        length: 300
+      });
+    
+    // console.log("strip: " + util.inspect(strip));
 
   var i = 0, color = [127, 0, 127];
+
   setInterval(function(){
 
+    // console.log("i: " + i);
     client.put_pixel(strip.id, i, color);
+    // client.put_pixel(strip.id, i+100, color);
 
-    if(i == strip.length){
+    if(i == strip.length) {
       i = 0;
-      color = color[0] == 0 ? [255, 255, 255] : [0, 0, 0];
+      color = color[0] == 0 ? [127, 0, 127] : [0, 0, 0];
     } else {
       i++;
     }
