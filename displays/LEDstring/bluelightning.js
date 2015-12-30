4f6450e00c5a343c9657f2157e6e6c8742d6c56f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-12-30 17:30:36 +0000

    Works with async writes.  Runs 10 LEDs at once.

--- displays/LEDstring/bluelightning.js
+++ displays/LEDstring/bluelightning.js
@@ -4,27 +4,28 @@ var fs = require('fs');
 var util = require('util');
 var LEDs = "/sys/firmware/lpd8806/device/rgb";
 var LEDcount = process.env.STRING_LEN;
-var ms = 10;
+var ms = 50;
 
 console.log("LEDcount = %d", LEDcount);
 
 var fd = fs.openSync(LEDs, 'w');
 console.log("fd=%d", fd);
 
-for(var i = 0; i < LEDcount; i++) {
-    // fs.writeSync(fd, util.format("150 0 0 %d", i));
-    fs.write(fd, util.format("0 50 0 %d", i), 0, 'utf8', function(err, written, string) {
-        // console.log("written = %d", written);
-    });
+// for(var i = 0; i < LEDcount; i++) {
+//     // fs.writeSync(fd, util.format("150 0 0 %d", i));
+//     fs.write(fd, util.format("0 50 0 %d", i));
+// }
+
+for(var i = 0; i<100; i+=10) {
+    console.log("i=%d", i);
+    updateLED(i, 1, 0, LEDcount);
 }
 
-updateLED(0, 1, 0, LEDcount);
-
 function updateLED(pos, dir, start, stop) {
-    fs.write(fd, util.format("0 0 0 %d", pos), 0, 'utf8', function(err, written, string) {});
+    fs.write(fd, util.format("0 0 0 %d", pos));
     // console.log("fd=%d", fd);
     // fs.writeSync(fd, util.format("0 0 0 %d", pos));
-    console.log("pos=%d, dir=%d, start=%d, stop=%d", pos, dir, start, stop);
+    // console.log("pos=%d, dir=%d, start=%d, stop=%d", pos, dir, start, stop);
     pos += dir;
     if(pos >= stop || pos < start) {
         dir = -dir;
@@ -36,10 +37,5 @@ function updateLED(pos, dir, start, stop) {
     setTimeout(updateLED, ms, pos, dir, start, stop);
 }
 
-// function testMe(err, written, string) {
-//     console.log("written = %d", written);
-//     fs.closeSync(fd);
-// }
-
-fs.writeSync(fd, "\n");
+// fs.writeSync(fd, "\n");
 // fs.closeSync(fd);
