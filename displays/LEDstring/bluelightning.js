a7beb0ccfbc1ce2f6083dc476257fd3cb7f0f3eb
root <root@beaglebone.localdomain>
2015-12-30 15:34:51 +0000

    Syncronous writes

---
+++ displays/LEDstring/bluelightning.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var util = require('util');
+var LEDs = "/sys/firmware/lpd8806/device/rgb";
+var LEDcount = process.env.STRING_LEN;
+
+console.log("LEDcount = %d", LEDcount);
+
+var fd = fs.openSync(LEDs, 'w');
+console.log("fd=%d", fd);
+
+for(var i = 0; i < LEDcount; i++) {
+    fs.writeSync(fd, util.format("150 50 50 %d", i));
+}
+
+
+// function testMe(err, written, string) {
+//     console.log("written = %d", written);
+//     fs.closeSync(fd);
+// }
+
+fs.writeSync(fd, "\n");
+fs.closeSync(fd);
