f0325ed304fa3d75abf536b7abf311fbd8c5e6c4
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2015-12-30 17:17:29 +0000

    cycling LED works with async writes

--- displays/LEDstring/bluelightning.js
+++ displays/LEDstring/bluelightning.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var util = require('util');
 var LEDs = "/sys/firmware/lpd8806/device/rgb";
 var LEDcount = process.env.STRING_LEN;
+var ms = 10;
 
 console.log("LEDcount = %d", LEDcount);
 
@@ -11,12 +12,29 @@ var fd = fs.openSync(LEDs, 'w');
 console.log("fd=%d", fd);
 
 for(var i = 0; i < LEDcount; i++) {
-    // fs.writeSync(fd, util.format("150 50 50 %d", i));
-    fs.write(fd, util.format("150 150 50 %d", i), 0, 'utf8', function(err, written, string) {
+    // fs.writeSync(fd, util.format("150 0 0 %d", i));
+    fs.write(fd, util.format("0 50 0 %d", i), 0, 'utf8', function(err, written, string) {
         // console.log("written = %d", written);
     });
 }
 
+updateLED(0, 1, 0, LEDcount);
+
+function updateLED(pos, dir, start, stop) {
+    fs.write(fd, util.format("0 0 0 %d", pos), 0, 'utf8', function(err, written, string) {});
+    // console.log("fd=%d", fd);
+    // fs.writeSync(fd, util.format("0 0 0 %d", pos));
+    console.log("pos=%d, dir=%d, start=%d, stop=%d", pos, dir, start, stop);
+    pos += dir;
+    if(pos >= stop || pos < start) {
+        dir = -dir;
+        pos += dir;
+        console.log("dir = %d", dir);
+    }
+  fs.write(fd, util.format("0 150 0 %d\n", pos), 0, 'utf8', function(err, written, string) {});
+    // fs.writeSync(fd, util.format("0 150 0 %d\n", pos));
+    setTimeout(updateLED, ms, pos, dir, start, stop);
+}
 
 // function testMe(err, written, string) {
 //     console.log("written = %d", written);
@@ -24,4 +42,4 @@ for(var i = 0; i < LEDcount; i++) {
 // }
 
 fs.writeSync(fd, "\n");
-fs.closeSync(fd);
+// fs.closeSync(fd);
