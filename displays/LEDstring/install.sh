6e35fd776ca6c97e10f9fa4ae221dd5c710bcb93
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-01-05 09:43:36 -0500

    Added instructions for Jessie

--- displays/LEDstring/install.sh
+++ displays/LEDstring/install.sh
@@ -1,1 +1,19 @@
+# This will make the driver
 apt-get install linux-headers-`uname -r`
+cd driver
+make
+
+# Here's how to get SPI working under Jessie
+# Edit /boot/uEnv.txt and removed the comment on the following line:
+
+##BeagleBone Black: HDMI (Audio/Video) disabled:
+dtb=am335x-boneblack-emmc-overlay.dtb
+
+# This will disable the HDMI and free up the pin needed for the SPI
+# Then later in the file add:
+
+##Example v4.1.x
+#cape_disable=bone_capemgr.disable_partno=
+cape_enable=bone_capemgr.enable_partno=BB-SPIDEV1
+
+# This will enable the SPI device we need.
