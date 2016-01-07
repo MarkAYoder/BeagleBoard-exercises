f6692ed4fadad16a5641cd3031ef89c244b2df2a
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-01-07 08:35:03 -0500

    Added simpler instructions for rtl_fm

--- rtl-sdr/install.sh
+++ rtl-sdr/install.sh
@@ -1,3 +1,15 @@
+# If you just want to listen to FM try:
+# http://sdr.osmocom.org/trac/wiki/rtl-sdr
+apt-get install rtl-sdr
+
+# Then you can decode on the Bone and send the data to a host to play.
+# On the bone
+# rtl_fm -f 92.7M -M wbfm -r 48000 - | netcat -l -p8082
+# On the host
+# nc bone 8082 | aplay -r 48k -f S16_LE
+
+
+# Here's how to track airplanes
 # From: http://makezine.com/projects/tracking-planes-with-rtl-sdr/
 
 apt-get install cmake
@@ -12,7 +24,3 @@ git clone https://github.com/MalcolmRobb/dump1090 MR-dump1090
 cd MR-dump1090
 make
 ./dump1090 --interactive --net --net-http-port 8081
-# Or on the bone
-# rtl_fm -f 92.7M -M wbfm -r 48000 - | netcat -l -p8082
-# On the host
-# nc bone 8082 | aplay -r 48k -f S16_LE
