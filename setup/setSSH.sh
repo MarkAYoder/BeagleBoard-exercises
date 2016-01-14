725873ecd1840c18072ce2860addf3614ca41c04
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-01-14 09:59:30 -0500

    Added second DIR parameter

--- setup/setSSH.sh
+++ setup/setSSH.sh
@@ -3,9 +3,9 @@
 # http://stackoverflow.com/questions/17686952/mounting-sshfs-on-unreliable-connection
 
 HOST=${1:-office}
-DIR=${2:-~/cdrom}
+DIR1=${2:-~/cdrom}
+DIR2=${3:-~/cdrom}
 
-echo $HOST
-echo $DIR
+echo $HOST, $DIR1, $DIR2
 
-sshfs -C -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 yoder@$HOST:$DIR $DIR
+sshfs -C -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 yoder@$HOST:$DIR1 $DIR2
