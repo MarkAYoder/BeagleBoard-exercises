ba3a55b06633aaf7de0792d628251d49c8662a07
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-01-22 17:10:07 -0500

    Restored condition/forecast

--- iot/weather/wunderground.js
+++ iot/weather/wunderground.js
@@ -5,7 +5,7 @@ var util    = require('util');
 var key = 'ec7eb641373d9256';
 // See http://www.wunderground.com/weather/api/d/docs?d=data/forecast for features
 // try 'forcast', 'forecast10day', 'conditions', 'astronomy', 'history'
-var feature = 'history';
+var feature = 'conditions/forecast';
 
 var url = util.format('http://api.wunderground.com/api/%s/%s/q/IN/Brazil.json',
                         key, feature);
