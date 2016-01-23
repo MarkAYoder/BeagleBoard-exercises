a0fa777f8d59fd4c932c3cd4ac7da6a3e90ce4bf
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-01-23 16:56:57 -0500

    Added JSON.parse so I can pull out various parts

--- iot/weather/wunderground.js
+++ iot/weather/wunderground.js
@@ -5,7 +5,7 @@ var util    = require('util');
 var key = 'ec7eb641373d9256';
 // See http://www.wunderground.com/weather/api/d/docs?d=data/forecast for features
 // try 'forcast', 'forecast10day', 'conditions', 'astronomy', 'history'
-var feature = 'conditions/forecast';
+var feature = 'conditions';
 
 var url = util.format('http://api.wunderground.com/api/%s/%s/q/IN/Brazil.json',
                         key, feature);
@@ -14,7 +14,10 @@ console.log(url);
 
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body); 
+    var weather = JSON.parse(body);
+    // console.log(weather); 
+    console.log("%s, %s", weather.current_observation.temperature_string,
+        weather.current_observation.wind_string); 
   } else {
     console.log("error=" + error + " response=" + JSON.stringify(response));
   }
