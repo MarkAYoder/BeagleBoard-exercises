f38a80d1bf70b2d238e892254729724320164ab5
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-02-01 12:54:18 -0500

    Cleaned up

--- iot/adafruit.io/nodeSub.js
+++ iot/adafruit.io/nodeSub.js
@@ -1,23 +1,17 @@
 #!/usr/bin/env node
-// Useage nodeSub.js [message] [topic]
+// Useage nodeSub.js [message] [feed]
 var mqtt = require('mqtt'),
     host = 'io.adafruit.com', // or localhost
     user = process.env.AIO_USER,
     pass = process.env.AIO_KEY,
-    feed = 'Wunderground',  // Default feed
-    mess = '100',   // Default message
+    mess = process.argv[2] ? process.argv[2] : '100',   // Default message
+    feed = process.argv[3] ? process.argv[3] : 'Wunderground',  // Default feed
   // , client = mqtt.connect();
     client = mqtt.connect({ 
     port: 1883, host: host, keepalive: 10000,
     username: user, password: pass
   });
 
-if(process.argv[2]) {
-  mess = process.argv[2];
-}
-if(process.argv[3]) {
-  feed = process.argv[3];
-}
 client.subscribe(user + '/feeds/'+ feed);
 client.publish  (user + '/feeds/'+ feed, mess);
 client.on('message', function (topic, message) {
