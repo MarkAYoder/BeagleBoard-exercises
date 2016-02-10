67cfc5c9326c8427199e2fe8dad1660ee10cb8bb
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-02-10 16:52:29 -0500

    Fixed IP mask

--- setup/firewall.sh
+++ setup/firewall.sh
@@ -3,7 +3,7 @@
 # Set up firewall to reject all but 317.112.*.* and 192.168.7.*
 iptables --policy INPUT DROP
 iptables -A INPUT -s 137.112.0.0/16 -j ACCEPT
-iptables -A INPUT -s 192.168.7.0/8 -j ACCEPT
-iptables -A INPUT -s 10.0.4.0/8 -j ACCEPT
+iptables -A INPUT -s 192.168.7.0/24 -j ACCEPT
+iptables -A INPUT -s 10.0.4.0/24 -j ACCEPT
 # Switch -A to -D to delete rule
-# iptables -D INPUT -s 192.168.7.0/8 -j ACCEPT
+# iptables -D INPUT -s 192.168.7.0/24 -j ACCEPT
