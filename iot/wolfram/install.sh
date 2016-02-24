55b73b5103930edc0857fe87ec98842455b6d6ce
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-02-24 10:13:05 -0500

    Added instructions for installing my version

--- iot/wolfram/install.sh
+++ iot/wolfram/install.sh
@@ -1,5 +1,10 @@
-# This one is getting an install error
+# This works without options
 npm install -g wolfram
+
+# My version will take options
+git clone https://github.com/MarkAYoder/node-wolfram.git
+cd $NODE_PATH
+ln -s ln -s wolfram/node_modules/libxmljs/ .
 
 # Here's another
 apt-get install libxml2-dev
--- iot/wolfram/words-test.js
+++ iot/wolfram/words-test.js
@@ -2,22 +2,25 @@
 
 console.log(process.env.WOLFRAM_APPID);
 
-var wolfram = require('./node-wolfram').createClient(process.env.WOLFRAM_APPID)
+var wolfram = require('./node-wolfram').createClient(process.env.WOLFRAM_APPID);
 
-wolfram.query({search: "words containing msdoep", 
+wolfram.query({search: "words containing abcdefg",  // This only works with my version
   options:
-    "&podstate=WordsMadeWithOnlyLetters__Show+all" +
-    "&podstate=WordsMadeWithOnlyLetters__Disallow+repetition" +
-    "&includepodid=WordsMadeWithOnlyLetters"}, 
+    "&podstate=WordsMadeWithOnlyLetters__Show+all"
+    +"&podstate=WordsMadeWithOnlyLetters__Disallow+repetition"
+    +"&includepodid=WordsMadeWithOnlyLetters"
+    }, 
     function(err, result) {
-	if(err) throw err
-	console.log("Size: %d", result.length);
-	console.log("Result: %j", result[0].subpods[0].value);
-})
+        if (err) throw err;
+        console.log("Size: %d", result.length);
+        console.log("Result: %j", result);
+    });
 
-wolfram.query("words containing abcdefg", 
+wolfram.query("words containing abcdefg",
     function(err, result) {
-	if(err) throw err
-	console.log("Size: %d", result.length);
-	console.log("Result: %j", result);
-})
+        if (err) throw err;
+        console.log("Size: %d", result.length);
+        console.log("Result: %j", result);
+        console.log("3rd result: %j", result[3]);
+        console.log("3rd result: %s", result[3].subpods[0].value);
+    });
