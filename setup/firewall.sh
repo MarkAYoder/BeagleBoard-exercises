7c79fd15db8fe9f4eb16fb3cef29c8c601d76404
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-03-10 16:48:05 -0500

    Made firewall work with 137.112.41.0/24

--- setup/firewall.sh
+++ setup/firewall.sh
@@ -2,7 +2,7 @@
 # Run on bone to only accept from a few IP ranges
 # Set up firewall to reject all but 317.112.*.* and 192.168.7.*
 iptables --policy INPUT DROP
-iptables -A INPUT -s 137.112.0.0/16 -j ACCEPT
+iptables -A INPUT -s 137.112.41.0/24 -j ACCEPT
 iptables -A INPUT -s 192.168.7.0/24 -j ACCEPT
 iptables -A INPUT -s 10.0.4.0/24 -j ACCEPT
 # Switch -A to -D to delete rule
