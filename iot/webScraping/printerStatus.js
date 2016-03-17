c20dc2630b4598a08ca680010b481a86ef15ed9f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-03-17 09:45:08 -0400

    Displays tray status and size

--- iot/webScraping/printerStatus.js
+++ iot/webScraping/printerStatus.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
-// var express = require('express');
-// var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-// var app     = express();
 
 // Let's scrape Incrediables
 var url = 'http://c205-ricoh.printer.rose-hulman.edu/web/guest/en/websys/webArch/topPage.cgi';
@@ -21,12 +18,14 @@ request(url, function(error, response, html){
 	        var path = data.children().first().children().first().children().first().children();
 	        // console.log("path: " + path);
 	        var tray = path.eq(0).text();
-	        if(tray === "Tray 1") {
-	        	var status = path.eq(3).children().first().children().first().children().first().text();
-		        console.log("tray: " + tray);
+	        if(tray.indexOf("Tray") > -1) {
+	        	var status = path.eq(1).children().first().children().first().children().first().children().first().attr('title');
+	        	var size   = path.eq(3).children().first().children().first().children().first().text();
+		        console.log("tray:   " + tray);
 		        console.log("status: " + status);
+		        console.log("size:   " + size);
 	        }
-        })
+        });
 	}
-})
+});
 	
