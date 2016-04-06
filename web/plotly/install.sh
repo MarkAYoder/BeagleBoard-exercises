85ea11dca032b52524cc1e7f3cfa6f5a9d529828
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-04-06 12:07:38 -0400

    Simplified getting current versions of plotly, jquery and underscore

--- web/plotly/install.sh
+++ web/plotly/install.sh
@@ -1,15 +1,13 @@
 # This uses the free BASIC version of plotlyjs which runs locally.
 # From https://plot.ly/javascript/
-wget http://d14fo0winaifog.cloudfront.net/plotlyjs_basic.zip
-apt-get install unzip
-unzip plotlyjs_basic.zip
-
-ln -s plotly_20150812a_basic weather    # Make path easier to remember
-
-cp plotWeather.html weather
+wget https://cdn.plot.ly/plotly-latest.min.js
 
 # Link it where the browser can see it 
-here=$PWD/weather
+here=$PWD
 cd /var/lib/cloud9
 ln -s $here .
 
+# Load these to server them locally
+wget https://code.jquery.com/jquery-2.2.3.min.js
+mv jquery-2.2.3.min.js jquery.min.js
+wget http://underscorejs.org/underscore-min.js
