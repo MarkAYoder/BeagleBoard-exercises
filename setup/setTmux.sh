6f31ecb878b0b83798a03caa230a0ae1b217fa1c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-04-06 17:18:46 -0400

    Updated for bedroom

--- setup/setTmux.sh
+++ setup/setTmux.sh
@@ -3,7 +3,7 @@ cd ~/exercises/iot/phant
 
 tmux new-session -d -s weather
  
-tmux new-window -t weather:1 -n 'Weather' './tempSoil.js'
+tmux new-window -t weather:1 -n 'Weather' './weather.js'
 tmux split-window -h -t weather:1 './flashUp.js'
 
 tmux attach-session -t weather
