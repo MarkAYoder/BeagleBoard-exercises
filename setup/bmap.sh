7d90eda8d634e6df0575e3aa93dc69717aa7e098
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-04-06 13:57:53 -0400

    Added instructions for out of date bmaptools

--- setup/bmap.sh
+++ setup/bmap.sh
@@ -2,5 +2,8 @@
 # http://elinux.org/BeagleBoardDebian#BeagleBone_White.2FBlack.2FGreen
 
 sudo apt-get install bmap-tools
+# The apt-get may get an old version, if so run
+# sudo apt-get update
+# sudo apt-get install bmap-tools/jessie
 
 sudo bmaptool copy bone-debian-7.8-console-armhf-2015-07-28-2gb.img.xz /dev/mmcblk0
