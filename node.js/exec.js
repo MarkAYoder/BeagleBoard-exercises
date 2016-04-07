e3f7dd00d79e1cbe0dbc4643c9cc1c1b9eec251d
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-04-07 15:14:28 -0400

    Made executable

--- node.js/exec.js
+++ node.js/exec.js
@@ -1,5 +1,6 @@
-# Here's how you can run a shell command and read it's output via exec
-# From http://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+#!/usr/bin/env node
+// Here's how you can run a shell command and read it's output via exec
+// From http://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
 
 var exec = require('child_process').exec,
     child;
