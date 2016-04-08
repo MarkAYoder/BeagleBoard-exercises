ab4d95aac80668b752ad257fb53cd180575d9dd1
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-04-07 21:29:26 -0400

    Switched to 9:02pm

--- setup/installCron.sh
+++ setup/installCron.sh
@@ -2,5 +2,6 @@
 echo "
 # Turn off LED triggers at night
 NODE_PATH=/usr/local/lib/node_modules
-1 5  58 * *   root    /root/exercises/setup/ledTrigger.js
-1 21  * * *   root    /root/exercises/setup/ledTrigger.js off" >> /etc/crontab
+58 5  * * *   root    /root/exercises/setup/ledTrigger.js
+2 21  * * *   root    /root/exercises/setup/ledTrigger.js off
+" >> /etc/crontab
