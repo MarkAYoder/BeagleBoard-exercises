36c374fa9e9ca4099043270f5a4e770e2809d429
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-04-23 12:37:27 -0400

    Moved async and noble-device to npm

--- sensors/sensorTag/install.sh
+++ sensors/sensorTag/install.sh
@@ -1,4 +1,6 @@
 # From: https://github.com/sandeepmistry/node-sensortag
-apt-get install libbluetooth-dev libudev-dev bluez async noble-device
-npm install -g sensortag
+apt-get install libbluetooth-dev libudev-dev bluez
+npm install -g sensortag async noble-device
 hcitool lescan
+
+ln -s $NODE_PATH/sensortag .
