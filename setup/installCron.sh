7e69e60d6b2db5c3bb6338c7abae1e46f245ba5c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-05-14 12:40:48 -0400

    Added recordPing

--- setup/installCron.sh
+++ setup/installCron.sh
@@ -4,4 +4,5 @@ echo "
 NODE_PATH=/usr/local/lib/node_modules
 58 5  * * *   root    /root/exercises/setup/ledTrigger.js
 2 21  * * *   root    /root/exercises/setup/ledTrigger.js off
+* * * * * root /root/exercises/iot/phant/recordPing.js 2>&1 | logger
 " >> /etc/crontab
