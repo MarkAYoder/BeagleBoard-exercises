d970e41ee3cc47cc3d3103c0a738a7f8cd6a7e35
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-06-09 16:42:03 -0400

    toFixed(2)

--- Jaars/test.js
+++ Jaars/test.js
@@ -24,22 +24,28 @@ var err;    // Value returned by digitWrite so that it runs synchorously
 
 for(var k=0; k<100; k++) {
     for(var i=0; i<headers; i++) {
-        console.log("header: " + i);
-        err = b.digitalWrite('P9_12', i & 0x1);
-        err = b.digitalWrite('P9_14', (i>>1) & 0x1);
+        // console.log("header: " + i);
+        err = b.digitalWrite('P9_12', (i>>1) & 0x1);
+        err = b.digitalWrite('P9_14',  i     & 0x1);
         // b.digitalWrite('P9_12', 0);
         // b.digitalWrite('P9_14', 0);
         // console.log("bits: " + (i & 0x1) + (i>>1 & 0x1));
         for(var j=0; j<pairs; j++) {
-            console.log("pair: " + j);
+            // console.log("pair: " + j);
             err = b.digitalWrite('P9_18', j & 0x1);
             err = b.digitalWrite('P9_22', (j>>1) & 0x1);
             // b.digitalWrite('P9_18', 0);
             // b.digitalWrite('P9_22', 0);
-            value[4*headers+pairs] = 1000*1.8*b.analogRead(analogIn);
-            // console.log("value: %d", value);
+            value[4*i+j] = 1000*1.8*b.analogRead(analogIn);
+            // console.log("value: %d", value[4*i+j]);
             sleep.usleep(100000);
             }
     }
-    console.log("values: " + value);
+    // console.log(" " + value);
+    for(ll=0; ll<value.length; ll++) {
+        process.stdout.write(value[ll].toFixed(2) + ", ");
+    }
+    process.stdout.write("\n");
+    
+    sleep.sleep(4);
 }
