898233ea0eb66dac8c34fe746b6509558291a3e3
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-06-14 14:39:59 -0400

    Added cloning instructions

--- cookbook/install.sh
+++ cookbook/install.sh
@@ -1,1 +1,5 @@
-apt-get install asciidoc
+# Just clone the two repos
+git clone https://github.com/jadonk/beaglebone-cookbook.git
+git clone https://github.com/MarkAYoder/bone101.git
+
+# apt-get install asciidoc
