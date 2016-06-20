29ea1e15b8ea52e7a700700a3fc60b0acd6c0bf8
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-06-20 14:42:11 -0400

    Added more info

--- docker/install.sh
+++ docker/install.sh
@@ -1,7 +1,11 @@
 # From: https://docs.docker.com/engine/installation/linux/debian/#debian-jessie-80-64-bit
 # Also: https://groups.google.com/forum/#!category-topic/beagleboard/beaglebone-black/2cnwVEmKqX4
 
+apt-get install docker.io
+
 docker info
 docker run armhf/hello-world
 docker run -i -t armhf/debian bash
 docker run -i -t armhf/ubuntu bash
+
+# Here's where to find other armhf images: https://hub.docker.com/u/armhf/?page=1
