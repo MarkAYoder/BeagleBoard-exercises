e7e79f537651a1a879a9f131ca5e4f99ef720c96
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-06-22 11:06:03 -0400

    Switched to ET time zone

--- web/plotly/install.sh
+++ web/plotly/install.sh
@@ -7,6 +7,10 @@ wget https://code.jquery.com/jquery-2.2.4.min.js
 mv jquery-2.2.4.min.js jquery.min.js
 wget http://underscorejs.org/underscore-min.js
 
+# For time zone conversion
+wget http://momentjs.com/downloads/moment.min.js
+wget http://momentjs.com/downloads/moment-timezone-with-data-2010-2020.min.js -O moment-timezone.min.js
+
 # Link it where the browser can see it 
 here=$PWD
 cd /var/lib/cloud9
