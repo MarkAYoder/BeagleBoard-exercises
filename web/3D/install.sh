22d144c36e528f5f9464889147afe8df49515687
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-06-22 15:21:51 -0400

    Updated install.sh

---
+++ web/3D/install.sh
@@ -0,0 +1,8 @@
+# From: http://www.aerotwist.com/tutorials/getting-started-with-three-js/
+
+mkdir js
+cd js
+
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/build/three.min.js
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/renderers/CanvasRenderer.js
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/renderers/Projector.js
