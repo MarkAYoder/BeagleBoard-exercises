5abcd01cf466b2a0d7ced84e237dd5cb414ecba4
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-07-01 11:40:23 -0400

    Added instructions for remoteproc

--- pru/install.sh
+++ pru/install.sh
@@ -14,3 +14,40 @@ wget http://exploringbeaglebone.com/wp-content/uploads/2014/12/Instruction-Set-S
 wget http://lxr.free-electrons.com/source/drivers/uio/uio_pruss.c
 
 # PRU C compiler: tiny.cc/ebb1311
+
+
+# remoteproc
+# http://processors.wiki.ti.com/index.php/PRU-ICSS
+
+# Examples
+# http://theembeddedkitchen.net/categories/beaglelogic
+
+# Get latest kernel
+apt-get install linux-image-4.4.14-ti-r33
+apt-get install linux-headers-4.4.14-ti-r33
+
+# Get BeagleLogic
+git clone https://github.com/abhishek-kakkar/BeagleLogic.git
+cd BeagleLogic/beaglelogic-firmware
+make
+make devicetree
+make firmware
+
+# This may be a better lead:
+https://zeekhuge.github.io/
+https://zeekhuge.github.io/post/a_handfull_of_commands_and_scripts_to_get_started_with_beagleboneblack/#working_with_prus:01d25bfd2399ec47b9c04f156786eab8
+
+git clone https://github.com/ZeekHuge/BeagleScope.git
+
+# Then add
+here=$PWD
+cd /usr/share/ti/cgt-pru
+mkdir bin
+cd bin
+ln -s /usr/bin/clpru .
+export PRU_CGT=/usr/share/ti/cgt-pru
+
+cd BeagleScope/examples/firmware_exmples/pru_blinky
+./deploy
+# Wire and LED to P8_45 and it should be blinking.
+# You might have to disable HDMI for this to work.
