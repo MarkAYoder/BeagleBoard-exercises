2cbfa68b9bcc5637ff7689b27cbe5320d745e569
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-07-02 14:24:47 -0400

    Now posting data

--- iot/artik/test.js
+++ iot/artik/test.js
@@ -2,17 +2,35 @@
 // Playing with Artik
 var https = require('https');
 var util  = require('util');
+var querystring = require('querystring');
+
+var postData = JSON.stringify({
+    "data": {
+        "state": 0,
+        "stepCount": 1,
+        "heartRate": 2,
+        "activity": 30
+    },
+    "sdid": "271e863bf1f04c95aa3735f9055f2781",
+    "type": "message"
+  });
+
+console.log('postData: ' + postData);
 
 var header = {
     "Content-Type": "application/json",
-    "Authorization": "Bearer 40a617e4e83e45ccb5167ef50a8e0246"
+    "Authorization": "Bearer 40a617e4e83e45ccb5167ef50a8e0246",
+    "Content-Length": postData.length
     };
     
-    var id = 'cd98e36c092345c89974f06decfa4540';
+var id = 'cd98e36c092345c89974f06decfa4540';
+var deviceID = '271e863bf1f04c95aa3735f9055f2781';
     
 var paths = [
     // '/users/self',
-    '/users/' + id + '/devices?count=100&includeProperties=true'
+    // '/users/' + id + '/devices?count=100&includeProperties=true',
+    // '/devices/' + deviceID + '?properties=true',
+    '/messages'
     ];
 
 
@@ -20,7 +38,7 @@ var options = {
   hostname: 'api.artik.cloud',
   path: '/v1.1/users/self',
   port: 443,
-  method: 'GET',
+  method: 'POST',
   headers: header
 };
 
@@ -39,6 +57,8 @@ for(var i=0; i<paths.length; i++) {
       });
     });
 }
+
+req.write(postData);
 
 req.end();
 
