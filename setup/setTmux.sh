ff78ba6feefe7dfd0885b9578be22e77931eb7b2
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-07-04 11:34:03 -0400

    Added artik.js

--- setup/setTmux.sh
+++ setup/setTmux.sh
@@ -4,6 +4,7 @@ cd ~/exercises/iot/phant
 tmux new-session -d -s weather
  
 tmux new-window -t weather:1 -n 'Weather' './weather.js'
-tmux split-window -h -t weather:1 './flashUp.js'
+tmux split-window -v -t weather:1 './flashUp.js'
+tmux split-window -h -t weather:1 './artik.js'
 
 tmux attach-session -t weather
