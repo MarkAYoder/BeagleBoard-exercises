3a41da2ab31a0e7150ef4b82f3a86c9a1b74e441
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-07-20 18:36:32 -0400

    Still trying to make it work

--- pru/examples/servo/pru1-servo.asm
+++ pru/examples/servo/pru1-servo.asm
@@ -11,11 +11,11 @@
 
 	.cdecls "main_pru1.c"
 
-DELAY	.macro time
-	LDI32	R11, time
-	QBEQ	$E?, R11, 0
-$M?:	SUB	R11, R11, 1
-	QBNE	$M?, R11, 0
+DELAY	.macro time, reg
+	LDI32	reg, time
+	QBEQ	$E?, reg, 0
+$M?:	SUB	reg, reg, 1
+	QBNE	$M?, reg, 0
 $E?:	
 	.endm
 	
@@ -24,9 +24,9 @@ $E?:
 	.global start
 start:
 	LDI 	R30, 0xFFFF
-	DELAY 	10000000
+	DELAY 	10000000, r11
 	LDI		R30, 0x0000
-	DELAY 	10000000
+	DELAY 	10000000, r11
 ; 	JMP	start	
 
 ; 	HALT
@@ -46,53 +46,46 @@ start:
 	.asg    C4,     CONST_SYSCFG         
 	.asg    C28,    CONST_PRUSHAREDRAM   
  
-; 	.asg PRU0_CTRL            0x22000
+	.asg	0x22000,	PRU0_CTRL
 	.asg    0x24000,    PRU1_CTRL       ; page 19
 	.asg    0x28,       CTPPR0          ; page 75
  
-; 	.asg OWN_RAM              0x000
-; 	.asg OTHER_RAM            0x020
-; 	.asg    0x100,     SHARED_RAM           
-	.asg    0x10,     SHARED_RAM       ; This is so prudebug can find it.
+	.asg	0x000,	OWN_RAM
+	.asg	0x020,	OTHER_RAM
+	.asg    0x100,	SHARED_RAM       ; This is so prudebug can find it.
+
+	LBCO	&r0, CONST_SYSCFG, 4, 4		; Enable OCP master port
+	CLR 	r0, r0, 4					; Clear SYSCFG[STANDBY_INIT] to enable OCP master port
+	SBCO	&r0, CONST_SYSCFG, 4, 4
 	
-;     LBCO	&r0, CONST_SYSCFG, 4, 4		; Enable OCP master port
-; 	CLR 	r0, r0, 4					; Clear SYSCFG[STANDBY_INIT] to enable OCP master port
-;  	SBCO	&r0, CONST_SYSCFG, 4, 4
-	LDI     r0, 0x00000120				; Configure the programmable pointer register for PRU0 by setting c28_pointer[15:0]
+; Configure the programmable pointer register for PRU0 by setting c28_pointer[15:0]
 	LDI     r0, SHARED_RAM              ; Set C28 to point to shared RAM
 	LDI32   r1, PRU1_CTRL + CTPPR0		; Note we use beginning of shared ram unlike example which
-	SBBO    &r0, r1, 0, 4				; has arbitrary 2048 offset, page 25
+	SBBO    &r0, r1, 0, 4				;  page 25
 	
-	LDI		r9, 0x00000000				; erase r9 to use to use later
+	LDI		r9, 0x0				; erase r9 to use to use later
 	
-	LDI 	r0, 0x00000000				; clear internal counters
-	LDI 	r1, 0x00000000	
-	LDI 	r2, 0x00000000
-	LDI 	r3, 0x00000000
-	LDI 	r4, 0x00000000
-	LDI 	r5, 0x00000000
-	LDI 	r6, 0x00000000
-	LDI 	r7, 0x00000000
-	LDI 	r30, 0x00000000				; turn off GPIO outputs
+	LDI 	r0, 0x0				; clear internal counters
+	LDI 	r1, 0x0	
+	LDI 	r2, 0x0
+	LDI 	r3, 0x0
+	LDI 	r4, 0x0
+	LDI 	r5, 0x0
+	LDI 	r6, 0x0
+	LDI 	r7, 0x0
+	LDI 	r30, 0x0				; turn off GPIO outputs
 	
-	LDI     r10, SHARED_RAM
-	ldi32   r12, 0xdeadbeef
-	ldi     r12, 0x200
-; 	sbbo    &r12, r10, 0, 4
-    sbco	&r12, CONST_PRUSHAREDRAM, 0, 4
-    
-    ; halt
-	
+
 ; Beginning of loop, should always take 48 instructions to complete
 CH1:			
 	SET 	r30, LED
 ; 	SET     r30, CH1BIT
-; 	DELAY 	100
+	DELAY 	1000, r11
 	QBEQ	CLR1, r0, 0						; If timer is 0, jump to clear channel
 	SET		r30, CH1BIT						; If non-zero turn on the corresponding channel
 	SUB		r0, r0, 1						; Subtract one from timer
 	CLR		r9, r9.t1						; waste a cycle for timing
-; 	SBCO	&r9, CONST_PRUSHAREDRAM, 0, 4	; write 0 to shared memory
+	SBCO	&r9, CONST_PRUSHAREDRAM, 0, 4	; write 0 to shared memory
 
 	QBA		CH1								; go back to check next channel
 	
@@ -100,8 +93,7 @@ CH1:
 CLR1:
 	CLR		r30, CH1BIT						; turn off the corresponding channel
 	CLR 	r30, LED
-	DELAY 	10000
+	DELAY 	1000, r11
 	LBCO	&r0, CONST_PRUSHAREDRAM, 0, 4	; Load new timer register
-; 	LBBO	&r0, r10, 0, 4       	        ; Load new timer register
-; 	LDI32    r0, 0x100
+	
 	QBA		CH1								; go back to check next channel
---
+++ pru/examples/servo/servo-test.c
@@ -0,0 +1,64 @@
+/* 
+ *
+ *  servo tester
+ *  (c) Copyright 2016
+ *  Mark A. Yoder, 20-July-2016
+ *
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+
+int main(int argc, char *argv[])
+{
+	unsigned int	*pru;
+	int	fd;
+	unsigned long	pruss_addr = 0x4A300000;		// Page 184 am335x TRM
+	int pruss_len = 0x80000;
+	
+	printf("Servo tester\n");
+	
+	// Use  /dev/mem
+	fd = open ("/dev/mem", O_RDWR | O_SYNC);
+	if (fd == -1) {
+		printf ("ERROR: could not open /dev/mem.\n\n");
+		return 1;
+	}
+	pru = mmap (0, pruss_len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, pruss_addr);
+	if (pru == MAP_FAILED) {
+		printf ("ERROR: could not map memory.\n\n");
+		return 1;
+	}
+	close(fd);
+	printf ("Using /dev/mem device.\n");
+	
+	int addr = 0x10000;
+	
+	printf("Addr %x contains %lx\n", addr, pru[addr/4]);
+	
+	pru[addr/4] = 0xfeedbeef;
+	
+	printf("Addr %x contains %lx\n", addr, pru[addr/4]);
+	
+	// int i;
+	// for(i=0; i<10000; i++) {
+	// 	printf("Updating...");
+	// 	fflush(stdout);
+	// 	pru[addr/4] = 0x10000;
+	// 	usleep(1000000);
+		
+	// 	printf("Updating2...");
+	// 	fflush(stdout);
+	// 	pru[addr/4] = 0x1000;
+	// 	usleep(1000000);
+	// }
+	
+	if(munmap(pru, pruss_len)) {
+		printf("munmap failed\n");
+	} else {
+		printf("munmap succeeded\n");
+	}
+}
+
