9775bd5d71765421513539d92fc1b5d936bf4933
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-07-21 15:18:35 -0400

    Does all three analyses

--- iot/google/NLP/test.sh
+++ iot/google/NLP/test.sh
@@ -1,8 +1,8 @@
 # From: https://cloud.google.com/natural-language/docs/getting-started
 
 export GOOGLE_APPLICATION_CREDENTIALS=NLP-test-e281274f4df1.json
-
+    
 curl -s -k -H "Content-Type: application/json" \
     -H "Authorization: Bearer ya29.Ci8nA0_tswnQzDz2O4Mr6RpvR9eWb2_wC4PTA6QaAdFk5ifeyIRCR6Nrd1bN2mdWcw" \
-    https://language.googleapis.com/v1beta1/documents:analyzeEntities \
-    -d @entity-request.json
+    https://language.googleapis.com/v1beta1/documents:annotateText \
+    -d @entity2.json
