2b4ab6875e9c17a77f868374ef0b1b5683c221f8
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-08-02 15:03:23 -0400

    Added RobertCNelson shell script

--- sensors/onewire/install.sh
+++ sensors/onewire/install.sh
@@ -1,3 +1,6 @@
+# From Robert Nelson
+wget https://raw.githubusercontent.com/beagleboard/bb.org-overlays/master/examples/BB-W1-P9.12/example.sh
+
 # From Cookbook
 dtc -O dtb -o BB-W1-00A0.dtbo -b 0 -@ BB-W1-00A0.dts
 cp BB-W1-00A0.dtbo /lib/firmware
