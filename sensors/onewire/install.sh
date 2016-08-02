89be21673dff932408724397511cd73fdf08b311
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-08-02 17:10:41 -0400

    Pulls temp out of return value

--- sensors/onewire/install.sh
+++ sensors/onewire/install.sh
@@ -2,7 +2,10 @@
 wget https://raw.githubusercontent.com/beagleboard/bb.org-overlays/master/examples/BB-W1-P9.12/example.sh
 
 # From Cookbook
-dtc -O dtb -o BB-W1-00A0.dtbo -b 0 -@ BB-W1-00A0.dts
-cp BB-W1-00A0.dtbo /lib/firmware
+# dtc -O dtb -o BB-W1-00A0.dtbo -b 0 -@ BB-W1-00A0.dts
+# cp BB-W1-00A0.dtbo /lib/firmware
 
-echo BB-W1 > $SLOTS
+# echo BB-W1 > $SLOTS
+
+echo "cape_enable=bone_capemgr.enable_partno=BB-W1-P9.12" >> /boot/uEnv.txt
+# Then reboot
