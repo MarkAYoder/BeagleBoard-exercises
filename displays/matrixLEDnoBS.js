3a8e6254fec334bd5f29d3bac86ef009feb6eb63
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-09-13 14:50:13 -0400

    Added BoneScript free js i2c example

---
+++ displays/matrixLEDnoBS.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+// This is a workaround since the BoneScript i2c isn't working.
+// Thanks to Ricky Rung
+// install:  npm install i2c@0.2.1  (the latest version doesn't work)
+
+var i2c = require('i2c');
+var port = '/dev/i2c-2';
+var matrix = 0x70;
+var time = 1000;
+
+var smile = [0x00, 0x3c, 0x00, 0x42, 0x28, 0x89, 0x04, 0x85,
+    0x04, 0x85, 0x28, 0x89, 0x00, 0x42, 0x00, 0x3c
+];
+var frown = [0x3c, 0x00, 0x42, 0x00, 0x85, 0x20, 0x89, 0x00,
+    0x89, 0x00, 0x85, 0x20, 0x42, 0x00, 0x3c, 0x00
+];
+var neutral = [0x3c, 0x3c, 0x42, 0x42, 0xa9, 0xa9, 0x89, 0x89,
+    0x89, 0x89, 0xa9, 0xa9, 0x42, 0x42, 0x3c, 0x3c
+];
+
+var wire = new i2c(0x70, {
+    device: '/dev/i2c-2'
+});
+
+wire.writeByte(0x21, function(err) {
+    wire.writeByte(0x81, function(err) {
+        wire.writeByte(0xe7, function(err) {
+            setTimeout(doFrown, 0);
+            setTimeout(doNeutral, 1 * time);
+            setTimeout(doSmile, 2 * time);
+        });
+    });
+});
+
+function doFrown() {
+    wire.writeBytes(0x00, frown, function(err) {
+    });
+}
+
+function doNeutral() {
+    wire.writeBytes(0x00, neutral, function(err) {
+    });
+}
+
+function doSmile() {
+    wire.writeBytes(0x00, smile, function(err) {
+    });
+}
