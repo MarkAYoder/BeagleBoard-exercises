d53a340819c669b1240dce2233a41148e4159dd7
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-09-14 10:14:21 -0400

    Added comments

--- displays/matrixLEDnoBS.js
+++ displays/matrixLEDnoBS.js
@@ -6,8 +6,9 @@
 var i2c = require('i2c');
 var port = '/dev/i2c-2';
 var matrix = 0x70;
-var time = 1000;
+var time = 1000; // Delay between images in ms
 
+// The first byte is GREEN, the second is RED.
 var smile = [0x00, 0x3c, 0x00, 0x42, 0x28, 0x89, 0x04, 0x85,
     0x04, 0x85, 0x28, 0x89, 0x00, 0x42, 0x00, 0x3c
 ];
@@ -21,10 +22,10 @@ var neutral = [0x3c, 0x3c, 0x42, 0x42, 0xa9, 0xa9, 0x89, 0x89,
 var wire = new i2c(0x70, {
     device: '/dev/i2c-2'
 });
-
-wire.writeByte(0x21, function(err) {
-    wire.writeByte(0x81, function(err) {
-        wire.writeByte(0xe7, function(err) {
+ 
+wire.writeByte(0x21, function(err) {            // Start oscillator (p10)
+    wire.writeByte(0x81, function(err) {        // Disp on, blink off (p11)
+        wire.writeByte(0xe7, function(err) {    // Full brightness (page 15)
             setTimeout(doFrown, 0);
             setTimeout(doNeutral, 1 * time);
             setTimeout(doSmile, 2 * time);
