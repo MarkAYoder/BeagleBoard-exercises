36776d7ad102d904509e18e94508e6629e15894c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-09-21 16:43:36 -0400

    Added tmp101 script

---
+++ sensors/tmp101.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+// Reads the tmp101 temperature sensor.
+
+var i2c = require('i2c-bus');
+var bus = 2;
+var tmp101 = [0x48, 0x49, 0x4a];
+var time = 1000;    // Time betwwen readings
+
+var sensor = i2c.openSync(bus);
+
+var temp;
+
+for(var i=0; i<tmp101.length; i++) {
+    temp = sensor.readByteSync(tmp101[i], 0x0);
+    console.log("temp: %dC, %dF (0x%s)", temp, temp*9/5+32, tmp101[i].toString(16));
+}
