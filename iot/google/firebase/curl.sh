e6230ae90352f8d85640f9ad3ff4bb34e96df9c3
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-09-24 16:53:49 -0400

    More playing

--- iot/google/firebase/curl.sh
+++ iot/google/firebase/curl.sh
@@ -3,11 +3,23 @@
 export DB='https://friendlychat-c7ea7.firebaseio.com/users.json'
 export apikey="AIzaSyDaMwP3HNH52o2JWC4Nb8ZNHpdb0k0pn6s"
 
-curl -X PUT -d '{
+curl -X POST -d '{
   "alanisawesome": {
     "name": "Alan Turing",
     "birthday": "June 23, 1912"
   }
-}' $DB \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $apikey"
+}' $DB 
+curl -X POST -d '{
+  "test": {
+    "name": "Test Ting",
+    "birthday": "June 23, 1912"
+  }
+}' $DB 
+# \
+#     -H "Content-Type: application/json" \
+#     -H "Authorization: Bearer $apikey"
+
+curl "$DB?print=pretty"
+
+# This never worked.  I don't know how to get the access token
+# curl "$DB?print=pretty&access_token=5FPzljup8kL-MZVnBWF6njkr"
