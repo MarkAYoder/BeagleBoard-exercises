493ea5041d77bc8ed53464fb11c09ea2f2ea3856
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-10-14 18:36:01 -0400

    Switched to pru0 and it now works

--- pru/examples/ebb/main_pru0.c
+++ pru/examples/ebb/main_pru0.c
@@ -51,10 +51,10 @@ void main(void)
 
 	// Access PRU Shared RAM using Constant Table
 	// C24 defaults to 0x00000000, we need to set bits 11:8 to 0x0200 in order to access local data RAM
-	PRU1_CTRL.CTBIR0_bit.C24_BLK_IDX = 0x2;
+	// PRU1_CTRL.CTBIR0_bit.C24_BLK_IDX = 0x2;
 	// C28 defaults to 0x00000000, we need to set bits 23:8 to 0x0100 in order to have it point to 0x00010000,
 	//		to access SHARED RAM
-	PRU1_CTRL.CTPPR0_bit.C28_BLK_POINTER = 0x0100;
+	// PRU1_CTRL.CTPPR0_bit.C28_BLK_POINTER = 0x0100;
 	
 	start();
 }
--- pru/examples/ebb/pru0-ledButton.asm
+++ pru/examples/ebb/pru0-ledButton.asm
@@ -16,19 +16,23 @@
 	.clink
 	.global start
 start:
-	set	r30, r30.t5           ; turn on the output pin (LED on)
+	; set	r30, r30.t5           ; turn on the output pin (LED on)
+	ldi32	r30, 0xffffffff
 	ldi32	r0, DELAY        ; store the length of the delay in REG0
 DELAYON:
 	SUB	r0, r0, 1        ; Decrement REG0 by 1
 	QBNE	DELAYON, r0, 0   ; Loop to DELAYON, unless REG0=0
-LEDOFF: 
-	clr	r30,	r30.t5           ; clear the output bin (LED off)
+LEDOFF:
+	ldi32	r30, 0x00000000
+	; clr	r30,	r30.t5           ; clear the output bin (LED off)
 	ldi32	r0, DELAY        ; Reset REG0 to the length of the delay
 DELAYOFF:
 	SUB	r0, r0, 1        ; decrement REG0 by 1
 	QBNE	DELAYOFF, r0, 0  ; Loop to DELAYOFF, unless REG0=0
 
-	QBBC	start, r31, 3    ; is the button pressed? If not, loop
+	qba	start
+	
+	; QBBC	start, r31, 3    ; is the button pressed? If not, loop
 
 END:                             ; notify the calling app that finished
 	; MOV	R31.b0, PRU0_R31_VEC_VALID | PRU_EVTOUT_0
