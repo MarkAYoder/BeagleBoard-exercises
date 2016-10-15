d5f3dba173199397dc2d239f8a40557a13dd81f1
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-10-15 08:17:21 -0400

    Added PRU_SUPPORT variable that's points to /opt/source/pru-software-support-package

--- pru/examples/ebb/setup.sh
+++ pru/examples/ebb/setup.sh
@@ -1,3 +1,6 @@
+# This sets things up for compiling
+# Enable remote proc by following: http://elinux.org/EBC_Exercise_30_PRU_via_remoteproc_and_RPMsg
+
 export PRU_CGT=/usr/share/ti/cgt-pru
 export PRU_SUPPORT=/opt/source/pru-software-support-package
 
