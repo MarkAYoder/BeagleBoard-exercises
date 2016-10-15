e2a26d779262eb2cc73300155e879ce10fc06d22
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-10-15 08:14:16 -0400

    Added PRU_SUPPORT variable that's points to /opt/source/pru-software-support-package

--- pru/examples/encoder/setup.sh
+++ pru/examples/encoder/setup.sh
@@ -1,3 +1,16 @@
+export PRU_CGT=/usr/share/ti/cgt-pru
+export PRU_SUPPORT=/opt/source/pru-software-support-package
+
+here=$PWD
+
+cd $PRU_CGT
+mkdir -p bin
+cd bin
+ln -s `which clpru`  .
+ln -s `which lnkpru` .
+
+cd $here
+
 HEADER=P8_
 PINS="15 16"
 
