7c9bbb4db4ece474a2a5a6ebdae2a908b59c9a54
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-10-15 08:24:08 -0400

    Added pointer to register conventions

--- pru/examples/ebb/pru0-ledButton.asm
+++ pru/examples/ebb/pru0-ledButton.asm
@@ -4,6 +4,9 @@
 ; Modified by Mark A. Yoder to use clpru and remote proc
 
 ; Passed the number of cycles to delay in R14
+; Register conventions are in "PRU Optimizing C/C++ Compiler v2.1 User's Guide"
+; http://www.ti.com/lit/ug/spruhv7a/spruhv7a.pdf
+; Section 6.3, Page 105
 
 	.clink
 	.global start
@@ -17,7 +20,7 @@ delayon:
 	qbne	delayon, r0, 0   ; Loop to DELAYON, unless REG0=0
 
 ledoff:
-	clr		r30,	r30.t5           ; clear the output bin (LED off)
+	clr		r30, r30.t5     ; clear the output bin (LED off)
 	mov		r0, r14	        ; Reset REG0 to the length of the delay
 
 delayoff:
@@ -28,3 +31,4 @@ delayoff:
 
 end:
 	jmp		r3				; r3 contains the return address
+							; Return value is in r14
