c059afb4211450490dbfaf4881944c4ec1bdf756
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-10-15 08:19:53 -0400

    Added PRU_SUPPORT variable that's points to /opt/source/pru-software-support-package

--- pru/examples/servo/setup.sh
+++ pru/examples/servo/setup.sh
@@ -1,3 +1,19 @@
+# This sets things up for compiling
+# Enable remote proc by following: http://elinux.org/EBC_Exercise_30_PRU_via_remoteproc_and_RPMsg
+
+export PRU_CGT=/usr/share/ti/cgt-pru
+export PRU_SUPPORT=/opt/source/pru-software-support-package
+
+here=$PWD
+
+cd $PRU_CGT
+mkdir -p bin
+cd bin
+ln -s `which clpru`  .
+ln -s `which lnkpru` .
+
+cd $here
+
 HEADER=P8_
 PINS="27 28 29 30 39 40 41 42 43 44 45 46"
 
