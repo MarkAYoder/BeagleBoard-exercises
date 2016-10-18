516c5b6226cdf2f279c7b4cbcdcea1f269615d03
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-10-18 13:23:46 -0400

    Cleaned up a bit

--- pru/examples/ebb/main_pru0.c
+++ pru/examples/ebb/main_pru0.c
@@ -36,7 +36,7 @@
 				  
 #include <stdint.h>
 #include <pru_cfg.h>
-#include <pru_ctrl.h>
+// #include <pru_ctrl.h>
 #include "resource_table_pru1.h"
 
 #define	INS_PER_US 200           // 5ns per instruction
@@ -51,8 +51,7 @@ extern void start(int time);
 volatile register unsigned int __R30;
 volatile register unsigned int __R31;
 
-void main(void)
-{
+void main(void) {
     /* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
