ceddb63138090a97f12a37cac33c1a4f76621339
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-10-20 12:52:09 -0400

    Switched to XOR

--- pru/examples/ebb/main_pru0.c
+++ pru/examples/ebb/main_pru0.c
@@ -56,10 +56,10 @@ void main(void) {
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
 	while(!(__R31&(1<<3))) {
-		__R30 |= 1<<5;
+		__R30 ^= 1<<5;
 		__delay_cycles(TIME);
-		__R30 &= ~(1<<5);
-		__delay_cycles(TIME);
+		// __R30 &= ~(1<<5);
+		// __delay_cycles(TIME);
 	}
 	__delay_cycles(TIME);	// Give some time for press to release
 	// Call assembly language
