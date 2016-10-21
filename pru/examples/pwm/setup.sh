1089878a9604044892c54f3d6408ec1ffbce62a8
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-10-21 17:28:23 -0400

    Moved clpru setup to install.sh

--- pru/examples/pwm/setup.sh
+++ pru/examples/pwm/setup.sh
@@ -4,16 +4,6 @@
 export PRU_CGT=/usr/share/ti/cgt-pru
 export PRU_SUPPORT=/opt/source/pru-software-support-package
 
-here=$PWD
-
-cd $PRU_CGT
-mkdir -p bin
-cd bin
-ln -s `which clpru`  .
-ln -s `which lnkpru` .
-
-cd $here
-
 HEADER=P8_
 PINS="27 28 29 30 39 40 41 42 43 44 45 46"
 
