6d131d6735be46cef00b9466aebaa956d90849b1
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-10-28 18:07:22 -0400

    Added lines and rectangle

--- displays/oled/test.js
+++ displays/oled/test.js
@@ -2,6 +2,7 @@
 // From: https://www.npmjs.com/package/oled-spi
 
 var oled = require('oled-spi');
+var font = require('oled-font-5x7');
  
 var opts = {
     device: "/dev/spidev2.0",
@@ -27,10 +28,16 @@ oled.begin(function(){
         // [64, 16, 1],
         // [32, 16, 1]
     ]);
-    // // args: (x0, y0, x1, y1, color) 
-    // oled.drawLine(1, 1, 64, 48, 1);
+    // args: (x0, y0, x1, y1, color) 
+    oled.drawLine(32, 16, 32+63, 16+47, 1);
+    oled.drawLine(32+63, 16, 32, 16+47, 1);
     
-    // // oled.invertDisplay(true);
+    // oled.invertDisplay(true);
     
-    // oled.fillRect(1, 1, 10, 20, 1);
+    oled.fillRect(32+32, 16+24, 10, 20, 1);
+    
+    oled.setCursor(1, 1);
+    oled.writeString(font, 1, 'Cats and dogs are really cool animals, you know.', 1, true);
+    
+    // oled.startscroll('left', 0, 15);
 });
