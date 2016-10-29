ae3df1ce8a75e8a4b3e689b66aa6b6dd8560506a
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-10-28 20:59:18 -0400

    Working on 64x48

--- displays/oled/install.sh
+++ displays/oled/install.sh
@@ -1,3 +1,9 @@
+# Data sheet https://cdn.sparkfun.com/assets/learn_tutorials/3/0/8/SSD1306.pdf
+npm install -g oled-spi
+npm install -g oled-font-5x7
 
-npm install oled-spi
-npm install oled-font-5x7
+# Wire  P9_30 to MOSI
+#       P9_31 to SCK
+#       P9_41 to Reset
+#       P9_42 to Data/Command
+#       3.3V  to Chip Select
--- displays/oled/test.js
+++ displays/oled/test.js
@@ -6,8 +6,8 @@ var font = require('oled-font-5x7');
  
 var opts = {
     device: "/dev/spidev2.0",
-    width: 128,
-    height: 64,
+    width: 64,
+    height: 48,
     dcPin: 7,
     rstPin : 20
 };
@@ -16,28 +16,35 @@ var oled = new oled(opts);
 oled.begin(function(){
     // do cool oled things here 
     oled.turnOnDisplay();
+    oled.clearDisplay();
     // draws 4 white pixels total 
     // format: [x, y, color] 
     oled.drawPixel([
-        [ 32,  16, 1],
-        [ 32,  16+47, 1],
-        [ 32+63, 16, 1],
-        [ 32+63, 16+47, 1]
+        [32,  16, 1],
+        [32,  16+47, 1],
+        [32+63, 16, 1],
+        [32+63, 16+47, 1],
+        [ 1,  1, 1],
+        [ 1, 48, 1],
+        [64, 1, 1],
+        [64,48, 1]
         // [64, 1, 1],
         // [64, 32, 1],
         // [64, 16, 1],
         // [32, 16, 1]
     ]);
     // args: (x0, y0, x1, y1, color) 
-    oled.drawLine(32, 16, 32+63, 16+47, 1);
-    oled.drawLine(32+63, 16, 32, 16+47, 1);
+    // oled.drawLine(32, 16, 32+63, 16+47, 1);
+    // oled.drawLine(32+63, 16, 32, 16+47, 1);
     
     // oled.invertDisplay(true);
     
     oled.fillRect(32+32, 16+24, 10, 20, 1);
     
-    oled.setCursor(1, 1);
-    oled.writeString(font, 1, 'Cats and dogs are really cool animals, you know.', 1, true);
+    // oled.setCursor(1, 1);
+    // oled.writeString(font, 1, 'Cats and dogs are really cool animals, you know.', 1, true);
+    
+    // oled.dimDisplay(true);
     
     // oled.startscroll('left', 0, 15);
 });
