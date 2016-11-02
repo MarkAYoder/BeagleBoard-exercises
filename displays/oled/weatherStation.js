c3266789f310b33a8b9dfcc2d8b4b03880b226ea
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-11-01 20:22:31 -0400

    Added time

--- displays/oled/weatherStation.js
+++ displays/oled/weatherStation.js
@@ -31,11 +31,12 @@ var keys = JSON.parse(fs.readFileSync(filename));
 console.log("Title: " + keys.title);
 // console.log(util.inspect(keys));
 
-// Fill these in with two url requests.
+// Fill these in with two url requests.  Don't display until both have returned
 var temperature = null;
 var humidity    = null;
 var weather     = null;
 
+// Get Bedroom data from phant
 var url = keys.outputUrl + "/latest.json";
 request(url, {timeout: 10000}, function (err, res, body) {
     if(err) {
@@ -47,31 +48,33 @@ request(url, {timeout: 10000}, function (err, res, body) {
     // console.log("data: " + data);
     temperature = (data.tempmid*9/5+32).toFixed(1);
     humidity    = (data.humidity*1).toFixed(1);
-    console.log("Temperature: %d, Humidity: %d", temperature, humidity);
+    // console.log("Temperature: %d, Humidity: %d", temperature, humidity);
     
-    if(weather) {
+    if(weather) {   // Display if wunderground has already responded
         displayWeather();
     }
 });
 
+// Get outdoor temp and forcast from wunderground
 var urlWeather = "http://api.wunderground.com/api/ec7eb641373d9256/conditions/forecast/q/IN/Brazil.json";
 request(urlWeather, {timeout: 10000}, function(err, res, body) {
     if(err) {
         console.log("err: " + err);
     }
     weather = JSON.parse(body);
-    console.log("Temp:%s, lo:%s, hi:%s",
-            weather.current_observation.temp_f,
-            weather.forecast.simpleforecast.forecastday[0].low.fahrenheit,
-            weather.forecast.simpleforecast.forecastday[0].high.fahrenheit
-            );
-    if(temperature) {
+    // console.log("Temp:%s, lo:%s, hi:%s",
+    //         weather.current_observation.temp_f,
+    //         weather.forecast.simpleforecast.forecastday[0].low.fahrenheit,
+    //         weather.forecast.simpleforecast.forecastday[0].high.fahrenheit
+    //         );
+    if(temperature) {   // Display if phant has already responded
         displayWeather();
     }
 });
 
+// Called after both phant and wunderground have responded.
 function displayWeather() {
-    console.log('displayWeather()');
+    // console.log('displayWeather()');
     oled.begin(function() {
         var xoff = 32;
         var yoff = 16;
@@ -89,7 +92,14 @@ function displayWeather() {
         oled.setCursor(0+xoff, 32+yoff);
         oled.writeString(font, 1, 
             'hi:  ' + weather.forecast.simpleforecast.forecastday[0].high.fahrenheit, 1, true);
-        setTimeout(off, timeOut);
+        oled.setCursor(0+xoff, 40+yoff);
+        
+        var d = new Date();
+        oled.writeString(font, 1, '      ' + d.getHours() + ':' + d.getMinutes(), 1, true);
+            
+
+            
+        setTimeout(off, timeOut);   // Only leave on for timeOut ms
     }); 
 }
 
