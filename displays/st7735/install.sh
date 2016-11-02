220091371135a93fbcafe5f7eaaa4b99b6f027dd
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-11-02 16:51:16 -0400

    Added P8_45 and P8_46

--- displays/st7735/install.sh
+++ displays/st7735/install.sh
@@ -2,12 +2,12 @@
 # Driver appears to be already installed.
 # Wiring:
 # Vcc   3.3V
-# Gnd
+# Gnd   Gnd
 # SCL   P9_31
 # SDA   P9_30
 # D/C   P9_42
 # RES   P9_41
-# CS    3.3V
+# CS    Gnd
 # Need to unconfigure GPIOs used for Reset and D/C. 
 # Edit /opt/source/bb.org-overlays/src/arm/univ-emmc-00A0.dts and remove references
 # to P9_41 and P9_42 (and P9.41 and P9.42)
@@ -19,7 +19,12 @@ git apply /tmp/removeP9_4142.patch
 make install
 # reboot
 # source setup.sh to config-pin P9_41 and P9_42
+
+# This is for RES to P9_41and D/C to P9_42
 modprobe fbtft_device busnum=2 name=adafruit18 debug=7 verbose=3 gpios=dc:7,reset:20
+
+# This is for RES to P8_45 and D/C to P8_46
+modprobe fbtft_device busnum=2 name=adafruit18 debug=1 verbose=1 gpios=dc:71,reset:70
 # The Adafruit LCD (https://www.adafruit.com/products/358) is 128x160
 
 apt install fbi
