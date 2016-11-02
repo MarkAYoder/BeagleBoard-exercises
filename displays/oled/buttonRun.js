787e91a257ef01bd137d1ad62419053c58e6d57d
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-11-02 07:25:40 -0400

    Added butonRun.js to run a command when a button is pressed

---
+++ displays/oled/buttonRun.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+// Runs a given command when a button is pressed
+
+console.log("Loading bonescript...");
+var b             = require('bonescript');
+console.log("Loading child_process...");
+var exec = require('child_process').exec;
+
+var inputPin = 'P8_45';
+var runMe = "./weatherStation.js";
+
+b.pinMode(inputPin, b.INPUT);
+
+b.attachInterrupt(inputPin, true, b.FALLING, interruptCallback);
+
+if(process.argv.length === 3) {
+    runMe = process.argv[2];
+}
+
+function interruptCallback(x) {
+    if(!x.attached) {
+        // console.log("Callback");
+        exec(runMe, function(err, stdout, stderr) {
+            if(err) {
+                console.log("err: " + err);
+            }
+            if(stderr) {
+                console.log("stderr: " + stderr);
+            }
+            if(stdout) {
+                console.log("stdout: " + stdout);
+            }
+         });
+    }
+}
+
+console.log("Ready to run %s on %s...", runMe, inputPin);
