3e68b229e2c3d860eabea6eb82c18c349d628a0a
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-12-26 11:07:07 -0500

    Works way down json to find all symbols

--- iot/google/googleVision/boggle/boxText.js
+++ iot/google/googleVision/boggle/boxText.js
@@ -10,26 +10,40 @@ console.log(vision.textAnnotations[0].boundingPoly);
 
 // console.log(vision.fullTextAnnotation.pages[0].blocks[0].paragraphs[0].words[0].symbols);
 
+var coordinates = "";
 var pages = vision.fullTextAnnotation.pages;
 for(var p in pages) {
-    console.log("page[%d] = " + pages[p], p);
+    // console.log("page[%d] = " + pages[p], p);
     var blocks = pages[p].blocks;
     for(var b in blocks) {
-        console.log("block[%d] = " + blocks[b], b);
+        // console.log("block[%d] = " + blocks[b], b);
         var paragraphs = blocks[b].paragraphs;
         for(var pg in paragraphs) {
-            console.log("paragraph[%d] = " + paragraphs[pg], pg);
+            // console.log("paragraph[%d] = " + paragraphs[pg], pg);
             var words = paragraphs[pg].words;
             for(var w in words) {
-                console.log("word[%d] = " + words[w], w);
+                // console.log("word[%d] = " + words[w], w);
                 var symbols = words[w].symbols;
                 for(var s in symbols) {
-                    console.log("symbol[%d] = " + util.inspect(symbols[s]), s);
+                    // console.log("symbol[%d] = " + util.inspect(symbols[s]), s);
+                    // console.log("text: %s, boundingBox: " + symbols[s].boundingBox, symbols[s].text);
+                    var vertices = symbols[s].boundingBox.vertices;
+                    var coord = "-draw \"polygon ";
+                    for(var j in vertices) {
+                        coord += vertices[j].x + ',' + vertices[j].y + ' ';
+                    }
+                    coordinates += coord + "\" -pointsize 28 -annotate +" + (vertices[0].x+15.0) + "+" + (vertices[0].y+20.0) + " ";
+                    coordinates += "'"+symbols[s].text+"' ";
+                    // console.log(vertices);
+                    // console.log(coordinates);
                 }
             }
         }
     }
 }
+console.log("convert %s -fill none -stroke black -strokewidth 3 "
+    + "%s "
+    + "tmp.jpg", process.argv[2], coordinates);
 
 // var coordinates = "";
 // for(var i in text) {
@@ -43,6 +57,3 @@ for(var p in pages) {
 //     // console.log(vertices);
 //     console.log(coordinates);
 // }
-// console.log("convert %s -fill none -stroke black -strokewidth 3 "
-//     + "%s "
-//     + "tmp.jpg", process.argv[2], coordinates);
