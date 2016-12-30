a424fdf3357cf75dcb5bb40b5cf63f2fa32a4036
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-12-30 14:53:49 -0500

    Added option to turn off letter by letter boxing

--- iot/google/googleVision/boggle/boxText.js
+++ iot/google/googleVision/boggle/boxText.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const util = require('util');
 const exec = require('child_process').exec;
 
+const all = 0; // Set to 1 to mark each letter
+
 var vision  = JSON.parse(fs.readFileSync(process.argv[3]).toString());
 var vertices = vision.textAnnotations[0].boundingPoly.vertices;
 
@@ -14,37 +16,40 @@ for(var j in vertices) {
 }
 bigBox += "\" ";
 bigBox += "-annotate +0+40 \"" + vision.textAnnotations[0].description + "\" ";
+console.log("\n" + vision.textAnnotations[0].description);
 
 // console.log(bigBox);
 
 // console.log(vision.fullTextAnnotation.pages[0].blocks[0].paragraphs[0].words[0].symbols);
 
 var coordinates = "";
-var count = 0;
-var pages = vision.fullTextAnnotation.pages;
-for(var p in pages) {
-    // console.log("page[%d] = " + pages[p], p);
-    var blocks = pages[p].blocks;
-    for(var b in blocks) {
-        // console.log("block[%d] = " + blocks[b], b);
-        var paragraphs = blocks[b].paragraphs;
-        for(var pg in paragraphs) {
-            // console.log("paragraph[%d] = " + paragraphs[pg], pg);
-            var words = paragraphs[pg].words;
-            for(var w in words) {
-                // console.log("word[%d] = " + words[w], w);
-                var symbols = words[w].symbols;
-                for(var s in symbols) {
-                    var vertices = symbols[s].boundingBox.vertices;
-                    var coord = "-draw \"polygon ";
-                    for(var j in vertices) {
-                        coord += vertices[j].x + ',' + vertices[j].y + ' ';
+if(all) {
+    var count = 0;
+    var pages = vision.fullTextAnnotation.pages;
+    for(var p in pages) {
+        // console.log("page[%d] = " + pages[p], p);
+        var blocks = pages[p].blocks;
+        for(var b in blocks) {
+            // console.log("block[%d] = " + blocks[b], b);
+            var paragraphs = blocks[b].paragraphs;
+            for(var pg in paragraphs) {
+                // console.log("paragraph[%d] = " + paragraphs[pg], pg);
+                var words = paragraphs[pg].words;
+                for(var w in words) {
+                    // console.log("word[%d] = " + words[w], w);
+                    var symbols = words[w].symbols;
+                    for(var s in symbols) {
+                        var vertices = symbols[s].boundingBox.vertices;
+                        var coord = "-draw \"polygon ";
+                        for(var j in vertices) {
+                            coord += vertices[j].x + ',' + vertices[j].y + ' ';
+                        }
+                        coordinates += coord + "\" -pointsize 10 -annotate +" + (vertices[0].x+1.0) + "+" + (vertices[0].y-1.0) + " ";
+                        coordinates += "'"+symbols[s].text+"' ";
+                        count++;
+                        // console.log(vertices);
+                        // console.log(coordinates);
                     }
-                    coordinates += coord + "\" -pointsize 10 -annotate +" + (vertices[0].x+1.0) + "+" + (vertices[0].y-1.0) + " ";
-                    coordinates += "'"+symbols[s].text+"' ";
-                    count++;
-                    // console.log(vertices);
-                    // console.log(coordinates);
                 }
             }
         }
---
+++ iot/google/googleVision/boggle/install.sh
@@ -0,0 +1,3 @@
+# From: https://developers.google.com/sheets/api/quickstart/nodejs
+npm install googleapis --save
+npm install google-auth-library --save
