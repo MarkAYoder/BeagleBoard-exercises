8f4ab25c02c5d92d6de28f050f8d55e445bc5edc
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2016-12-30 17:17:44 -0500

    Each line is now quoted

--- iot/google/googleVision/books/boxText.js
+++ iot/google/googleVision/books/boxText.js
@@ -10,6 +10,9 @@ const all = 0; // Set to 1 to mark each letter
 
 var vision  = JSON.parse(fs.readFileSync(process.argv[3]).toString());
 var vertices = vision.textAnnotations[0].boundingPoly.vertices;
+var text = vision.textAnnotations[0].description;
+console.log(text);
+console.log("Marking %s charcters", text.length);
 
 // console.log(vertices);
 var bigBox = " -fill none -stroke green -strokewidth 1 -draw \"polygon ";
@@ -17,9 +20,7 @@ for(var j in vertices) {
     bigBox += vertices[j].x + ',' + vertices[j].y + ' ';
 }
 bigBox += "\" ";
-bigBox += "-annotate +0+40 \"" + vision.textAnnotations[0].description + "\" ";
-console.log("\n" + vision.textAnnotations[0].description);
-var count = vision.textAnnotations[0].description.length;
+bigBox += "-annotate +0+40 \"" + text + "\" ";
 
 // console.log(bigBox);
 
@@ -27,9 +28,26 @@ var cmd = "convert " + process.argv[2] + bigBox + " frame.jpg";
             
 // console.log(cmd);
 
-console.log("Marking %s charcters", count);
-
 exec(cmd, function(err, stdout, stderr) {
+  if (err) {
+    console.error("exec err: " + err);
+  }
+  if(stdout) {
+    console.log("stdout: " + stdout);
+  }
+  if(stderr) {
+    console.log("stderr: " + stderr);
+  }
+});
+
+// Put each line in "'s so they will appear in it's own cell
+text = text.replace(/^/, "\"");     // Start with a ""
+text = text.replace(/\n/g, '" "');  // End each newline and begin each line
+text = text.replace(/$/, '"');      // End with quote
+// text = text.replace(/'/, "\\'"); // No need to escape ' now
+console.log("text: " + text);
+
+exec("./appendSheet.js " + text, function(err, stdout, stderr) {
   if (err) {
     console.error("exec err: " + err);
   }
