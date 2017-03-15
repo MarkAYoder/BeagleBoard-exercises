5afcd03ad13340d933dcfcc2dfca937293fb316e
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-03-14 20:41:43 -0400

    Playing with firebase

---
+++ iot/google/firebase/getStarted/install.sh
@@ -0,0 +1,5 @@
+# From: https://firebase.google.com/docs/functions/get-started
+npm install -g firebase-tools
+# Had to do twice to get current version 3.5.0
+firebase login
+firebase init functions
