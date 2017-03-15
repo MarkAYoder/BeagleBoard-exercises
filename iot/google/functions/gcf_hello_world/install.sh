769e66fb54767e2a2474ec33fd56fc6986cf475f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-03-15 17:47:47 -0400

    Added --limit

--- iot/google/functions/gcf_hello_world/install.sh
+++ iot/google/functions/gcf_hello_world/install.sh
@@ -8,6 +8,6 @@ gsutil mb -p $PROJECT gs://$BUCKET
 
 gcloud beta functions deploy helloWorld --stage-bucket $BUCKET --trigger-topic hello_world
 gcloud beta functions call   helloWorld --data '{"myMessage":"Hello World!"}'
-gcloud beta functions logs read helloWorld
+gcloud beta functions logs read helloWorld --limit 50
 
 gcloud beta pubsub topics publish hello_world '{"myMessage":"Testing..."}'
