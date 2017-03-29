1ed8a7b2e83b9658c6a7fd3c4ffa44523aa83d80
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-03-29 16:57:58 -0400

    Added BoldPort cordwood driver

---
+++ boldport/cordwoodBlink.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+// Flashes Cordwood LEDs https://www.boldport.com/products/cordwood-puzzle-second-edition/
+
+var b = require('bonescript');
+
+var leds = ["P8_8", "P8_10", "P8_12", "P8_14", "P8_16", "P8_18"];
+
+for(var i in leds) {
+    b.pinMode(leds[i], b.OUTPUT);
+}
+
+var state = b.LOW;
+for(var i in leds) {
+    b.digitalWrite(leds[i], state);
+}
+
+setInterval(toggle, 1000);
+
+function toggle() {
+    if(state == b.LOW) state = b.HIGH;
+    else state = b.LOW;
+    for(var i in leds) {
+        b.digitalWrite(leds[i], state);
+    }
+    console.log("State: ", state);
+}
---
+++ boldport/cordwoodRun.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+// Flashes Cordwood LEDs https://www.boldport.com/products/cordwood-puzzle-second-edition/
+
+var b = require('bonescript');
+
+var leds = ["P8_8", "P8_10", "P8_12", "P8_14", "P8_16", "P8_18"];
+
+for(var i in leds) {
+    b.pinMode(leds[i], b.OUTPUT);
+}
+
+var state = 0;
+for(var i in leds) {
+    b.digitalWrite(leds[i], state);
+}
+
+setInterval(toggle, 250);
+
+function toggle() {
+    b.digitalWrite(leds[state], b.LOW);
+
+    state++;
+    if(state >= leds.length) {
+        state = 0;
+    }
+
+    b.digitalWrite(leds[state], b.HIGH);
+
+}
