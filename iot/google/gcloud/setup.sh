99ac2c1650a7650d086e0b5f90285d906635091f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-03-31 17:32:44 -0400

    Added firewall command

--- iot/google/gcloud/setup.sh
+++ iot/google/gcloud/setup.sh
@@ -2,6 +2,12 @@
 echo Check /etc/hosts for extra entries
 gcloud compute instances list | tail -3 | awk '{print $5 "\t" $1}' | tee /etc/hosts
 
+# Start the instances
+gcloud config set compute/zone us-central1-b
+gcloud compute instances start instance-1 instance-2 instance-3
+# gcloud compute instances stop instance-1 instance-2 instance-3
+
 # Open port 6379 for redis
 
 gcloud compute firewall-rules list
+gcloud compute firewall-rules create redis --allow=tcp:6379
