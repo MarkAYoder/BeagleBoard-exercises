062a958173c19c3c26b3020f53b0a734390d0181
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-04-04 11:31:58 -0400

    Updated paths for user debian

--- setup/installCron.sh
+++ setup/installCron.sh
@@ -2,7 +2,7 @@
 echo "
 # Turn off LED triggers at night
 NODE_PATH=/usr/local/lib/node_modules
-58 5  * * *   root    /root/exercises/setup/ledTrigger.js
-2 21  * * *   root    /root/exercises/setup/ledTrigger.js off
-* * * * * root /root/exercises/iot/phant/recordPing.js 2>&1 | logger
+58 5  * * *   root    /home/debian/exercises/setup/ledTrigger.js
+2 21  * * *   root    /home/debian/exercises/setup/ledTrigger.js off
+* * * * * root /home/debian/exercises/iot/phant/recordPing.js 2>&1 | logger
 " >> /etc/crontab
