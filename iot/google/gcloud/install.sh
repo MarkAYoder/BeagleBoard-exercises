06a84aff6963dea2e02839ac9229113d21c852c7
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-04-06 17:26:27 -0400

    Added install commands for bone

--- iot/google/gcloud/install.sh
+++ iot/google/gcloud/install.sh
@@ -1,1 +1,10 @@
 # From: https://cloud.google.com/sdk/docs/
+
+SDKz=google-cloud-sdk-150.0.0-linux-x86.tar.gz
+SDK=google-cloud-sdk
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$SDKz
+tar xzf $SDKz
+cd $SDK
+./install.sh
+gcloud components install alpha
+gcloud init
