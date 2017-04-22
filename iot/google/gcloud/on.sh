007be53cac7f54708b60c11775cd0c9d72659f1b
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-04-22 13:30:47 -0400

    Switched to starting only one instance

--- iot/google/gcloud/on.sh
+++ iot/google/gcloud/on.sh
@@ -3,5 +3,6 @@
 gcloud alpha billing accounts projects link trim-approach-136823 --account-id=007BE9-25D7D3-F8BBDD
 
 # Start the instances
-gcloud compute instances start instance-1 instance-2 instance-3 instance-4
+# gcloud compute instances start instance-1 instance-2 instance-3 instance-4
+gcloud compute instances start instance-1
 
