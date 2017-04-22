76d58629e0fe9c19eba1ebf2797c2a7fbd7d8b1c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-04-22 13:30:19 -0400

    Updated version to 152.0.0

--- iot/google/gcloud/install.sh
+++ iot/google/gcloud/install.sh
@@ -1,6 +1,6 @@
 # From: https://cloud.google.com/sdk/docs/
 
-SDKz=google-cloud-sdk-150.0.0-linux-x86.tar.gz
+SDKz=google-cloud-sdk-152.0.0-linux-x86.tar.gz
 SDK=google-cloud-sdk
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$SDKz
 tar xzf $SDKz
