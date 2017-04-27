adf01f2d5c0a3ec2e5587f793db888cb3c493922
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-04-27 17:20:25 -0400

    Initial version

---
+++ iot/twitter/sentiment.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+// From https://www.npmjs.com/package/twitter
+// See hhttps://dev.twitter.com/rest/public
+// to see what you can request
+
+var Twitter = require('twitter');
+
+// const screen_name = 'MarkAYoder';
+const screen_name = 'realDonaldTrump';
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    });
+
+'use strict';
+
+// [START language_quickstart]
+// Imports the Google Cloud client library
+const Language = require('@google-cloud/language');
+
+// Your Google Cloud Platform project ID
+const projectId = 'trim-approach-136823';
+
+// Instantiates a client
+const language = Language({
+  projectId: projectId
+});
+
+// Get timeline for a given user
+var opts = {screen_name: screen_name, count: 10};
+client.get('statuses/user_timeline', opts,  function(error, params, response) {
+    console.log(error);
+    if(error) throw error;
+    // console.log(params);  // Tweet body
+    var i;
+    for(i=0; i<params.length; i++) {
+        console.log(i + ": " + params[i].created_at + " " + params[i].text);   // Text only
+        // console.log();   // Text only
+        
+        // Detects the sentiment of the text
+        language.detectSentiment(params[i].text)
+          .then((results) => {
+            const sentiment = results[0];
+        
+            console.log(`Text: ${sentiment}`);
+            console.log(`Sentiment score: ${sentiment.score}`);
+            console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+          })
+          .catch((err) => {
+            console.error('ERROR:', err);
+          });
+    }
+});
