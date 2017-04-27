fec4a387d2e99a7147abb1bdd2c4e6ad56c7f3c2
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-04-27 16:56:13 -0400

    Added strams.js

---
+++ iot/twitter/streams.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+// From: https://github.com/desmondmorris/node-twitter/tree/master/examples#streams
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+/**
+ * Stream statuses filtered by keyword
+ * number of tweets per second depends on topic popularity
+ **/
+client.stream('statuses/filter', {track: 'twitter'},  function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
