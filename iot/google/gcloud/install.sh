dd2019ab109d650b5347240dfe550565532047e7
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-04-27 16:38:27 -0400

    Added nlp quickstart

--- iot/google/gcloud/install.sh
+++ iot/google/gcloud/install.sh
@@ -1,6 +1,6 @@
 # From: https://cloud.google.com/sdk/docs/
 
-SDKz=google-cloud-sdk-150.0.0-linux-x86.tar.gz
+SDKz=google-cloud-sdk-153.0.0-linux-x86.tar.gz
 SDK=google-cloud-sdk
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$SDKz
 tar xzf $SDKz
@@ -8,3 +8,11 @@ cd $SDK
 ./install.sh
 gcloud components install alpha
 gcloud init
+
+# From: https://cloud.google.com/natural-language/docs/reference/libraries#client-libraries-resources-nodejs
+
+sudo npm install --save @google-cloud/language
+gcloud auth application-default login
+
+wget https://raw.githubusercontent.com/GoogleCloudPlatform/nodejs-docs-samples/master/language/quickstart.js
+# Edit const projectId =
