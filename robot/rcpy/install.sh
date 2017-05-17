08e20631f34c016db28d4d08219db31a6b64deb1
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-05-17 14:45:51 -0400

    Adding install info for rcpy

---
+++ robot/rcpy/install.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/mcdeoliveira/rcpy
+
+
+sudo apt update
+sudo apt install python3 python3-setuptools python3-dev
+
+cd rcpy
+sudo python3 setup.py install
+# Exits with error
