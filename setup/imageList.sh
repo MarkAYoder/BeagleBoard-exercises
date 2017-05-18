a313549379e58b8b6de9c4f7d9a523e2d9e00a8c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-05-17 20:19:38 -0400

    Pass parameter to fetch given image

--- setup/imageList.sh
+++ setup/imageList.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
 # Goes to rcn-ee to list current images
+# Run without parameter to list what available
+# imageList.sh 2017-05-14 to get the 2017-05-14 iot image
 SITE=https://rcn-ee.com/rootfs/bb.org/testing/ 
-google-chrome $SITE
-curl $SITE | sed -n '/^$/!{s/<[^>]*>//g;p;}' 
-echo $SITE
+RELEASE=stretch-iot
+if [ "$1" == "" ]; then
+	google-chrome $SITE
+	curl $SITE | sed -n '/^$/!{s/<[^>]*>//g;p;}' 
+	echo $SITE
+else
+	echo $1
+	     wget $SITE/$1/$RELEASE/bone-debian-$RELEASE-armhf-$1-4gb.bmap
+	time wget $SITE/$1/$RELEASE/bone-debian-$RELEASE-armhf-$1-4gb.img.xz
+fi
+
