e03e15138520fef294023007550a339a8c4060d5
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-05-22 15:06:20 -0400

    Added more instructions

--- robot/rcpy/install.sh
+++ robot/rcpy/install.sh
@@ -1,9 +1,14 @@
+git clone https://github.com/mcdeoliveira/Robotics_Cape_Installer.git
 git clone https://github.com/mcdeoliveira/rcpy
 
 
 sudo apt update
 sudo apt install python3 python3-setuptools python3-dev
 
-cd rcpy
+cd Robotics_Cape_Installer
+git checkout devel
+sudo ./install.sh
+
+cd ../rcpy
+git checkout devel
 sudo python3 setup.py install
-# Exits with error
