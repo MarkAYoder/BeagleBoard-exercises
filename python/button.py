b59045159fb0a5a139f27f479087e4a8eabf95f5
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-05-25 15:16:09 -0400

    Reads pause button and sets LED

---
+++ python/button.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Pin table at https://github.com/beagleboard/beaglebone-blue/blob/master/BeagleBone_Blue_Pin_Table.csv
+
+# Import PyBBIO library:
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+button="P8_9"
+LED="USR3"
+
+# Set the GPIO pins:
+GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(button, GPIO.IN)
+
+print("Running...")
+
+while True:
+  state = GPIO.input(button)
+  GPIO.output(LED, state)
+  
+  time.sleep(0.1)
