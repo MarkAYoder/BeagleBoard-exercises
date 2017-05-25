6ada2987a02820cff7f0158a9925cd4a9c7e79a5
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-05-25 15:27:36 -0400

    Cleaning up

--- python/button.py
+++ python/button.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Reads the PAUSE button and sets the LED
 # Pin table at https://github.com/beagleboard/beaglebone-blue/blob/master/BeagleBone_Blue_Pin_Table.csv
 
 # Import PyBBIO library:
@@ -6,10 +7,10 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 
 button="P8_9"
-LED="USR3"
+LED   ="USR3"
 
 # Set the GPIO pins:
-GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(LED,    GPIO.OUT)
 GPIO.setup(button, GPIO.IN)
 
 print("Running...")
