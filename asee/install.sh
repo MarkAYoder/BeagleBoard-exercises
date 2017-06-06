cf4b0b593480d4d3bc46475ad9939c4c09f91874
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-06-06 09:19:45 -0400

    Updated date

--- asee/install.sh
+++ asee/install.sh
@@ -2,7 +2,7 @@
 # Here are the extra things I install on the bone for the ASEE workshop.
 # Run this on the host computer with the BeagleBone Blue attached via USB
 # --Mark
-# 25-May-2017
+# 6-June-2017
 
 ln -s /opt/source/Robotics_Cape_Installer/ .
 ln -s /opt/source/rcpy/ .
