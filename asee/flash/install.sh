fb4835e4ce9c535c40be6593c99c265ce6d08564
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-06-06 15:27:10 -0400

    Playing with flash and socket.io

---
+++ asee/flash/install.sh
@@ -0,0 +1,7 @@
+# From https://blog.miguelgrinberg.com/post/easy-websockets-with-flask-and-gevent
+git clone https://github.com/miguelgrinberg/Flask-SocketIO
+cd Flask-SocketIO/example
+sudo apt install python3-pip
+pip3 install -r requirements.txt
+python3 app.py
+
