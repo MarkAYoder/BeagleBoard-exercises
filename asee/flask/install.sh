32b1b3b8d46accef357744da07d03db8cf02e93d
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-06-06 19:21:06 -0400

    Added host='0.0.0.0' so it will work on Bone

--- asee/flask/install.sh
+++ asee/flask/install.sh
@@ -3,5 +3,7 @@ git clone https://github.com/miguelgrinberg/Flask-SocketIO
 cd Flask-SocketIO/example
 sudo apt install python3-pip
 pip3 install -r requirements.txt
+# Edit app.py to include: socketio.run(app, host='0.0.0.0')
+
 python3 app.py
 
