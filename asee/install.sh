02a8205cd10fa08051dd54c32f3f322001884474
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-06-06 09:17:53 -0400

    Updated for 2017-06-04 build

--- asee/install.sh
+++ asee/install.sh
@@ -4,24 +4,18 @@
 # --Mark
 # 25-May-2017
 
-# git clone https://github.com/mcdeoliveira/Robotics_Cape_Installer.git
-# git clone https://github.com/mcdeoliveira/rcpy
-# git clone https://github.com/mcdeoliveira/ctrl.git
-
-git clone https://github.com/mcdeoliveira/pyctrl.git
-
 ln -s /opt/source/Robotics_Cape_Installer/ .
 ln -s /opt/source/rcpy/ .
 
 sudo apt update
 sudo apt install python3-numpy
 
-# cd Robotics_Cape_Installer
-# git checkout devel
-# sudo ./install.sh
-
-# cd ../rcpy
-# sudo python3 setup.py install
+git clone https://github.com/mcdeoliveira/pyctrl.git
 
 cd ../pyctrl
 sudo python3 setup.py install
+
+EXAMPLES=/var/lib/cloud9/examples/robot
+mkdir -p $EXAMPLES
+cd $EXAMPLES
+ln -s ~/asee python
