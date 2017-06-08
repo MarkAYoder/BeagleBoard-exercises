04eba26060922bca304f6ac078da4b48f370557e
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-06-08 15:03:10 +0000

    Added wget's to keep everything local.  Switched title

--- robot/flask/install.sh
+++ robot/flask/install.sh
@@ -6,5 +6,10 @@ sudo apt install python3-pip
 pip3 install -r requirements.txt
 # Edit app.py to include: socketio.run(app, host='0.0.0.0')
 
-python3 app.py
+cd templates
+wget https://code.jquery.com/jquery-3.2.1.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.2/socket.io.slim.js
+
+cd ..
+python3 robot.py
 
