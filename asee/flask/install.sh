f9516e46ee083eab5d72b0be935379b1f6a2fb43
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-06-08 10:18:59 -0400

    Added apt update

--- asee/flask/install.sh
+++ asee/flask/install.sh
@@ -1,6 +1,7 @@
 # From https://blog.miguelgrinberg.com/post/easy-websockets-with-flask-and-gevent
 git clone https://github.com/miguelgrinberg/Flask-SocketIO
 cd Flask-SocketIO/example
+sudo apt update
 sudo apt install python3-pip
 pip3 install -r requirements.txt
 # Edit app.py to include: socketio.run(app, host='0.0.0.0')
