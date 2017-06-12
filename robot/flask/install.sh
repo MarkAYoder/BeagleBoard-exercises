c20fe9cfe742cf400afd200d6ef4baa76401386e
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-06-12 14:02:35 -0400

    Added wget to get requirements

--- robot/flask/install.sh
+++ robot/flask/install.sh
@@ -1,10 +1,10 @@
 # From https://blog.miguelgrinberg.com/post/easy-websockets-with-flask-and-gevent
-git clone https://github.com/miguelgrinberg/Flask-SocketIO
-cd Flask-SocketIO/example
+# git clone https://github.com/miguelgrinberg/Flask-SocketIO
 sudo apt update
 sudo apt install python3-pip
+
+wget https://github.com/miguelgrinberg/Flask-SocketIO/raw/master/example/requirements.txt
 pip3 install -r requirements.txt
-cd ../..
 
 # We need local copies of this files since we can't depend on having exernal Internet
 mkdir -p static
