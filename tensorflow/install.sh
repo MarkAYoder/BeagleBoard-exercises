e4c1b898169d9493a1bada0c622940680ff4ba34
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-07-20 14:47:28 -0400

    Added instructions

--- tensorflow/install.sh
+++ tensorflow/install.sh
@@ -1,1 +1,6 @@
 https://www.tensorflow.org/install/install_linux
+
+sudo apt-get install python3-pip python3-dev python-virtualenv # for Python 3.n
+virtualenv --system-site-packages -p python3 tensorflow # for Python 3.n
+source tensorflow/bin/activate
+pip3 install --upgrade tensorflow     # for Python 3.n
