2a6a2c6348f94ea4071cda2126ac350f33c39a60
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-07-22 17:07:00 -0400

    Added mnist examples

--- tensorflow/install.sh
+++ tensorflow/install.sh
@@ -4,3 +4,11 @@ sudo apt-get install python3-pip python3-dev python-virtualenv # for Python 3.n
 virtualenv --system-site-packages -p python3 tensorflow # for Python 3.n
 source tensorflow/bin/activate
 pip3 install --upgrade tensorflow     # for Python 3.n
+
+
+# From: https://www.tensorflow.org/get_started/mnist/beginners
+wget https://github.com/tensorflow/tensorflow/raw/r1.2/tensorflow/examples/tutorials/mnist/mnist_softmax.py
+
+# From: https://www.tensorflow.org/get_started/mnist/pros
+wget https://github.com/tensorflow/tensorflow/raw/r1.2/tensorflow/examples/tutorials/mnist/mnist_deep.py
+
