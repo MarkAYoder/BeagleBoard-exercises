0f83dde93feffaa356be443a589c85a6275e400d
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-07-27 18:17:36 -0400

    Added VERSION and set it to 9.1

--- setup/imageList.sh
+++ setup/imageList.sh
@@ -2,8 +2,9 @@
 # Goes to rcn-ee to list current images
 # Run without parameter to list what available
 # imageList.sh 2017-05-14 to get the 2017-05-14 iot image
-SITE=https://rcn-ee.com/rootfs/bb.org/testing/ 
+SITE=https://rcn-ee.com/rootfs/bb.org/testing 
 RELEASE=stretch-iot
+VERSION=9.1
 if [ "$1" == "" ]; then
 	curl $SITE | sed -n '/^$/!{s/<[^>]*>//g;p;}' | awk -F/ '{print $1}'
 	echo $SITE
@@ -12,7 +13,7 @@ elif [ "$1" == "web" ]; then
 else
 	echo $1
 	cd ~/BeagleBoard/Images
-	     URL=$SITE/$1/$RELEASE/bone-debian-9.0-iot-armhf-$1-4gb
+	     URL=$SITE/$1/$RELEASE/bone-debian-$VERSION-iot-armhf-$1-4gb
 	     wget $URL.bmap
 	     wget $URL.img.xz.sha256sum
 	time wget $URL.img.xz
