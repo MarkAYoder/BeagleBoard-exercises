340d1dd743e288d87f8245a98bd1950c5191ab21
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-08-28 20:57:25 -0400

    Added instance-16

--- iot/google/gcloud/off.sh
+++ iot/google/gcloud/off.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Stop the instances
-gcloud compute instances stop instance-1 instance-2 instance-3 instance-4
+# gcloud compute instances stop instance-1 instance-2 instance-3 instance-4
+gcloud compute instances stop instance-16
 
 # Turn off billing
 gcloud alpha billing accounts projects unlink trim-approach-136823
