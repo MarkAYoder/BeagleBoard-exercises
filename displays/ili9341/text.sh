353e0013e75f5ca9da173d9984593f9396af2550
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-02 15:56:27 -0400

    Added text at bottom

--- displays/ili9341/text.sh
+++ displays/ili9341/text.sh
@@ -4,10 +4,12 @@ SIZE=320x240
 TMP_FILE=/tmp/frame.png
 
 # From: http://www.imagemagick.org/Usage/text/
-convert -background lightblue -fill blue -font Times-Roman -pointsize 36 \
+convert -background lightblue -fill blue -font Times-Roman -pointsize 24 \
       -size $SIZE \
-      -geometry +100+100 \
       label:'ImageMagick\nExamples\nby Anthony' \
+      -draw "text 0,200 'Bottom of Display'" \
       $TMP_FILE
 
 sudo fbi -noverbose -T 1 $TMP_FILE
+
+# convert -list font
