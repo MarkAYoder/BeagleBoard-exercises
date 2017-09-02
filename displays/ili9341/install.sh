dda822bf5b601fd4edc66320c439fbe59944c3fd
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-02 13:44:27 -0400

    split into install and setup

--- displays/ili9341/install.sh
+++ displays/ili9341/install.sh
@@ -1,36 +1,36 @@
 #!/bin/sh
 # From: https://gist.github.com/jadonk/0e4a190fc01dc5723d1f183737af1d83
 
-sudo bash << EOF
-    rmmod --force fb_ili9341
-    rmmod --force fbtft
-    rmmod --force fbtft_device
+# From: https://github.com/notro/fbtft/wiki/Framebuffer-use
+# This is for controlling the framebuffer
+apt install fbset
 
-    echo 49 > /sys/class/gpio/unexport # RESET - V14 - GP0_PIN4
-    echo 57 > /sys/class/gpio/unexport # D/C - U16 - GP0_PIN3
-    
-    echo 29 > /sys/class/gpio/unexport # CS - H18
-    echo gpio > /sys/devices/platform/ocp/ocp\:P9_23_pinmux/state # RESET - V14 - GP0_4
-    echo gpio > /sys/devices/platform/ocp/ocp\:U16_pinmux/state # D/C - U16 - GP0_3
-    echo spi > /sys/devices/platform/ocp/ocp\:P9_31_pinmux/state # SCLK - A13 - S1.1_5
-    echo spi > /sys/devices/platform/ocp/ocp\:P9_29_pinmux/state # MISO - B13 - S1.1_4
-    echo spi > /sys/devices/platform/ocp/ocp\:P9_30_pinmux/state # MOSI - D12 - S1.1_3
-    echo spi > /sys/devices/platform/ocp/ocp\:H18_pinmux/state # CS - H18 - S1.1_6
-    sleep 2
-    modprobe fbtft_device name=adafruit28 busnum=1 rotate=90 gpios=reset:49,dc:57
-EOF
-
+# Get framebuffer imageviewer
 apt install fbi
 
+# Get some images to work on
 wget https://kernel.org/theme/images/logos/tux.png
 wget http://www.rose-hulman.edu/InstituteBrandResources/RH_Graphic_Secondary.zip
 
-export FRAMEBUFFER=/dev/fb0
-
-fbi -noverbose -T 1 -a tux.png
-fbi -t 5 -blend 1000 -noverbose -T 1 -a Matthias.jpg Malachi.jpg Alan.jpg Louis.jpg
-
+# Get mplayer to play some video
 sudo apt install mplayer
  
+ # Get some sample video
 wget http://hubblesource.stsci.edu/sources/video/clips/details/images/hst_1.mpg
 wget http://www.koeniglich.de/pics/RedsNightmare.mpg
+
+# 1.  Solder the two (2) JST SH 6 Wire Assemblies to the 2.4 TFT Breakout with: 
+
+# JST Pin	TFT Pin
+# 1-1 GND
+# 1-2	Vin
+# 1-3	MOSI
+# 1-4	MISO
+# 1-5	CLK
+# 1-6	CS
+# 2-3	D/C
+# 2-4	RST
+
+# 2.  Connect	JST-1 to S1.1 connector on BeagleBone Blue.
+
+# 3.  Connect JST-2 to GP0 connector on BeagleBone Blue.
---
+++ displays/ili9341/setup.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# From: https://gist.github.com/jadonk/0e4a190fc01dc5723d1f183737af1d83
+
+# Connect the display before running this.
+
+sudo bash << EOF
+    # remove the framebuffer modules
+    rmmod --force fb_ili9341
+    rmmod --force fbtft
+    rmmod --force fbtft_device
+
+    # unexport poins 49 and 57 so the framebuffer can use them
+    echo 49 > /sys/class/gpio/unexport # RESET - V14 - GP0_PIN4
+    echo 57 > /sys/class/gpio/unexport # D/C - U16 - GP0_PIN3
+    echo 29 > /sys/class/gpio/unexport # CS - H18
+        
+    # Set the pinmuxes for the display
+    echo gpio > /sys/devices/platform/ocp/ocp\:P9_23_pinmux/state # RESET - V14 - GP0_4
+    echo gpio > /sys/devices/platform/ocp/ocp\:U16_pinmux/state # D/C - U16 - GP0_3
+    echo spi > /sys/devices/platform/ocp/ocp\:P9_31_pinmux/state # SCLK - A13 - S1.1_5
+    echo spi > /sys/devices/platform/ocp/ocp\:P9_29_pinmux/state # MISO - B13 - S1.1_4
+    echo spi > /sys/devices/platform/ocp/ocp\:P9_30_pinmux/state # MOSI - D12 - S1.1_3
+    echo spi > /sys/devices/platform/ocp/ocp\:H18_pinmux/state # CS - H18 - S1.1_6
+    sleep 2
+    
+    # Insert the framebuffer modules
+    modprobe fbtft_device name=adafruit28 busnum=1 rotate=90 gpios=reset:49,dc:57
+EOF
+
+# 
+export FRAMEBUFFER=/dev/fb0
+
+# Display an image
+fbi -noverbose -T 1 -a tux.png
+# Cycle between several images
+fbi -t 5 -blend 1000 -noverbose -T 1 -a Matthias.jpg Malachi.jpg Alan.jpg Louis.jpg
+
+# Play a movie
+SDL_VIDEODRIVER=fbcon 
+SDL_FBDEV=/dev/fb0
+mplayer -vf-add rotate=2 -framedrop hst_1.mpg
+
+# Look at the framebuffer settings
+fbset
