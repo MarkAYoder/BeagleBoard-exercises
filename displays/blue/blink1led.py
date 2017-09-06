48889def8b6687a056f9fb1b37ad36ffa6230d73
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-06 12:51:19 -0400

    Added delay variable

--- displays/blue/blink1led.py
+++ displays/blue/blink1led.py
@@ -2,12 +2,13 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-LED = "USR0"; 
+LED = "USR0"
+delay = 0.25
 
 GPIO.setup(LED, GPIO.OUT)
 
 while True:
     GPIO.output(LED, 1)
-    time.sleep(0.25)
+    time.sleep(delay)
     GPIO.output(LED, 0)
-    time.sleep(0.25)
+    time.sleep(delay)
