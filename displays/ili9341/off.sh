accaaa8c08fb939290bd1ebaba7a00923d2a90ec
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-06 13:54:04 -0400

    Separated on.sh and off.sh from setup.sh.  Rotated for RedsNightmare.mpg

---
+++ displays/ili9341/off.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# From: https://gist.github.com/jadonk/0e4a190fc01dc5723d1f183737af1d83
+
+# Connect the display before running this.
+
+sudo bash << EOF
+    # remove the framebuffer modules
+    rmmod --force fb_ili9341
+    rmmod --force fbtft
+    rmmod --force fbtft_device
+
+    # unexport poins 49 and 57 so the framebuffer can use them
+    echo 49 > /sys/class/gpio/unexport # RESET - V14 - GP0_PIN4
+    echo 57 > /sys/class/gpio/unexport # D/C - U16 - GP0_PIN3
+    echo 29 > /sys/class/gpio/unexport # CS - H18
+        
+EOF
