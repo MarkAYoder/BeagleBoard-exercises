57ee8d8532db422088a0a34d235f049682f371a9
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-07 17:25:32 -0400

    Switched to real events

--- displays/blue/buttonEvent.py
+++ displays/blue/buttonEvent.py
@@ -21,18 +21,36 @@ GPIO.setup(buttonM, GPIO.IN)
 GPIO.output(LEDp, 1)
 GPIO.output(LEDm, 1)
 
+def updateLED(channel):
+    print("channel = " + channel)
+    state = GPIO.input(channel)
+    if(channel == 'PAUSE'):
+      LED = LEDp
+    else:
+      LED = LEDm
+    GPIO.output(LED, state)
+    print(LED + " Toggled")
+
 print("Running...")
 
-GPIO.add_event_detect(buttonP, GPIO.BOTH) # RISING, FALLING or BOTH
-GPIO.add_event_detect(buttonM, GPIO.BOTH)
+GPIO.add_event_detect(buttonP, GPIO.BOTH, callback=updateLED) # RISING, FALLING or BOTH
+GPIO.add_event_detect(buttonM, GPIO.BOTH, callback=updateLED)
 
-while True:   # This is ugly since we have to poll for the event
-  if GPIO.event_detected(buttonP):
-    state = GPIO.input(buttonP)
-    GPIO.output(LEDp, state)
-    print(LEDp + " Toggled")
+# while True:   # This is ugly since we have to poll for the event
+#   if GPIO.event_detected(buttonP):
+#     state = GPIO.input(buttonP)
+#     GPIO.output(LEDp, state)
+#     print(LEDp + " Toggled")
     
-  if GPIO.event_detected(buttonM):
-    state = GPIO.input(buttonM)
-    GPIO.output(LEDm, state)
-    print(LEDm + " Toggled")
+#   if GPIO.event_detected(buttonM):
+#     state = GPIO.input(buttonM)
+#     GPIO.output(LEDm, state)
+#     print(LEDm + " Toggled")
+
+try:
+    while True:
+        pass
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+GPIO.cleanup()
