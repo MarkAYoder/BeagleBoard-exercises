682f248a33d222912016fff4d39b51c80f9f4408
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-09 14:22:17 -0400

    Added python examples

--- iot/blynk/install.sh
+++ iot/blynk/install.sh
@@ -1,3 +1,9 @@
 # From: http://docs.blynk.cc/
 
 sudo npm install -g onoff blynk-library
+
+# This doesn't work with python3
+sudo pip3 install blynkapi
+
+# This look more promising
+git clone https://github.com/BLavery/PiBlynk.git
