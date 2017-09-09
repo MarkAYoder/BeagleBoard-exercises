0275cca641ed3971d06f757029f8895d4953d263
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-09 16:51:16 -0400

    Get jpg's for each tweet

--- iot/twitter/timeLineMedia.js
+++ iot/twitter/timeLineMedia.js
@@ -7,8 +7,9 @@ var Twitter = require('twitter');
 const util = require('util');
 const exec = require('child_process').exec;
 
-const screen_name = 'MarkAYoder';
+// const screen_name = 'MarkAYoder';
 // const screen_name = 'realDonaldTrump';
+const screen_name = 'anneforsure';
 
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -18,29 +19,32 @@ var client = new Twitter({
     });
 
 // Get timeline for a given user
-var opts = {screen_name: screen_name, count: 5};
+var opts = {screen_name: screen_name, count: 10};
 client.get('statuses/user_timeline', opts,  function(error, params, response) {
     if(error) {
         console.log(error);
         throw error;
     }
-
     // console.log(params);  // Tweet body
-    // console.log(util.inspect(params[1].entities));
-    // console.log(util.inspect(params[1].entities.media[0].media_url_https));
-    const cmd = 'wget ' + params[1].entities.media[0].media_url_https + ' -O /tmp/twitter.jpg';
-    console.log("Running: " + cmd);
-    exec(cmd, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-    //   console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    });
-    // var i;
-    // for(i=0; i<params.length; i++) {
-    //     console.log(i + ": " + params[i].created_at + " " + params[i].text);   // Text only
-    //     // console.log();   // Text only
-    // }
+    var i;
+    for(i=0; i<params.length; i++) {
+        console.log(i + ": " + params[i].created_at + " " + params[i].text);   // Text only
+        // console.log();   // Text only
+        // console.log(util.inspect(params[i].entities));
+        if(params[i].entities.media) {
+            // console.log(util.inspect(params[i].entities.media[0].media_url_https));
+            const cmd = 'wget ' + params[i].entities.media[0].media_url_https + 
+                            ' -O /tmp/twitter' + i + '.jpg';
+            console.log("Running: " + cmd);
+            exec(cmd, (error, stdout, stderr) => {
+              if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+              }
+            //   console.log(`stdout: ${stdout}`);
+              console.log(`stderr: ${stderr}`);
+            });
+        }
+
+    }
 });
