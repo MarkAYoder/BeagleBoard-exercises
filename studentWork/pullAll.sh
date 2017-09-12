2be892df319edf5fbd74663cb87d69fbdc94fb69
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-12 09:34:46 -0400

    Made dir print in red

--- studentWork/pullAll.sh
+++ studentWork/pullAll.sh
@@ -4,7 +4,7 @@ for dir in *
 do
     if [ -d $dir ] && [ "${dir}" != "Old" ]
     then
-	echo $dir
+	echo -e "\e[31m$dir\e[00m"
 	cd $dir
 	git pull
 	cd ..
--- studentWork/statusAll.sh
+++ studentWork/statusAll.sh
@@ -2,7 +2,7 @@
 # Updates all student repos
 for dir in *; do
     if [ -d "$dir" ]; then
-        echo $dir
+        echo -e "\e[31m$dir\e[00m"
         cd "$dir"
         git status
         cd ..
