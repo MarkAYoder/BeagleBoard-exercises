fd023e54ebaae5412e89f4b19ec4ff23813790bc
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-13 11:43:47 -0400

    Added battery and WIFI leads and moved to better names

--- displays/blue/blinkLEDs.py
+++ displays/blue/blinkLEDs.py
@@ -3,13 +3,16 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 
 LEDs = ["USR0", "USR1", "USR2", "USR3", 
-            "GP0_4", "GP0_5", "GP0_6",
+            "GP0_3", "GP0_5", "GP0_6",
             "GP1_3", "GP1_4",
-            "RED", "GREEN"
+            "RED", "GREEN",
+            "BAT25", "BAT50", "BAT75", "BAT100",
+            "WIFI"
             ]
 delay = 0.25
 
 for LED in LEDs:
+    print(LED)
     GPIO.setup(LED, GPIO.OUT)
 
 while True:
