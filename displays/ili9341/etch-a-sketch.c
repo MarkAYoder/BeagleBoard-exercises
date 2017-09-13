23ff0fac905e8f04ce21c518b19737f4df79d2b8
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-13 15:17:15 -0400

    Very simple etch-a-sketch working, but needs work

---
+++ displays/ili9341/etch-a-sketch.c
@@ -0,0 +1,119 @@
+/*
+To test that the Linux framebuffer is set up correctly, and that the device permissions
+are correct, use the program below which opens the frame buffer and draws a gradient-
+filled red square:
+
+retrieved from:
+Testing the Linux Framebuffer for Qtopia Core (qt4-x11-4.2.2)
+
+http://cep.xor.aps.anl.gov/software/qt4-x11-4.2.2/qtopiacore-testingframebuffer.html
+*/
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
+#include "/opt/source/Robotics_Cape_Installer/libraries/rc_usefulincludes.h"
+#include "/opt/source/Robotics_Cape_Installer/libraries/roboticscape.h"
+
+int main()
+{
+    int fbfd = 0;
+    struct fb_var_screeninfo vinfo;
+    struct fb_fix_screeninfo finfo;
+    long int screensize = 0;
+    char *fbp = 0;
+    int x = 0, y = 0;
+    int xold = 0, yold = 0;
+    long int location = 0;
+
+    // Open the file for reading and writing
+    fbfd = open("/dev/fb0", O_RDWR);
+    if (fbfd == -1) {
+        perror("Error: cannot open framebuffer device");
+        exit(1);
+    }
+    printf("The framebuffer device was opened successfully.\n");
+
+    // Get fixed screen information
+    if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
+        perror("Error reading fixed information");
+        exit(2);
+    }
+
+    // Get variable screen information
+    if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
+        perror("Error reading variable information");
+        exit(3);
+    }
+
+    printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
+    printf("Offset: %dx%d, line_length: %d\n", vinfo.xoffset, vinfo.yoffset, finfo.line_length);
+    
+    if (vinfo.bits_per_pixel != 16) {
+        printf("Can't handle %d bpp, can only do 16.\n", vinfo.bits_per_pixel);
+        exit(5);
+    }
+
+    // Figure out the size of the screen in bytes
+    screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
+
+    // Map the device to memory
+    fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
+    if ((int)fbp == -1) {
+        perror("Error: failed to map framebuffer device to memory");
+        exit(4);
+    }
+    printf("The framebuffer device was mapped to memory successfully.\n");
+
+    // initialize hardware first
+	if(rc_initialize()){
+		fprintf(stderr,"ERROR: failed to run rc_initialize(), are you root?\n");
+		return -1;
+	}
+
+	printf("\nRaw encoder positions\n");
+	printf("   E1   |");
+	printf("   E2   |");
+	printf("   E3   |");
+	printf("   E4   |");
+	printf(" \n");
+
+	while(rc_get_state() != EXITING) {
+		printf("\r");
+		for(int i=1; i<=4; i++){
+			printf("%6d  |", rc_get_encoder_pos(i));
+		}
+		fflush(stdout);
+        // Update framebuffer
+        // Figure out where in memory to put the pixel
+        x = rc_get_encoder_pos(2)/5 % vinfo.xres;
+        y = rc_get_encoder_pos(3)/5 % vinfo.yres;
+        
+        if((x != xold) || (y != yold)) {
+            printf("Updating location to %d, %d\n", x, y);
+            xold = x;
+            yold = y;
+            location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
+                       (y+vinfo.yoffset) * finfo.line_length;
+    
+            int r = 0;     // 5 bits
+            int g = 0;      // 6 bits
+            int b = 31;      // 5 bits
+            unsigned short int t = r<<11 | g << 5 | b;
+            *((unsigned short int*)(fbp + location)) = t;
+        }
+		
+		rc_usleep(50000);
+	}
+	
+	rc_cleanup();
+    
+    munmap(fbp, screensize);
+    close(fbfd);
+    return 0;
+}
