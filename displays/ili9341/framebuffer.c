752d0b101a693c954c7aeeba636fc76f8a0235a1
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-13 14:27:58 -0400

    Cleaned up code

--- displays/ili9341/framebuffer.c
+++ displays/ili9341/framebuffer.c
@@ -49,6 +49,11 @@ int main()
 
     printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
     printf("Offset: %dx%d, line_length: %d\n", vinfo.xoffset, vinfo.yoffset, finfo.line_length);
+    
+    if (vinfo.bits_per_pixel != 16) {
+        printf("Can't handle %d bpp, can only do 16.\n", vinfo.bits_per_pixel);
+        exit(5);
+    }
 
     // Figure out the size of the screen in bytes
     screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
@@ -61,23 +66,20 @@ int main()
     }
     printf("The framebuffer device was mapped to memory successfully.\n");
 
-    for (y = 50; y < 200; y++)
+    for (y = 50; y < 200; y++) {
         for (x = 25; x < 250; x++) {
             // Figure out where in memory to put the pixel
             location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
                        (y+vinfo.yoffset) * finfo.line_length;
 
-            if (vinfo.bits_per_pixel == 16) {
-                int r = 31;     // 5 bits
-                int g = 0;      // 6 bits
-                int b = 0;      // 5 bits
-                unsigned short int t = r<<11 | g << 5 | b;
-                *((unsigned short int*)(fbp + location)) = t;
-            } else  {
-                printf("Can't handle %d bpp\n", vinfo.bits_per_pixel);
-            }
-
+            int r = 0;     // 5 bits
+            int g = 0;      // 6 bits
+            int b = 31;      // 5 bits
+            unsigned short int t = r<<11 | g << 5 | b;
+            *((unsigned short int*)(fbp + location)) = t;
+            // usleep(100);
         }
+    }
     munmap(fbp, screensize);
     close(fbfd);
     return 0;
