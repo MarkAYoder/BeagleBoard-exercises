991ed9211467cd9707a6ee19e339addcf16de873
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-16 18:41:07 -0400

    Added echos

--- displays/ili9341/reset.sh
+++ displays/ili9341/reset.sh
@@ -5,10 +5,12 @@ sudo bash << EOF
     # Save contents of framebuffer
     cat /dev/fb0 > /tmp/fb0
     # Disable the framebuffer
+    echo Disabling framebuffer
     rmmod fbtft_device
     sleep 1
     # Get access to the RESET pin
     echo 49  > /sys/class/gpio/export # RESET - V14 - GP0_PIN4
+    echo Toggling RESET
     sleep 1
     # Toggle it
     echo out > /sys/class/gpio/gpio49/direction
@@ -19,6 +21,7 @@ sudo bash << EOF
     echo 49  > /sys/class/gpio/unexport # RESET - V14 - GP0_PIN4
 
     # Fire up the framebuffer
+    echo Firing up framebuffer
     sleep 1
     modprobe fbtft_device name=adafruit28 busnum=1 rotate=90 gpios=reset:49,dc:57
 
