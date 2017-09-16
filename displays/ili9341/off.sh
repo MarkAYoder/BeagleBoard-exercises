e0db1b6546bc1736e022814d1c181552a366f36f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-16 18:15:14 -0400

    Back to pins GP0_3 and GP0_4

--- displays/ili9341/off.sh
+++ displays/ili9341/off.sh
@@ -10,10 +10,10 @@ sudo bash << EOF
     rmmod --force fbtft_device
 
     # unexport poins 49 and 57 so the framebuffer can use them
-    # echo 49 > /sys/class/gpio/unexport # RESET - V14 - GP0_PIN4
-    # echo 57 > /sys/class/gpio/unexport # D/C - U16 - GP0_PIN3
-    echo 113 > /sys/class/gpio/unexport # RESET - V14 - GP0_PIN4
-    echo 116 > /sys/class/gpio/unexport # D/C - U16 - GP0_PIN3
+    echo 49 > /sys/class/gpio/unexport # RESET - V14 - GP0_PIN4
+    echo 57 > /sys/class/gpio/unexport # D/C - U16 - GP0_PIN3
+    # echo 113 > /sys/class/gpio/unexport # RESET - V14 - GP0_PIN4
+    # echo 116 > /sys/class/gpio/unexport # D/C - U16 - GP0_PIN3
 
     # Turn of chip select:  H18 is cs 0,  C18 is cs 1
     echo 29 > /sys/class/gpio/unexport # CS - H18
--- displays/ili9341/on.sh
+++ displays/ili9341/on.sh
@@ -10,16 +10,16 @@ sudo bash << EOF
     rmmod --force fbtft_device
 
     # unexport poins 49 and 57 so the framebuffer can use them
-    # echo 49 > /sys/class/gpio/unexport # RESET - V14 - GP0_4
-    # echo 57 > /sys/class/gpio/unexport # D/C - U16 - GP0_3
-    echo 113 > /sys/class/gpio/unexport # RESET - V14 - GP0_6
-    echo 116 > /sys/class/gpio/unexport # D/C - U16 - GP0_5
+    echo 49 > /sys/class/gpio/unexport # RESET - V14 - GP0_4
+    echo 57 > /sys/class/gpio/unexport # D/C - U16 - GP0_3
+    # echo 113 > /sys/class/gpio/unexport # RESET - V14 - GP0_6
+    # echo 116 > /sys/class/gpio/unexport # D/C - U16 - GP0_5
         
     # Set the pinmuxes for the display
-    # echo gpio > /sys/devices/platform/ocp/ocp\:P9_23_pinmux/state # RESET - V14 - GP0_4
-    # echo gpio > /sys/devices/platform/ocp/ocp\:U16_pinmux/state # D/C     - U16 - GP0_3
-    echo gpio > /sys/devices/platform/ocp/ocp\:P9_28_pinmux/state # RESET- P9_28 - GP0_6
-    echo gpio > /sys/devices/platform/ocp/ocp\:D13_pinmux/state   # D/C  - D13 - GP0_5
+    echo gpio > /sys/devices/platform/ocp/ocp\:P9_23_pinmux/state # RESET - V14 - GP0_4
+    echo gpio > /sys/devices/platform/ocp/ocp\:U16_pinmux/state # D/C     - U16 - GP0_3
+    # echo gpio > /sys/devices/platform/ocp/ocp\:P9_28_pinmux/state # RESET- P9_28 - GP0_6
+    # echo gpio > /sys/devices/platform/ocp/ocp\:D13_pinmux/state   # D/C  - D13 - GP0_5
     echo spi > /sys/devices/platform/ocp/ocp\:P9_31_pinmux/state  # SCLK - A13 - S1.1_5
     echo spi > /sys/devices/platform/ocp/ocp\:P9_29_pinmux/state  # MISO - B13 - S1.1_4
     echo spi > /sys/devices/platform/ocp/ocp\:P9_30_pinmux/state  # MOSI - D12 - S1.1_3
@@ -33,6 +33,6 @@ sudo bash << EOF
     sleep 1
     
     # Insert the framebuffer modules
-    # modprobe fbtft_device name=adafruit28 busnum=1 rotate=90 gpios=reset:49,dc:57
-    modprobe fbtft_device name=adafruit28 busnum=1 rotate=90 gpios=reset:113,dc:116 cs=0
+    modprobe fbtft_device name=adafruit28 busnum=1 rotate=90 gpios=reset:49,dc:57
+    # modprobe fbtft_device name=adafruit28 busnum=1 rotate=90 gpios=reset:113,dc:116 cs=0
 EOF
