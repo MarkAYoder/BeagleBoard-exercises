2251c9eca8984d00e20d402e285df7a9d6ad950f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-09-30 15:14:13 -0400

    Updated to 173

--- iot/google/gcloud/install.sh
+++ iot/google/gcloud/install.sh
@@ -1,6 +1,6 @@
 # From: https://cloud.google.com/sdk/docs/
 
-SDKz=google-cloud-sdk-153.0.0-linux-x86.tar.gz
+SDKz=google-cloud-sdk-173.0.0-linux-x86.tar.gz
 SDK=google-cloud-sdk
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$SDKz
 tar xzf $SDKz
