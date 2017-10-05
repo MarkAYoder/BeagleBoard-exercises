ed49dd1a0452120c94deb2f75eb6f73af2e11330
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-10-05 16:29:43 -0400

    Initial commit

---
+++ displays/ili9341/draw.sh
@@ -0,0 +1,19 @@
+# Here's how to use imagemagick to display text
+# Make a blank image
+SIZE=320x240
+TMP_FILE=/tmp/frame.png
+
+# From: http://www.imagemagick.org/Usage/text/
+# convert -background lightblue -fill blue -font Times-Roman -pointsize 24 \
+#       -size $SIZE \
+#       label:'ImageMagick\nExamples\nby Anthony' \
+#       -draw "text 0,200 'Bottom of Display'" \
+#       $TMP_FILE
+
+convert -size $SIZE xc:skyblue -fill white -stroke black \
+      -draw "circle 160,120 160,40"          $TMP_FILE
+
+sudo fbi -noverbose -T 1 $TMP_FILE
+
+# convert -list font
+
