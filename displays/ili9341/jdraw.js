3f3071e56caf6429d7feb5b28e5a3c7974ffb201
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-10-06 16:50:10 -0400

    Moved everything to jdraw

--- displays/ili9341/jdraw.js
+++ displays/ili9341/jdraw.js
@@ -2,6 +2,8 @@
 // Generates imagemagick commands to draw a clock face
 const util = require('util');
 
+const TMP_FILE = '/tmp/frame.png';
+
 const xmax = 320;   // Display size
 const ymax = 240;
 
@@ -12,17 +14,34 @@ const rad = 100;
 const len = 10;     // length of tics
 
 var ang = 0;
+var cmd = '';
 
-console.log("convert -size $SIZE xc:skyblue -fill white -stroke black \\");
-console.log("-draw \"");
-console.log(util.format("circle %d,%d %d,%d", xcent, ycent, xcent, ycent-rad));
+cmd = util.format("convert -size %dx%d xc:skyblue -fill white -stroke black \\\n",
+        xmax, ymax);
+cmd += "-draw \"\n";
+cmd += util.format("circle %d,%d %d,%d\n", xcent, ycent, xcent, ycent-rad);
 
 // Draw the ticks around the face
 for(ang=0; ang<2*Math.PI; ang+=Math.PI/6) {
-    console.log(util.format("line %d,%d %d,%d", xcent+rad*Math.cos(ang), ycent+rad*Math.sin(ang), 
-        xcent+(rad-len)*Math.cos(ang), ycent+(rad-len)*Math.sin(ang)));
+    cmd += util.format("line %d,%d %d,%d\n", xcent+rad*Math.cos(ang), ycent+rad*Math.sin(ang), 
+        xcent+(rad-len)*Math.cos(ang), ycent+(rad-len)*Math.sin(ang));
 }
 
+cmd +=  "\" " + TMP_FILE + "; ";
 
+cmd += "sudo fbi -noverbose -T 1 " + TMP_FILE;
 
-console.log("\" $TMP_FILE");
+// console.log(cmd);
+
+const exec = require('child_process').exec;
+exec(cmd, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  if(stdout) console.log(`stdout: ${stdout}`);
+  if(stderr) console.log(`stderr: ${stderr}`);
+});
+
+var d = new Date();
+console.log("Seconds: " + d.getSeconds());
