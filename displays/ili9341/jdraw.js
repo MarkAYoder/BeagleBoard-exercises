c1fef1e630b402d5146e823f2002a6387443e845
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-10-09 10:06:23 -0400

    Seconds are working

--- displays/ili9341/jdraw.js
+++ displays/ili9341/jdraw.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 // Generates imagemagick commands to draw a clock face
 const util = require('util');
+// const b    = require('bonescript');
 
 const TMP_FILE = '/tmp/frame.png';
 
@@ -10,7 +11,7 @@ const ymax = 240;
 const xcent = xmax/2;   // Put center in middle of display
 const ycent = ymax/2;
 
-const rad = 100;
+const rad = 50;
 const len = 10;     // length of tics
 
 var ang = 0;
@@ -29,26 +30,29 @@ for(ang=0; ang<2*Math.PI; ang+=Math.PI/6) {
 
 // Draw second hand
 
-const d = new Date();
-const sec = d.getSeconds();
-console.log("Seconds: " + sec);
+setInterval(displayTime, 1000);
 
-ang = Math.PI/2-2*Math.PI*sec/60;
-cmd += util.format("line %d,%d %d,%d\n", xcent, ycent, xcent+rad*Math.cos(ang), ycent-rad*Math.sin(ang));
-
-cmd +=  "\" " + TMP_FILE + "; ";
-
-// cmd += "sudo fbi -noverbose -T 1 " + TMP_FILE;
-cmd += "gnome-open " + TMP_FILE;
-
-console.log(cmd);
-
-const exec = require('child_process').exec;
-exec(cmd, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  if(stdout) console.log(`stdout: ${stdout}`);
-  if(stderr) console.log(`stderr: ${stderr}`);
-});
+function displayTime() {
+    const d = new Date();
+    console.log("Seconds: " + d.getSeconds());
+    
+    var ang = Math.PI/2-2*Math.PI*d.getSeconds()/60;
+    var currentCmd = cmd + util.format("line %d,%d %d,%d\n", xcent, ycent, xcent+rad*Math.cos(ang), ycent-rad*Math.sin(ang));
+    
+    currentCmd +=  "\" " + TMP_FILE + "; ";
+    
+    currentCmd += "sudo fbi -noverbose -T 1 " + TMP_FILE;
+    // currentCmd += "gnome-open " + TMP_FILE;
+    
+    // console.log(currentCmd);
+    
+    const exec = require('child_process').exec;
+    exec(currentCmd, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      if(stdout) console.log(`stdout: ${stdout}`);
+    //   if(stderr) console.log(`stderr: ${stderr}`);
+    });
+}
