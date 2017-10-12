b331828b04edf27c2b2bd353d3027fcf216c8de0
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-10-12 09:59:10 -0400

    Added pip install

--- displays/ili9341/pygame/install.sh
+++ displays/ili9341/pygame/install.sh
@@ -4,3 +4,5 @@ sudo apt install mercurial python3-dev python3-numpy libav-tools libsdl-image1.2
 sudo apt install libsmpeg-dev libsdl1.2-dev  libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev
 sudo apt install libfreetype6-dev
 sudo python3 -m pip install pygame
+
+sudo pip3 install requests
