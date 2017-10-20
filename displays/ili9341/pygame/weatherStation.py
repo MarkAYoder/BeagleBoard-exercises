c5b814d0bb86ace467bfa65a66f97b2b85ef84c2
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-10-20 11:15:41 -0400

    I'm not sure why there are changes

--- displays/ili9341/pygame/weatherStation.py
+++ displays/ili9341/pygame/weatherStation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# // Displays temp and humidity and forcast from wunderground
+# // Displays temp and humidity and forecast from wunderground
 # // 
 # // Gets data from weather underground.
 # // Displays as
@@ -12,23 +12,27 @@
 # //  forecasted low and hi temps.
 
 import requests
+import sys
 
 # Get outdoor temp and forcast from wunderground
 urlWeather = "http://api.wunderground.com/api/ec7eb641373d9256/yesterday/history/conditions/forecast/q/IN/Brazil.json"
-r = requests.get(urlWeather)
-if(r.status_code==200):
-    # print("headers: ", r.headers)
-    # print("text: ", r.text)
-    # print("json: ", r.json())
-    weather = r.json()
-    print("Temp: ", weather['current_observation']['temp_f'])
-    print("Yesterday Max: ", weather['history']['dailysummary'][0]['maxtempi'])
-    print("Yesterday Min: ", weather['history']['dailysummary'][0]['mintempi'])
-    print("Humid:", weather['current_observation']['relative_humidity'])
-    print("Low:  ", weather['forecast']['simpleforecast']['forecastday'][0]['low']['fahrenheit'])
-    print("High: ", weather['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit'])
-    print("Min: ", weather['forecast']['simpleforecast']['forecastday'][0]['avewind']['mph'])
-    print("Max: ", weather['forecast']['simpleforecast']['forecastday'][0]['maxwind']['mph'])
-    # print("All : ", weather)
-else:
-    print("status_code: ", r.status_code)
+try:
+    r = requests.get(urlWeather)
+    if(r.status_code==200):
+        # print("headers: ", r.headers)
+        # print("text: ", r.text)
+        # print("json: ", r.json())
+        weather = r.json()
+        print("Temp: ", weather['current_observation']['temp_f'])
+        print("Yesterday Max: ", weather['history']['dailysummary'][0]['maxtempi'])
+        print("Yesterday Min: ", weather['history']['dailysummary'][0]['mintempi'])
+        print("Humid:", weather['current_observation']['relative_humidity'])
+        print("Low:  ", weather['forecast']['simpleforecast']['forecastday'][0]['low']['fahrenheit'])
+        print("High: ", weather['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit'])
+        print("Wind:\n\tAve: ", weather['forecast']['simpleforecast']['forecastday'][0]['avewind']['mph'])
+        print("\tMax: ", weather['forecast']['simpleforecast']['forecastday'][0]['maxwind']['mph'])
+        # print("All : ", weather)
+    else:
+        print("status_code: ", r.status_code)
+except:
+    print("Unexpected error:", sys.exc_info()[0])
