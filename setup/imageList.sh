45b6172b5c620fadcc6336c56e4ff0bf523beefe
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-10-25 15:05:40 -0400

    Switched from chrome to gnome-open.  Removed .bmap file

--- setup/imageList.sh
+++ setup/imageList.sh
@@ -9,18 +9,17 @@ if [ "$1" == "" ]; then
 	curl -s $SITE/ | sed -n '/^$/!{s/<[^>]*>//g;p;}' | awk -F/ '{print $1}'
 	echo $SITE
 elif [ "$1" == "web" ]; then
-	google-chrome $SITE
+	gnome-open $SITE
 else
 	echo $1
 	cd ~/BeagleBoard/Images
-	    URL=$SITE/$1/$RELEASE/bone-debian-$VERSION-iot-armhf-$1-4gb
-	    wget $URL.bmap
-# Check to see we we found anything, so so, wget the rest of the files.
+	    URL=$SITE/$1/$RELEASE/bone-debian-$VERSION-iot-armhf-$1-4gb.img.xz
+	    wget $URL.sha256sum
+# Check to see we we found anything, if so, wget the rest of the files.
 # If not, list what you did find.
 	    if [ $? -eq 0 ]
 	    then	# Success
-	        wget $URL.img.xz.sha256sum
-		time wget $URL.img.xz
+		time wget $URL
 	    else
 		echo -e "\n\e[91m$1: not found\e[0m\n"
 		curl -s $SITE/$1/ | sed -n '/^$/!{s/<[^>]*>//g;p;}' | awk -F/ '{print $1}'
