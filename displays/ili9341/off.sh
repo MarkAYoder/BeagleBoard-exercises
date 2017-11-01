9b005cc9ac4a9af0bf5895212e5bc35ddaf9c82f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-11-01 16:50:05 -0400

    Still switching to parameters

--- displays/ili9341/off.sh
+++ displays/ili9341/off.sh
@@ -3,23 +3,24 @@
 
 # Connect the display before running this.
 
+export GPIO=/sys/class/gpio
+export OCP=/sys/devices/platform/ocp
+export LED=49
+export RESET=113    # RESET - V14 - GP0_6
+export DC=116       # D/C - U16 - GP0_3
+export CS=29        # CS - H18
+
 sudo bash << EOF
     # remove the framebuffer modules
-    rmmod fb_ili9341
-    rmmod fbtft
-    rmmod fbtft_device
+    if lsmod | grep -q 'fbtft_device ' ; then rmmod fbtft_device;  fi
+    if lsmod | grep -q 'fb_ili9341 '   ; then rmmod fb_ili9341;    fi
+    if lsmod | grep -q 'fbtft '        ; then rmmod fbtft;         fi
 
-    # unexport poins 49 and 57 so the framebuffer can use them
-    # echo 49 > /sys/class/gpio/unexport # RESET - V14 - GP0_PIN4
-    # echo 57 > /sys/class/gpio/unexport # D/C - U16 - GP0_PIN3
-    echo 113 > /sys/class/gpio/unexport # RESET - V14 - GP0_PIN4
-    echo 116 > /sys/class/gpio/unexport # D/C - U16 - GP0_PIN3
-
-    # Turn of chip select:  H18 is cs 0,  C18 is cs 1
-    echo 29 > /sys/class/gpio/unexport # CS - H18
-    # echo 7 > /sys/class/gpio/unexport # CS - C18
-    
     # Turn off LED
-    echo 0 > /sys/class/gpio/gpio49/value
+    if [ -d $GPIO/gpio$LED    ]
+    then 
+        echo 0 > $GPIO/gpio$LED/value
+        echo $LED    > $GPIO/unexport
+    fi
         
 EOF
