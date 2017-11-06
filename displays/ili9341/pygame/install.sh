761696301ab11f15d8ab9cf7848ee80c7d9cda9f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-11-06 17:07:19 -0500

    Added python3-pip

--- displays/ili9341/pygame/install.sh
+++ displays/ili9341/pygame/install.sh
@@ -1,6 +1,6 @@
 # From: https://www.pygame.org/wiki/GettingStarted
 # I don't know if all of these are needed.
-sudo apt install mercurial python3-dev python3-numpy libav-tools libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev
+sudo apt install mercurial python3-pip python3-dev python3-numpy libav-tools libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev
 sudo apt install libsmpeg-dev libsdl1.2-dev  libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev
 sudo apt install libfreetype6-dev
 sudo python3 -m pip install pygame
