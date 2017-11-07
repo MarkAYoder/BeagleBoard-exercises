365312bd10f7fde8e80a246e37186c617c6fec07
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-11-06 20:29:37 -0500

    Added LED backlight control

--- setup/ledTrigger.js
+++ setup/ledTrigger.js
@@ -9,9 +9,11 @@ if(process.argv[2] === 'off') {
     fs.writeFile(path+'2/trigger', 'none');
     fs.writeFile(path+'3/trigger', 'none');
     fs.writeFile('/sys/class/leds/wifi/brightness', '0');
+    fs.writeFile('/sys/class/gpio/gpio49/value', '0');
 } else {
     fs.writeFile(path+'0/trigger', 'heartbeat');
     fs.writeFile(path+'1/trigger', 'mmc0');
     fs.writeFile(path+'2/trigger', 'cpu0');
     fs.writeFile(path+'3/trigger', 'mmc1');
+    fs.writeFile('/sys/class/gpio/gpio49/value', '1');
 }
