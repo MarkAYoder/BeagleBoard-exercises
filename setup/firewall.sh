5b098a63007ed66ca223c2aef11955ee873ea574
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-12-05 10:43:14 -0500

    Added comments

--- setup/firewall.sh
+++ setup/firewall.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 # Run on bone to only accept from a few IP ranges
 # Set up firewall to reject all but 317.112.*.* and 192.168.7.*
 iptables --policy INPUT DROP
+# Only ECE subnet
 iptables -A INPUT -s 137.112.41.0/24 -j ACCEPT
+# Beagle network
 iptables -A INPUT -s 192.168.7.0/24 -j ACCEPT
+# Home network
 iptables -A INPUT -s 10.0.4.0/24 -j ACCEPT
 
 # Allow outgoing things to come back
