377b39e829c9022a4d42cfb12b7a30bc16c5c1f1
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-12-23 13:39:48 -0500

    Copied from iot/blynk

---
+++ displays/LEDstring/sliders.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+// From https://www.instructables.com/id/Blynk-JavaScript-in-20-minutes-Raspberry-Pi-Edison/
+var Blynk = require('blynk-library');
+
+var AUTH = '087e4c21298e413ab9d5f87a5279e5c9';
+
+var blynk = new Blynk.Blynk(AUTH);
+
+var v1 = new blynk.VirtualPin(1);
+var v9 = new blynk.VirtualPin(9);
+
+v1.on('write', function(param) {
+    console.log('V1:', param[0]);
+});
+
+v9.on('read', function() {
+    v9.write(new Date().getSeconds());
+});
