ffe8835016161bed54c46ef1527ae7d11641b31a
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2017-12-23 14:49:50 -0500

    Added sliders

--- displays/LEDstring/blynk.js
+++ displays/LEDstring/blynk.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 // From Blinks various LEDs
 const Blynk = require('blynk-library');
-const b = require('bonescript');
 const util = require('util');
 const exec = require('child_process').exec;
 var fs = require('fs');
@@ -14,21 +13,62 @@ const AUTH = 'dc1c083949324ca28fbf393231f8cf09';
 
 var blynk = new Blynk.Blynk(AUTH);
 
-var v4 = new blynk.VirtualPin(4);
-var v10 = new blynk.WidgetLED(10);
-// console.log(util.inspect(v1));
-// var v9 = new blynk.VirtualPin(9);
+var v0 = new blynk.VirtualPin(0);   // Left button
+var v1 = new blynk.VirtualPin(1);   // Right button
+var v4 = new blynk.VirtualPin(4);   // Color picker
+var v5 = new blynk.VirtualPin(5);   //Color slider
+var v6 = new blynk.VirtualPin(6);
+var v7 = new blynk.VirtualPin(7);
+var v8 = new blynk.VirtualPin(8);   // Scale
 
-v4.on('write', function(param) {
-    var r = param[0];
-    var g = param[1];
-    var b = param[2];
+function display(r, g, b) {
     console.log(util.format("R: %d, G: %d, B: %d", r, g, b));
     for(var i=0; i<LEDcount; i++) {
-        fs.write(fd, util.format("%d %d %d %d ", r, g, b, i));
+        fs.write(fd, util.format("%d %d %d %d ", r>>scale, g, b, i));
     }
     // Update string
     fs.write(fd, util.format("\n"));
+}
 
+// var v10 = new blynk.WidgetLED(10);
+// console.log(util.inspect(v1));
+// var v9 = new blynk.VirtualPin(9);
+
+var r = 128;    // Color
+var g = 128;
+var b = 128;
+var scale = 0;
+
+v4.on('write', function(param) {
+    r = param[0];
+    g = param[1];
+    b = param[2];
+    display(r, g, b);
 });
 
+v0.on('write', function(param) {
+    console.log('V0: ', param[0]);
+});
+v1.on('write', function(param) {
+    console.log('V1: ', param[0]);
+});
+v5.on('write', function(param) {
+    console.log('Red: ', param[0]);
+    r = param[0];
+    display(r, g, b);
+});
+v6.on('write', function(param) {
+    console.log('Green: ', param[0]);
+    g = param[0];
+    display(r, g, b);
+});
+v7.on('write', function(param) {
+    console.log('Blue: ', param[0]);
+    b = param[0];
+    display(r, g, b);
+});
+v8.on('write', function(param) {
+    console.log('Scale: ', param[0]);
+    scale = param[0];
+    display(r, g, b);
+});
