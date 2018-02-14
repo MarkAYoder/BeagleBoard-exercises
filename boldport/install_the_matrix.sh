4590d8626914916c774b68bfec19431cf6e9c890
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-02-13 19:52:07 -0500

    Added apt install

--- boldport/install_the_matrix.sh
+++ boldport/install_the_matrix.sh
@@ -1,10 +1,11 @@
 # Here's how to install The Matrix
 # From: https://www.boldport.com/products/the-matrix/
 git clone https://github.com/threebytesfull/matrix-pi.git
+sudo apt install python-smbus # python3-smbus
 cd matrix-pi
 sudo pip install the_matrix
-i2cdetect -y 2  # I wired to i2c bus two.  P9_19 - SCL, P9_20 - SDA
-# THe library defaults to bus 1.  I edited the library to use bus 2
+i2cdetect -y -r 2  # I wired to i2c bus 2.  P9_19 - SCL, P9_20 - SDA
+# The library defaults to bus 1.  I edited the library to use bus 2
 pip show the_matrix     # This shows where pip installed the library
 cd /usr/local/lib/python2.7/dist-packages/the_matrix
 grep bus_ *             # Show which files need to be edited to change the bus.
