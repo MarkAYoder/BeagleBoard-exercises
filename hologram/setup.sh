4188881895a0bc101d73bf9b8a9cb5c686923494
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-03-06 12:29:01 -0500

    Added commands to copy from /opt

--- hologram/setup.sh
+++ hologram/setup.sh
@@ -1,2 +1,7 @@
 config-pin P9_11 uart
 config-pin P9_13 uart
+
+PPPATH=/opt/scripts/tools/software/hologram-tools/ppp
+
+sudo bash -c "sed 's/ACM0/O4/' $PPPATH/peers/nova > /etc/ppp/peers/nova"
+sudo cp $PPPATH/chatscripts/nova /etc/chatscripts
