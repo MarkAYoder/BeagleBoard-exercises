8424cff829d8f75e3e495b48742f5f44af250cbf
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-03-06 07:59:37 -0500

    Adding install.sh

---
+++ hologram/install.sh
@@ -0,0 +1,4 @@
+# From: https://www.hackster.io/hologram/cellular-modem-at-command-workshop-299e13?utm_source=hackster&utm_medium=email&utm_campaign=new_projects
+# https://learn.adafruit.com/fona-tethering-to-raspberry-pi-or-beaglebone-black/overview
+
+git clone https://github.com/benstr/AT-Workshop.git
