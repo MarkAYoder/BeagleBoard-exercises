ef9fc6650e465ba188c74473607a75b5366d8175
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-05-03 09:31:13 -0400

    No longer putting clpru and lnkpru in /usr/share/ti/cgt-pru/bin

--- pru/examples/pwm/install.sh
+++ pru/examples/pwm/install.sh
@@ -1,18 +1,7 @@
-# You to disable the hdmi audo and video.
+# You need to disable the hdmi audo and video.
 # Edit /boot/uEnv.txt and uncomment the following two lines:
 
 #disable_uboot_overlay_video=1
 #disable_uboot_overlay_audio=1
 
 # Then reboot the bone
-
-export PRU_CGT=/usr/share/ti/cgt-pru
-
-here=$PWD
-
-cd $PRU_CGT
-mkdir -p bin
-
-cd bin
-ln -s `which clpru`  .
-ln -s `which lnkpru` .
