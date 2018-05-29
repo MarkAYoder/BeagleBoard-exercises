1f03dd3e4e62012688ff8f6d2a06cc6ce5fb0ca8
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-05-29 13:32:50 -0400

    Cleaned up

--- asee/2018/setup.sh
+++ asee/2018/setup.sh
@@ -1,11 +1,4 @@
 # This updates the files used by the workshop
-# rsync -a python2 bone2:/tmp/
 FILE=python
 DIR=/var/lib/cloud9/examples/robot
 rsync -av  --delete --no-times --no-group --no-owner $FILE bone2:$DIR/
-ssh bone2 "
-
-cd $DIR
-# chgrp cloud9ide $FILE
-
-"
