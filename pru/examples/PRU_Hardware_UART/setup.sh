cec5c349b2210a8424d351b395d6ac73b4cbc895
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-06-01 12:40:05 -0400

    Removed PRU_CGT and PRU_SUPPORT.  These are in Makefile

--- pru/examples/PRU_Hardware_UART/setup.sh
+++ pru/examples/PRU_Hardware_UART/setup.sh
@@ -3,8 +3,5 @@ config-pin P9_24 pru_uart
 # Configure rx
 config-pin P9_26 pru_uart
 
-export PRU_CGT=/usr/share/ti/cgt-pru
-export PRU_SUPPORT=/opt/source/pru-software-support-package
-
 export PRUN=0
 export TARGET=uart1
