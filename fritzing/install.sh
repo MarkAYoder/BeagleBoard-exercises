cadd9e6b8d4930fdc198c4ff79bd79670662fe96
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-06-29 11:08:36 -0400

    Updated for 9.3b

--- fritzing/install.sh
+++ fritzing/install.sh
@@ -1,5 +1,6 @@
-wget http://fritzing.org/download/0.9.2b/linux-64bit/fritzing-0.9.2b.linux.AMD64.tar.bz2
-tar xvjf fritzing-0.9.2b.linux.AMD64.tar.bz2
+VERS=0.9.3b
+http://fritzing.org/download/$VERS/linux-64bit/fritzing-$VERS.linux.AMD64.tar.bz2
+tar xvjf fritzing-$VERS.linux.AMD64.tar.bz2
 
 # Get Adafruit library 
 # From: https://learn.adafruit.com/using-the-adafruit-library-with-fritzing/import-the-library-into-fritzing
@@ -7,7 +8,7 @@ wget https://github.com/adafruit/Fritzing-Library/archive/master.zip
 unzip master
 
 # Open fritzing
-cd fritzing-0.9.2b.linux.AMD64
+cd fritzing-$VERS.linux.AMD64
 ./Fritzing
 
 # Follow Adafruit instructions
