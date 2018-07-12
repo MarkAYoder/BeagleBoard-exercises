dd858986fe892e367dd483dc964e7db017df86af
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-07-12 16:13:26 -0400

    Added mqtt example

---
+++ iot/mydevices/install.sh
@@ -0,0 +1,9 @@
+# From: https://cayenne.mydevices.com/cayenne/dashboard/first-visit/raspberrypi/step-1
+
+# This doesn't appear to be working...
+wget https://cayenne.mydevices.com/dl/rpi_vzytuchzth.sh
+chmod +x rpi_vzytuchzth.sh
+sudo ./rpi_vzytuchzth.sh
+
+# Let's try this
+git clone https://github.com/myDevicesIoT/cayennejs.git
---
+++ iot/mydevices/play.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+var Cayenne = require('cayennejs');
+
+// Initiate MQTT API
+const cayenneClient = new Cayenne.MQTT({
+  username: "9d396770-6f4d-11e8-84d1-4d9372e87a68",
+  password: "b977512eb348da0f00a02d000acf36d324ae6346",
+  clientId: "a4153ba0-6f4d-11e8-ab28-e7cb4e37d88a"
+});
+
+cayenneClient.connect((err, mqttClient) => {
+  // dashboard widget automatically detects datatype & unit
+  cayenneClient.kelvinWrite(3, 65);
+
+  // sending raw values without datatypes
+  cayenneClient.rawWrite(4, 123);
+
+  // subscribe to data channel for actions (actuators)
+  cayenneClient.on("cmd9", function(data) {
+    console.log(data);
+  });
+
+});
