0111115c417412608e9decf62c7355e7d5b095ad
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-07-12 13:37:29 -0400

    Adding iot-core examples

---
+++ iot/google/iot-core/install.sh
@@ -0,0 +1,37 @@
+# From: https://cloud.google.com/iot/docs/quickstart
+
+git clone https://github.com/GoogleCloudPlatform/nodejs-docs-samples
+
+export PROJECT_ID=core-iot-may
+
+gcloud pubsub subscriptions create \
+    projects/$PROJECT_ID/subscriptions/my-subscription \
+    --topic=projects/$PROJECT_ID/topics/my-device-events
+    
+node cloudiot_mqtt_example_nodejs.js \
+    --projectId=$PROJECT_ID \
+    --registryId=my-registry \
+    --deviceId=my-device \
+    --privateKeyFile=rsa_private.pem \
+    --numMessages=5 \
+    --algorithm=RS256
+
+gcloud pubsub subscriptions pull --auto-ack \
+    projects/$PROJECT_ID/subscriptions/my-subscription
+    
+gcloud pubsub subscriptions delete \
+    projects/$PROJECT_ID/subscriptions/my-subscription
+
+
+# These are for the weather station example
+node cloudiot_mqtt_example_nodejs.js \
+    --projectId=$PROJECT_ID \
+    --registryId=weather-station-registry \
+    --deviceId=my-device \
+    --privateKeyFile=rsa_private.pem \
+    --numMessages=5 \
+    --algorithm=RS256 \
+    --messageType=state
+
+gcloud pubsub subscriptions pull --auto-ack --limit 5 telemetry-subscription	
+    
---
+++ iot/google/iot-core/weather_install.sh
@@ -0,0 +1,25 @@
+# From: https://medium.com/google-cloud/build-a-weather-station-using-google-cloud-iot-core-and-mongooseos-7a78b69822c5
+
+git clone https://github.com/alvarowolfx/weather-station-gcp-mongoose-os
+
+export PROJECT_ID=iot-core-may
+
+# Authenticate with Google Cloud:
+gcloud auth login
+# Create cloud project — choose your unique project name:
+gcloud projects create $PROJECT_ID
+# Set current project
+gcloud config set project $PROJECT_ID
+
+
+# Add permissions for IoT Core
+gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:cloud-iot@system.gserviceaccount.com --role=roles/pubsub.publisher
+# Create PubSub topic for device data:
+gcloud pubsub topics create telemetry-topic
+# Create PubSub subscription for device data:
+gcloud pubsub subscriptions create \
+    --topic telemetry-topic telemetry-subscription
+# Create device registry:
+gcloud iot registries create weather-station-registry \
+    --region us-central1 \
+    --state-pubsub-topic=telemetry-topic
