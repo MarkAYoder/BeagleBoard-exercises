8243daada1bda1eac438d840878b867fd403f87e
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-07-20 07:57:03 -0400

    Switched from 9.4 to 9.5

--- setup/imageList.sh
+++ setup/imageList.sh
@@ -4,7 +4,7 @@
 # imageList.sh 2017-05-14 to get the 2017-05-14 iot image
 SITE=https://rcn-ee.com/rootfs/bb.org/testing 
 RELEASE=stretch-iot
-VERSION=9.4
+VERSION=9.5
 if [ "$1" == "" ]; then
 	curl -s $SITE/ | sed -n '/^$/!{s/<[^>]*>//g;p;}' | awk -F/ '{print $1}'
 	echo $SITE
