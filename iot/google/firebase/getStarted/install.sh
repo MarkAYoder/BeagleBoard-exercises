16b64da1a04c01b4eb5760e295bdf181ed3f48aa
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-09-14 21:23:06 -0400

    Removed firebase

--- iot/google/firebase/getStarted/install.sh
+++
@@ -1,5 +0,0 @@
-# From: https://firebase.google.com/docs/functions/get-started
-npm install -g firebase-tools
-# Had to do twice to get current version 3.5.0
-firebase login
-firebase init functions
