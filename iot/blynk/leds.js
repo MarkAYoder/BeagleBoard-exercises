e05bf007822df15ecde14e921c28f36a5eb5dcb4
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-10-05 09:43:25 -0400

    Cleaned up

--- iot/blynk/leds.js
+++ iot/blynk/leds.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// From Blinks various LEDs
+// Blinks various LEDs
 const Blynk = require('blynk-library');
 const b = require('bonescript');
 const util = require('util');
@@ -16,8 +16,6 @@ var blynk = new Blynk.Blynk(AUTH);
 
 var v0 = new blynk.VirtualPin(0);
 var v10 = new blynk.WidgetLED(10);
-// console.log(util.inspect(v1));
-// var v9 = new blynk.VirtualPin(9);
 
 v0.on('write', function(param) {
     console.log('V0:', param[0]);
@@ -26,13 +24,9 @@ v0.on('write', function(param) {
 
 v10.setValue(0);    // Initiallly off
 
-// v9.on('read', function() {
-//     v9.write(new Date().getSeconds());
-// });
-
 b.attachInterrupt(button, toggle, b.CHANGE);
 
 function toggle(x) {
-    console.log("V1: ", x.value);
+    console.log("V10: ", x.value);
     x.value ? v10.turnOff() : v10.turnOn();
 }
