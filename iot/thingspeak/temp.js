0088fad782d3edf4906142bf3aabc65b45887a50
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-10-24 15:00:40 -0400

    Switched from 8 bit to 12 bit

--- iot/thingspeak/temp.js
+++ iot/thingspeak/temp.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 // Measure the weather with TMP101 sensors
 const i2c     = require('i2c-bus');
-const util          = require('util');
-const fs            = require('fs');
+const util    = require('util');
+const fs      = require('fs');
 const ms = 15*60*1000;               // Repeat time
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://mqtt.thingspeak.com');
@@ -21,6 +21,12 @@ const keys = JSON.parse(fs.readFileSync(filename));
 const url = "channels/" + keys.channel_id + "/publish/" + keys.write_key;
 // console.log("url: " + url);
 
+// Set to 12-bit mode
+for(var i=0; i<tmp101.length; i++) {
+  const CMD_ADDR = 0x1;
+  sensor.writeByteSync(tmp101[i], CMD_ADDR, 0x60);  // 12-bit mode
+}
+
 client.on('connect', function() {
   var tempOld = [];
   var temp = [];
@@ -34,7 +40,10 @@ client.on('connect', function() {
         // Read the temp sensors
         for(var i=0; i<tmp101.length; i++) {
             // Convert from C to F
-            temp[i] = sensor.readByteSync(tmp101[i], 0x0)*9/5+32;
+            temp[i] = sensor.readWordSync(tmp101[i], 0x0);    // Read temp
+            // Swap the bytes and scale
+            temp[i] = (((temp[i] & 0xff) << 8) | ((temp[i] & 0xff00) >> 8))/256;
+            temp[i] = temp[i]*9/5+32;     //Convert to F
             // temp[i] = Math.random();
             console.log("temp: %dF (0x%s)", temp[i], tmp101[i].toString(16));
         }
