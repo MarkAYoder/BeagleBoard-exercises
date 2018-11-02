d81d3860509c3188f476d6147a8f5930ce8fd725
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-11-02 09:11:01 -0400

    Switched to python 3

--- displays/ili9341/usr/install.sh
+++ displays/ili9341/usr/install.sh
@@ -1,16 +1,17 @@
 # From: https://learn.adafruit.com/user-space-spi-tft-python-library-ili9341-2-8/usage
+# Now works for python3
 sudo apt update
 # sudo apt install build-essential python-dev python-smbus 
 sudo apt install python-pip python-imaging python-numpy
 # sudo pip3 install Adafruit_BBIO
 
-cd ~
 git clone https://github.com/adafruit/Adafruit_Python_ILI9341.git
 cd Adafruit_Python_ILI9341
 sudo python3 setup.py install
 
+# https://stackoverflow.com/questions/23970006/using-pillow-with-python-3
+sudo apt install zlib1g-dev libjpeg62-turbo-dev
+# sudo apt-get install libtiff4-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev python-tk
+sudo pip3 install Pillow
+
 # sudo pip3 install spidev
-
-
-# python 3
-sudo python3 setup.py install
