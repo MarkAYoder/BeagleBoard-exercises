beb816996ddcbf66b97fd9f84519fb11be78fa11
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-11-02 08:21:36 -0400

    Updated for python3

--- displays/ili9341/usr/install.sh
+++ displays/ili9341/usr/install.sh
@@ -1,14 +1,15 @@
 # From: https://learn.adafruit.com/user-space-spi-tft-python-library-ili9341-2-8/usage
 sudo apt update
-sudo apt install build-essential python-dev python-smbus python-pip python-imaging python-numpy
-sudo pip install Adafruit_BBIO
+# sudo apt install build-essential python-dev python-smbus 
+sudo apt install python-pip python-imaging python-numpy
+# sudo pip3 install Adafruit_BBIO
 
 cd ~
 git clone https://github.com/adafruit/Adafruit_Python_ILI9341.git
 cd Adafruit_Python_ILI9341
-sudo python setup.py install
+sudo python3 setup.py install
 
-sudo pip install spidev
+# sudo pip3 install spidev
 
 
 # python 3
