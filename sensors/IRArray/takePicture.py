f928a19759256bc8f54abd254f5fadb000116b6e
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-11-07 16:26:00 -0500

    Removed LED

--- sensors/IRArray/takePicture.py
+++ sensors/IRArray/takePicture.py
@@ -4,25 +4,23 @@
 import Adafruit_BBIO.GPIO as GPIO
 import os
 import datetime
+import threading
 
 button="P2_17"  # PAUSE=P8_9, MODE=P8_10
-LED   ="USR3"
 
-# Set the GPIO pins:
-GPIO.setup(LED,    GPIO.OUT)
+def takePic():
+    print("Pressed")
+    file = "/tmp/" + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + ".jpg"
+    # print(file)
+  
+    os.system("./fb2jpg.sh /dev/fb0 " + file)
+    os.system("rclone copy " + file + " drive:Pictures")
+    
+# Set the GPIO pin:
 GPIO.setup(button, GPIO.IN)
 
 print("Running...")
 
 while True:
-  state = GPIO.input(button)
-  GPIO.output(LED, state)
-  
-  GPIO.wait_for_edge(button, GPIO.FALLING)  # FALLING, RISING, BOTH
-  print("Pressed")
-  
-  file = "/tmp/" + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + ".jpg"
-  print(file)
-  
-  os.system("./fb2jpg.sh /dev/fb0 " + file)
-  os.system("rclone copy " + file + " drive:Pictures")
+    GPIO.wait_for_edge(button, GPIO.RISING)  # FALLING, RISING, BOTH
+    takePic()
