a160422ecce858536003ad89c65e89fe3afa9cfb
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-11-07 14:28:56 -0500

    Cleaned up

--- sensors/IRArray/fb2jpg.sh
+++ sensors/IRArray/fb2jpg.sh
@@ -1,12 +1,20 @@
 # From: https://stackoverflow.com/questions/3781549/how-to-convert-16-bit-rgb-frame-buffer-to-a-viewable-format
 
+TMP=/tmp/fb2jpg.jpg
+INFILE=${1:-/dev/fb0}
+OUTFILE=${2:-fb2jpg.pjg}
+
 ffmpeg \
+  -y -loglevel 0 \
   -vcodec rawvideo \
   -f rawvideo \
   -pix_fmt rgb565 \
   -s 240x320 \
-  -i test.rgb \
+  -i $INFILE \
   \
   -f image2 \
   -vcodec mjpeg \
-  test.jpg
+  $TMP
+
+convert -rotate "90" $TMP $OUTFILE
+echo Outputted to $OUTFILE
