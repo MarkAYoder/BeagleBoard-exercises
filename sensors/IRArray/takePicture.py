12a4ad0f4246183222157f7cb0d56d1b6c5aec53
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-11-07 17:13:52 -0500

    Added LED back

--- sensors/IRArray/takePicture.py
+++ sensors/IRArray/takePicture.py
@@ -7,17 +7,26 @@ import datetime
 import threading
 
 button="P2_17"  # PAUSE=P8_9, MODE=P8_10
+LED = "USR3"
 
 def takePic():
     print("Pressed")
+    # Turn LED on to show it's working
+    GPIO.output(LED, 1)
+
     file = "/tmp/" + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + ".jpg"
     # print(file)
   
     os.system("./fb2jpg.sh /dev/fb0 " + file)
     os.system("rclone copy " + file + " drive:Pictures")
     
+    # Turn LED off
+    GPIO.output(LED, 0)
+    
 # Set the GPIO pin:
 GPIO.setup(button, GPIO.IN)
+GPIO.setup(LED,    GPIO.OUT)
+GPIO.output(LED, 0)
 
 print("Running...")
 
