eef9faae6798414b9ae5c4befdff8cd873ba89fe
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-11-07 15:53:34 -0500

    Cleaned up file names

--- sensors/IRArray/takePicture.py
+++ sensors/IRArray/takePicture.py
@@ -2,6 +2,8 @@
 # Takes a pictures from the IRarray and copied it to Google Drive
 # Import PyBBIO library:
 import Adafruit_BBIO.GPIO as GPIO
+import os
+import datetime
 
 button="P2_17"  # PAUSE=P8_9, MODE=P8_10
 LED   ="USR3"
@@ -16,5 +18,11 @@ while True:
   state = GPIO.input(button)
   GPIO.output(LED, state)
   
-  GPIO.wait_for_edge(button, GPIO.BOTH)
+  GPIO.wait_for_edge(button, GPIO.FALLING)  # FALLING, RISING, BOTH
   print("Pressed")
+  
+  file = "/tmp/" + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + ".jpg"
+  print(file)
+  
+  os.system("./fb2jpg.sh /dev/fb0 " + file)
+  os.system("rclone copy " + file + " drive:Pictures")
