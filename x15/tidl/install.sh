39658ffc89fb03839fd2582dcf4d1848d69e6bab
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-11-27 20:45:54 -0500

    Added more links

--- x15/tidl/install.sh
+++ x15/tidl/install.sh
@@ -1,1 +1,3 @@
 # From: https://github.com/rcn-ee/tidl-api/tree/v01.00.00.03-bb.org
+# See also: http://downloads.ti.com/mctools/esd/docs/tidl-api/index.html
+# And ... http://software-dl.ti.com/processor-sdk-linux/esd/docs/latest/linux/Foundational_Components_TIDL.html
