2601c42d1d358df28a3aaf463b28025d3cf8857b
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-12-03 14:09:37 -0500

    Added 6x1 instructions

--- displays/matrix64x32/install.sh
+++ displays/matrix64x32/install.sh
@@ -8,6 +8,7 @@
 #	Set Universe Count to 36, Click Save. restart FPPD
 #	
 #	go to Input/Output Setup:Channel Outputs
+#	Select LED Panels tab
 #	Set Panel Layout (WxH) to 3x1
 #	Click Save and restart FPPD
 #	On the LED Panel Layout: set the panles to P-3, P-2, P-1
@@ -15,3 +16,14 @@
 #	Got to Setup lab and click E1.31 Setup
 #	Unicast, IP address of Beagle, Starting Universe 1, # of Universes 36, Last Channel 512
 #	Click Save
+
+# Now switching to a 6x1 layout
+# In fpp
+#	Channel Inputs, Universe Count: 72, Set
+#	Save
+#
+#	Input/Output Setup:Channel Outputs
+#	LED Panels tab
+#	Panel Layout (WxH): 6x1
+#	LED Panel Layout: P-6, P-5, P-4, etc.
+
