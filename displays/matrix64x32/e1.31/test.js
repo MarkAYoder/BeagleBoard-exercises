ee08423dcc93a7f887c32b890e86d3710c183130
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-12-23 15:46:37 -0500

    Trying to update multiple universes.  Not working yet

--- displays/matrix64x32/e1.31/test.js
+++ displays/matrix64x32/e1.31/test.js
@@ -8,22 +8,27 @@ var client = new e131.Client('fpp');  // or use a universe
 var packet = client.createPacket(512);  // we want 8 RGB (x3) slots
 var slotsData = packet.getSlotsData();
 packet.setSourceName('test E1.31 client');
-packet.setUniverse(0x01);  // make universe number consistent with the client
-// packet.setOption(packet.Options.PREVIEW, true);  // don't really change any fixture
+// packet.setUniverse(0x01);  // make universe number consistent with the client
+packet.setOption(packet.Options.PREVIEW, true);  // don't really change any fixture
 // packet.setPriority(packet.DEFAULT_PRIORITY);  // not strictly needed, done automatically
 // console.log("packet: ");
 // console.log(packet);
 console.log("slotsData: " + slotsData.length);
 // console.log(slotsData); 
-console.log("looping...");
+// console.log("looping...");
 // slotsData is a Buffer view, you can use it directly
-var color = 0;
+
 function cycleColor() {
   // console.log("cycleColor");
-  for (var idx=0; idx<slotsData.length; idx++) {
-    slotsData[idx] = color % 0xff;
-    color = color + 90;
-  }
-  client.send(packet);
+    for(var univ=1; univ<9; univ++) {
+	packet.setUniverse(univ);
+	for (var idx=0; idx<slotsData.length; idx+=3) {
+	    slotsData[idx+0] = 0xff;
+	    slotsData[idx+1] = 0x00;
+	    slotsData[idx+2] = 0x00;
+	}
+	client.send(packet);
+    }
 }
-setInterval(cycleColor, 250);
+// setInterval(cycleColor, 250);
+cycleColor();
