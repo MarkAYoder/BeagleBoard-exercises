b08991082f17bbf7ceec2f2d25c2dabf268f995a
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-12-25 13:48:02 -0500

    Cleaning up code.  Works with 510 channel universes

--- displays/matrix64x32/e1.31/test.py
+++ displays/matrix64x32/e1.31/test.py
@@ -3,20 +3,22 @@
 import sacn
 import time
 
-univ = 1
 sender = sacn.sACNsender()  # provide an IP-Address to bind to if you are using Windows and want to use multicast
 sender.start()  # start the sending thread
-for univ in range(1, 14):
+# sender[univ].multicast = False  # set multicast to True
+# Keep in mind that if multicast is on, unicast is not used
+
+for univ in range(1, 74):
 	sender.activate_output(univ)  # start sending out data in the 1st universe
-	# sender[univ].multicast = False  # set multicast to True
 	sender[univ].destination = "fpp"  # or provide unicast information.
-	# Keep in mind that if multicast is on, unicast is not used
+
+for univ in range(1, 74):
 	# sender[univ].dmx_data = (0, 255, 0, 255, 0, 0, 255, 255, 255)  # some test DMX data
 	row = []
-	for i in range(0, 512, 3):
-		row.append(  0)
-		row.append(128)
-		row.append(  0)
+	for i in range(0, 510, 3):
+		row.append( 10)
+		row.append( 10)
+		row.append( 30)
 	sender[univ].dmx_data = row
 
 time.sleep(0.1)  # send the data for 10 seconds
