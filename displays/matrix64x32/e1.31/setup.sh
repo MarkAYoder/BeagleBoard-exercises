5b65eb97fa94f709b76bcb60f43d86505f53bd3b
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-12-26 12:21:57 -0500

    Suggested sshfs paths

---
+++ displays/matrix64x32/e1.31/setup.sh
@@ -0,0 +1,3 @@
+# Paths to various slide shows
+sshfs DiskStation:160G/160G/My\ Pictures/2010/2018/Events/Christmas\ 2018 show
+sshfs DiskStation:160G/160G/My\ Pictures/2010/2018/Trips/Indianapolis\ Children\'s\ Museum show
