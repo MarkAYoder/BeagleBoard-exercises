bd20f03d61df9b3d52a5cc23033f3730581c7ad3
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-12-26 09:48:08 -0500

    Added variables

--- displays/matrix64x32/e1.31/show.sh
+++ displays/matrix64x32/e1.31/show.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-# Do a slide show on the LED matrix
-cols=192
-rows=64
+# Do a slide show on the LED matrix with gamma correction
+# Resized images are kept in /tmp and reused
+
+tmp=/tmp
+show=show	# Where the original images are
+size=192x64
+gamma=0.5
+time=3
+
 while [ 1 ] ; do
-	for file in show/*.jpg; do
-	    echo $file
-	    echo $(basename -- $file)
-	    small=/tmp/$(basename -- $file)
-	    gamma="${small%.*}".gamma."${small#*.}"
+	for file in $show/*.jpg; do
+	    # echo $file
+	    # echo $(basename -- $file)
+	    small=$tmp/$(basename -- $file)
+	    # Stick gamma in the file name
+	    gammafile="${small%.*}".gamma."${small#*.}"
 	    if [ ! -f $small ]; then
 		echo Resizing $file
-		convert $file -resize 192x64 $small
-		convert $small -level 0%,100%,0.5  $gamma
+		convert $file -resize $size $small
+		convert $small -level 0%,100%,$gamma  $gammafile
 	    fi
 	    ./image.py $small
-	    sleep 3
-	    for g in 0.4 0.45 0.5 0.55 0.6 ; do
-		echo gamma: $g
-		convert $small -level 0%,100%,$g $gamma
-	    	./image.py $gamma
-	    	sleep 1
-	    done
+	    sleep $time
+	    ./image.py $gammafile
+	    sleep $time
 	done
 done
