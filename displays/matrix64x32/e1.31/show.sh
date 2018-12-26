be340aa7a8bd875da0ff0f88e5d229483726d723
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-12-26 09:08:10 -0500

    Added gamma

--- displays/matrix64x32/e1.31/show.sh
+++ displays/matrix64x32/e1.31/show.sh
@@ -7,10 +7,19 @@ while [ 1 ] ; do
 	    echo $file
 	    echo $(basename -- $file)
 	    small=/tmp/$(basename -- $file)
+	    gamma="${small%.*}".gamma."${small#*.}"
 	    if [ ! -f $small ]; then
+		echo Resizing $file
 		convert $file -resize 192x64 $small
+		convert $small -level 0%,100%,0.5  $gamma
 	    fi
 	    ./image.py $small
-	    sleep 10
+	    sleep 3
+	    for g in 0.4 0.45 0.5 0.55 0.6 ; do
+		echo gamma: $g
+		convert $small -level 0%,100%,$g $gamma
+	    	./image.py $gamma
+	    	sleep 1
+	    done
 	done
 done
