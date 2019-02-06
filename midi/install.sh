12e1afd9a4311c81c111770634cbf968b0f33208
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-02-05 20:29:37 -0500

    Added midisport-firware

--- midi/install.sh
+++ midi/install.sh
@@ -3,3 +3,5 @@ timidity -iA  # Listens on 4 ports
 aplaymidi -p 128:0 bwv538t.mid	# Plays to one of those ports
 
 sudo apt install kmidimon	# Monitors midi traffice
+
+sudo apt-get install midisport-firmware
