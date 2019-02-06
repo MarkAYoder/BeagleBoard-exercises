aec4b8d55c16f3a8c29c76f1d3424408b2b6e1f9
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-02-05 19:35:29 -0500

    Added midi info

---
+++ midi/install.sh
@@ -0,0 +1,5 @@
+# From: https://sfxpt.wordpress.com/2015/02/02/how-to-play-midi-files-under-ubuntu-linux/
+
+sudo apt-get install freepats timidity timidity-interfaces-extra
+
+# More useful info: http://tedfelix.com/linux/linux-midi.html
