b91dc3e351859854f7f87b5d0b8d780b178ca5ee
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-02-07 19:18:19 -0500

    Move installs, more mid

--- midi/install.sh
+++ midi/install.sh
@@ -5,3 +5,11 @@ aplaymidi -p 128:0 bwv538t.mid	# Plays to one of those ports
 sudo apt install kmidimon	# Monitors midi traffice
 
 sudo apt-get install midisport-firmware
+
+sudo apt install vkeybd
+
+sudo apt-get install libcanberra-gtk-module
+
+
+time sudo apt install vmpk
+
