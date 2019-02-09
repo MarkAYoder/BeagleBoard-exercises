eb8a45ed9aa9406b2dafa4b4940fde8137888bd8
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-02-09 15:44:26 -0500

    Maps channels to 0-2

--- midi/play.py
+++ midi/play.py
@@ -2,15 +2,19 @@
 # From: https://github.com/mido/mido
 
 import mido
-import time
+import time, sys
 
 # See what ports are out there
 # print(mido.get_output_names())
 # print(mido.get_input_names())
 
 organ = "MidiSport 1x1:MidiSport 1x1 MIDI 1 20:0"
+
 filename = 'midifiles/little_f.mid'
-filename = 'midifiles/brand3.mid'
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+
+print("Playing: " + filename)
 
 outport = mido.open_output(organ)
 
@@ -45,19 +49,18 @@ outport = mido.open_output(organ)
 #     if i>500:
 #         break
 
-i=0
 for msg in mido.MidiFile(filename):
-    i=i+1
-    if i>0:
-        time.sleep(msg.time)
-        if not msg.is_meta:
-            outport.send(msg)
-            print(msg)
-        else:
-            print('***')
-            print(msg)
-    if i>5000:
-        break
+    time.sleep(msg.time)
+    if not msg.is_meta:
+        if hasattr(msg, 'channel'):
+            # map all channels to 0-2
+            if msg.channel != 9:   # Don't do drums
+                outport.send(msg.copy(channel=msg.channel%3))
+                # if msg.channel != (msg.channel%3):
+                #   print(msg)
+    else:
+        print('***')
+        print(msg)
 
 # port.panic()
 # Stop all notes
