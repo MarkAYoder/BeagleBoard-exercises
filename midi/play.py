b4247a4be88c2ee1ae5332dad5c5c7810f3019a4
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-02-09 12:58:02 -0500

    Added meta message detection

--- midi/play.py
+++ midi/play.py
@@ -9,15 +9,17 @@ import time
 # print(mido.get_input_names())
 
 organ = "MidiSport 1x1:MidiSport 1x1 MIDI 1 20:0"
+filename = 'midifiles/little_f.mid'
+filename = 'midifiles/brand3.mid'
 
 outport = mido.open_output(organ)
 
-msg = mido.Message('note_on', note=60, channel=2)
+# msg = mido.Message('note_on', note=60, channel=2)
 # msg.type
 # msg.note
 # msg.bytes()
 # msg.copy(channel=2)
-print(msg)
+# print(msg)
 
 
 # print(outport)
@@ -33,14 +35,28 @@ print(msg)
 #         if msg.note == 36:
 #             break
 
-mid = mido.MidiFile('midifiles/little_f.mid')
+# mid = mido.MidiFile(filename)
+# i=0
+# for msg in mid.play():
+#     # if i>200:
+#     outport.send(msg)
+#     print(msg)
+#     i=i+1
+#     if i>500:
+#         break
+
 i=0
-for msg in mid.play():
-    if i>200:
-        outport.send(msg)
-        print(msg)
+for msg in mido.MidiFile(filename):
     i=i+1
-    if i>500:
+    if i>0:
+        time.sleep(msg.time)
+        if not msg.is_meta:
+            outport.send(msg)
+            print(msg)
+        else:
+            print('***')
+            print(msg)
+    if i>5000:
         break
 
 # port.panic()
