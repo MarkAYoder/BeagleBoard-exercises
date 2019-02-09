abd9e3bf6145823d0a7c15904b60ae9cbb5a7b7d
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-02-09 11:44:13 -0500

    Learning how to play notes

--- midi/play.py
+++ midi/play.py
@@ -2,20 +2,37 @@
 # From: https://github.com/mido/mido
 
 import mido
-msg = mido.Message('note_on', note=60)
-msg.type
-msg.note
-msg.bytes()
+import time
 
-msg.copy(channel=2)
+# See what ports are out there
+# print(mido.get_output_names())
+# print(mido.get_input_names())
 
-port = mido.open_output('Port Name')
-port.send(msg)
+outport = mido.open_output("MidiSport 1x1:MidiSport 1x1 MIDI 1 20:0")
 
-with mido.open_input() as inport:
-    for msg in inport:
-        print(msg)
+msg = mido.Message('note_on', note=60, channel=2)
+# msg.type
+# msg.note
+# msg.bytes()
+# msg.copy(channel=2)
+print(msg)
 
-mid = mido.MidiFile('song.mid')
-for msg in mid.play():
-    port.send(msg)
+
+# print(outport)
+outport.send(msg)
+
+time.sleep(1)
+
+# outport.send(mido.Message('note_off', note=60))
+
+# with mido.open_input() as inport:
+#     for msg in inport:
+#         print(msg)
+
+# mid = mido.MidiFile('song.mid')
+# for msg in mid.play():
+#     port.send(msg)
+
+# port.panic()
+# Stop all notes
+outport.reset()
