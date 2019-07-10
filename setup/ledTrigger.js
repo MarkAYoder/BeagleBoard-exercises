1d159de21f7f8c9bbfd06fb84bb1bbf5ae1a4d03
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-07-10 09:23:37 -0400

    Removed gpio49

--- setup/ledTrigger.js
+++ setup/ledTrigger.js
@@ -8,12 +8,12 @@ if(process.argv[2] === 'off') {
     fs.writeFile(path+'1/trigger', 'none');
     fs.writeFile(path+'2/trigger', 'none');
     fs.writeFile(path+'3/trigger', 'none');
-    fs.writeFile('/sys/class/leds/wifi/brightness', '0');
-    fs.writeFile('/sys/class/gpio/gpio49/value', '0');
+//    fs.writeFile('/sys/class/leds/wifi/brightness', '0');
+//    fs.writeFile('/sys/class/gpio/gpio49/value', '0');
 } else {
     fs.writeFile(path+'0/trigger', 'heartbeat');
     fs.writeFile(path+'1/trigger', 'mmc0');
     fs.writeFile(path+'2/trigger', 'cpu0');
     fs.writeFile(path+'3/trigger', 'mmc1');
-    fs.writeFile('/sys/class/gpio/gpio49/value', '1');
+//    fs.writeFile('/sys/class/gpio/gpio49/value', '1');
 }
