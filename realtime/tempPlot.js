f7e5bc84fba3ce71555f8ac9cf9bc17f47a56848
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-08-16 16:09:39 -0400

    Building temp plotting page

---
+++ realtime/tempPlot.js
@@ -0,0 +1,214 @@
+    var socket;
+    var firstconnect = true,
+        samples = 100,          // Number of values to plot
+        gpioNum = ["P9_12", "P9_14"],   // GPIO pins to plot
+        ainNum  = ["P9_37", "P9_35"],   // Analog ins to plot
+        plotTop, plotBot,
+        topTimer, botTimer,     // The setInterval timers
+        gpioData = [], igpio = [],  // The data to plot and the current index
+        ainData  = [], iain  = [],
+        i;
+        var updateBotInterval = 1000;    // Update interval in ms
+        // var updateTopInterval = 100;
+
+    // Initialize gpioData and ainData to be an array of arrays
+    for (i = 0; i < gpioNum.length; i++) {
+        gpioData[i] = [0];         // Put an array in
+        gpioData[i][samples] = 0;  // Preallocate sample elements
+        igpio[i] = 0;
+    }
+    for(i=0; i<ainNum.length; i++) {
+        ainData[i] = [0];
+        ainData[i][samples] = 0;
+        iain[i] = 0;
+    }
+    
+    function connect() {
+      if(firstconnect) {
+        socket = io.connect(null);
+
+        socket.on('message', function(data)
+            { status_update("Received: message " + data);});
+        socket.on('connect', function()
+            { status_update("Connected to Server"); });
+        socket.on('disconnect', function()
+            { status_update("Disconnected from Server"); });
+        socket.on('reconnect', function()
+            { status_update("Reconnected to Server"); });
+        socket.on('reconnecting', function( nextRetry )
+            { status_update("Reconnecting in " + nextRetry/1000 + " s"); });
+        socket.on('reconnect_failed', function()
+            { status_update("Reconnect Failed"); });
+
+        // socket.on('ain',  ain);
+        socket.on('gpio', gpio);
+
+        firstconnect = false;
+        updateTop();
+      }
+      else {
+        socket.socket.reconnect();
+        botTimer = setInterval(updateBot, updateBotInterval);
+        //topTimer = setInterval(updateTop, updateTopInterval);
+        updateTop();
+        }
+    }
+
+    function disconnect() {
+      clearInterval(botTimer);
+      //clearInterval(topTimer);
+      socket.disconnect();
+    }
+
+    function led(ledNum) {
+        socket.emit('led', ledNum);
+    }
+
+    function trigger(trig) {
+        socket.emit('trigger', trig);
+    }
+
+    // When new data arrives, convert it and plot it.
+//     function ain(data) {
+//         var idx = ainNum.indexOf(data.pin); // Find position in ainNum array
+// //        var num = data[0];
+// //        data = data.value;
+// //        status_update("ain" + num + "(" + idx + "): " + data + ", iain: " + iain[idx]);
+//         ainData[idx][iain[idx]] = [iain[idx], data.value];
+//         iain[idx]++;
+//         if(iain[idx] >= samples) {
+//             iain[idx] = 0;
+//             ainData[idx] = [];
+//         }
+//         plotTop.setData(ainData);
+//         plotTop.draw();
+//         setTimeout(function(){socket.emit("ain", data.pin);}, updateTopInterval);
+//     }
+
+    function gpio(data) {
+	    var idx = gpioNum.indexOf(data.pin);
+//        var num = data[0];
+//        data = atob(data[1]);
+//        data = data[1];
+        gpioData[idx][igpio[idx]] = [igpio[idx], data.value];
+        igpio[idx]++;
+//        status_update("gpio" + num + "(" + idx + "): " + data + " igpio: " + igpio[idx]);
+        if(igpio[idx] >= samples) {
+            igpio[idx] = 0;
+            gpioData[idx] = [];
+        }
+        plotBot.setData(gpioData);
+        plotBot.draw();
+    }
+
+    function status_update(txt){
+      document.getElementById('status').innerHTML = txt;
+    }
+
+    // Request data every updateInterval ms
+    // function updateTop() {
+    //     var i;
+    //     for(i=0; i<ainNum.length; i++) {
+    //         socket.emit("ain", ainNum[i]);
+    //     }
+    // }
+    //topTimer = setInterval(updateTop, updateTopInterval);
+
+    function updateBot() {
+        var i;
+        for (i = 0; i < gpioNum.length; i++) {
+            socket.emit("gpio", {err:0, gpioNum:gpioNum[i]});
+        }
+    }
+    botTimer = setInterval(updateBot, updateBotInterval);
+
+// Here's the jQuery
+$(function () {
+
+    function initPlotData() {
+        // zip the generated y values with the x values
+        var result = [];
+        for (var i = 0; i <= samples; i++)
+            result[i] = [i, 0];
+        return result;
+    }
+
+    // setup control widget
+    $("#ainNum").val(ainNum).change(function () {
+        ainNum = $(this).val().split(",");
+        updateTop();
+    });
+
+    $("#gpioNum").val(gpioNum).change(function () {
+        gpioNum = $(this).val().split(",");
+    });
+
+    $("#updateTopInterval").val(updateTopInterval).change(function () {
+        var v = $(this).val();
+        if (v && !isNaN(+v)) {
+            updateTopInterval = +v;
+            if (updateTopInterval < 10)
+                updateTopInterval = 10;
+            if (updateTopInterval > 2000)
+                updateTopInterval = 2000;
+            $(this).val("" + updateTopInterval);
+        }
+        // Reset the timer
+        //clearInterval(topTimer);
+        //topTimer = setInterval(updateTop, updateTopInterval);
+    });
+
+    $("#updateBotInterval").val(updateBotInterval).change(function () {
+        var v = $(this).val();
+        if (v && !isNaN(+v)) {
+            updateBotInterval = +v;
+            if (updateBotInterval < 10)
+                updateBotInterval = 10;
+            if (updateBotInterval > 2000)
+                updateBotInterval = 2000;
+            $(this).val("" + updateBotInterval);
+        }
+        // Reset timer
+        clearInterval(botTimer);
+        botTimer = setInterval(updateBot, updateBotInterval);
+    });
+
+    // setup plot
+    var optionsTop = {
+        series: { 
+            shadowSize: 0, // drawing is faster without shadows
+            points: { show: false},
+            lines:  { show: true, lineWidth: 5}
+        }, 
+        yaxis:	{ min: 0, max: 2 },
+        xaxis:	{ show: true },
+        legend:	{ position: "ne" }
+    };
+    plotTop = $.plot($("#plotTop"), 
+        [ 
+          { data:  initPlotData(), 
+            label: "Bird Sonar"},
+          { data:  initPlotData(),
+            label: "Scanner Sonar"}
+        ],
+            optionsTop);
+
+    var optionsBot = {
+        series: { 
+            shadowSize: 0, // drawing is faster without shadows
+            points: { show: false},
+            lines:  { show: true, lineWidth: 5}
+        }, 
+        yaxis:	{ min: 0, max: 2 },
+        xaxis:	{ show: true },
+        legend:	{ position: "ne" }
+    };
+    plotBot = $.plot($("#plotBot"), 
+        [ 
+          { data:  initPlotData(),
+            label: "P9_12"},
+          { data:  initPlotData(),
+            label: "P9_14"}
+        ],
+            optionsBot);
+});
---
+++ realtime/tempServer.js
@@ -0,0 +1,183 @@
+#!/usr/bin/node
+// From Getting Started With node.js and socket.io 
+// http://codehenge.net/blog/2011/12/getting-started-with-node-js-and-socket-io-v0-7-part-2/
+// This is a general server for the various web frontends
+// buttonBox, ioPlot, realtimeDemo
+"use strict";
+
+var port = 9090, // Port to listen on
+    bus = '/dev/i2c-1',
+    busNum = 1,     // i2c bus number
+    i2cNum = 0,             // Remembers the address of the last request
+    http = require('http'),
+    url = require('url'),
+    fs = require('fs'),
+    b = require('bonescript'),
+    child_process = require('child_process'),
+    server,
+    connectCount = 0,	// Number of connections to server
+    errCount = 0;	// Counts the AIN errors.
+    
+//  Audio
+    var frameCount = 0,     // Counts the frames from arecord
+        lastFrame = 0,      // Last frame sent to browser
+        audioData,          // all data from arecord is saved here and sent
+			                // to the client when requested.
+        audioChild = 0,     // Process for arecord
+        audioRate = 8000;
+        
+        // PWM
+        var pwm = 'P9_21';
+
+// Initialize various IO things.
+function initIO() {
+    // Make sure gpios 7 and 20 are available.
+    b.pinMode('P9_12', b.INPUT);
+    b.pinMode('P9_14', b.INPUT);
+    b.pinMode(pwm,     b.ANALOG_OUTPUT);    // PWM
+    
+    // Initialize pwm
+}
+
+function send404(res) {
+    res.writeHead(404);
+    res.write('404');
+    res.end();
+}
+
+initIO();
+
+server = http.createServer(function (req, res) {
+// server code
+    var path = url.parse(req.url).pathname;
+    console.log("path: " + path);
+    if (path === '/') {
+        path = '/tempPlot.html';
+    }
+
+    fs.readFile(__dirname + path, function (err, data) {
+        if (err) {return send404(res); }
+//            console.log("path2: " + path);
+        res.write(data, 'utf8');
+        res.end();
+    });
+});
+
+server.listen(port);
+console.log("Listening on " + port);
+
+// socket.io, I choose you
+var io = require('socket.io').listen(server);
+// io.set('log level', 2);
+
+// See https://github.com/LearnBoost/socket.io/wiki/Exposed-events
+// for Exposed events
+
+// on a 'connection' event
+io.sockets.on('connection', function (socket) {
+
+    console.log("Connection " + socket.id + " accepted.");
+//    console.log("socket: " + socket);
+
+    // now that we have our connected 'socket' object, we can 
+    // define its event handlers
+
+    // Send value every time a 'message' is received.
+//     socket.on('ain', function (err, ainNum) {
+//         b.analogRead(ainNum, function(x) {
+//             if(x.err && errCount++<5) console.log("AIN read error");
+//             if(typeof x.value !== 'number' || x.value === "NaN") {
+//                 console.log('x.value = ' + x.value);
+//             } else {
+//                 socket.emit('ain', {pin:ainNum, value:x.value});
+//             }
+// //            if(ainNum === "P9_38") {
+// //                console.log('emitted ain: ' + x.value + ', ' + ainNum);
+// //            }
+//         });
+//     });
+
+    socket.on('gpio', function (xx) {
+        var gpioNum = xx.gpioNum;
+    console.log('gpio' + gpioNum);
+        b.digitalRead(gpioNum, function(x) {
+            if (x.err) throw x.err;
+            socket.emit('gpio', {pin:gpioNum, value:x.value});
+            console.log('emitted gpio: ' + x.value + ', ' + gpioNum);
+        });
+    });
+
+    socket.on('led', function (ledNum) {
+        var ledPath = "/sys/class/leds/beaglebone:green:usr" + ledNum + "/brightness";
+//        console.log('LED: ' + ledPath);
+        fs.readFile(ledPath, 'utf8', function (err, data) {
+            if(err) throw err;
+            data = data.substring(0,1) === "1" ? "0" : "1";
+//            console.log("LED%d: %s", ledNum, data);
+            fs.writeFile(ledPath, data);
+        });
+    });
+
+    function trigger(arg) {
+        var ledPath = "/sys/class/leds/beaglebone:green:usr";
+//    console.log("trigger: " + arg);
+	    arg = arg.split(" ");
+	    for(var i=0; i<4; i++) {
+//	    console.log(" trigger: ", arg[i]);
+	        fs.writeFile(ledPath + i + "/trigger", arg[i]);
+	    }
+    }
+    
+    socket.on('trigger', function(trig) {
+//	console.log('trigger: ' + trig);
+	    if(trig) {
+            trigger("heartbeat mmc0 cpu0 none");
+        } else {
+            trigger("none none none none");
+        }
+    });
+    
+    // Send a packet of data every time a 'audio' is received.
+    socket.on('audio', function () {
+//        console.log("Received message: " + message + 
+//            " - from client " + socket.id);
+        if(audioChild === 0) {
+            startAudio();
+        }
+        socket.emit('audio', sendAudio() );
+    });
+    
+    socket.on('matrix.bs', function (i2cNum) {
+        var i;
+        var line = new Array(16);
+        // console.log('Got i2c request:' + i2cNum);
+        b.i2cOpen(bus, 0x70);
+        for(i=0; i<16; i++) {
+            // Can only read one byte at a time.  Something's wrong
+            line[i] = b.i2cReadBytes(bus, i, 1)[0].toString(16);
+            // console.log("line: " + JSON.stringify(line[i]));
+        }
+        console.log(line.join(' '));
+        socket.emit('matrix', line.join(' '));
+    });
+
+
+    socket.on('disconnect', function () {
+        console.log("Connection " + socket.id + " terminated.");
+        connectCount--;
+        console.log("connectCount = " + connectCount);
+    });
+
+    connectCount++;
+    console.log("connectCount = " + connectCount);
+});
+
+function sendAudio() {
+//        console.log("Sending data");
+    if(frameCount === lastFrame) {
+//            console.log("Already sent frame " + lastFrame);
+    } else {
+        lastFrame = frameCount;
+    }
+    return(audioData);
+}
