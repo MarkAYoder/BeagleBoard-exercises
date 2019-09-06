88170112505236abab000b486110be280656bd38
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-09-06 16:17:36 -0400

    Added SPI 1 bus

--- displays/ili9341/fb/on.sh
+++ displays/ili9341/fb/on.sh
@@ -5,9 +5,13 @@
 export LED=51         # P9_16
 
 # This is for the Black SPI 0
-export RESET=12     # RESET - P9_20
-export DC=13        # D/C   - P9_19
-export CS=5         # CS    - P9_17
+# export RESET=12     # RESET - P9_20
+# export DC=13        # D/C   - P9_19
+# export CS=5         # CS    - P9_17
+# This is for the Black SPI 1
+export RESET=117    # RESET - P9_25
+export DC=115       # D/C   - P9_27
+export CS=113       # CS    - P9_28
 # This is for the Pocket SPI 0
 # export RESET=57     # RESET - P2.06
 # export DC=58        # D/C   - P2.04
@@ -20,12 +24,19 @@ sudo bash << EOF
     if lsmod | grep -q 'fbtft '        ; then rmmod --force fbtft;         fi
 
     # Set the pinmuxes for the display
-    config-pin P9_19 gpio   # D/C
-    config-pin P9_20 gpio   # RESET
-    config-pin P9_18 spi    # spi 0_d1 MOSI
-    config-pin P9_21 spi    # spi 0_d0 MISO
-    config-pin P9_22 spi_sclk # spi 0_sclk
-    config-pin P9_17 spi_cs # spi 0_cs0
+    # config-pin P9_19 gpio   # D/C
+    # config-pin P9_20 gpio   # RESET
+    # config-pin P9_18 spi    # spi 0_d1 MOSI
+    # config-pin P9_21 spi    # spi 0_d0 MISO
+    # config-pin P9_22 spi_sclk # spi 0_sclk
+    # config-pin P9_17 spi_cs # spi 0_cs0
+    
+    config-pin P9_27 gpio   # D/C
+    config-pin P9_25 gpio   # RESET
+    config-pin P9_30 spi    # spi 1_d1 MOSI
+    config-pin P9_29 spi    # spi 1_d0 MISO
+    config-pin P9_31 spi_sclk # spi 1_sclk
+    config-pin P9_28 spi_cs # spi 1_cs0
     
     # LED pin, turn on
     ./backlight.py
@@ -33,7 +44,7 @@ sudo bash << EOF
     sleep 0.1
     
     # Insert the framebuffer modules
-    modprobe fbtft_device name=adafruit28 busnum=1 rotate=90 gpios=reset:$RESET,dc:$DC cs=0
+    modprobe fbtft_device name=adafruit28 busnum=2 rotate=90 gpios=reset:$RESET,dc:$DC cs=0
 
     # Turn off cursor
     while [ ! -e /dev/fb0 ]
