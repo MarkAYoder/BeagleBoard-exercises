cff2b25c0553ba4bc7cb6c7f1a2c54bc82828d16
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-09-11 14:50:34 -0400

    Moved to eQEP2

--- sensors/eQEP/encoderBBIO.py
+++ sensors/eQEP/encoderBBIO.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # From: https://adafruit-beaglebone-io-python.readthedocs.io/en/latest/Encoder.html
-from Adafruit_BBIO.Encoder import RotaryEncoder, eQEP1
+from Adafruit_BBIO.Encoder import RotaryEncoder, eQEP2
 import time
 
 # Instantiate the class to access channel eQEP2, and initialize
 # that channel
 
-myEncoder = RotaryEncoder(eQEP1)
+myEncoder = RotaryEncoder(eQEP2)
 myEncoder.setAbsolute()
 myEncoder.enable()
 
