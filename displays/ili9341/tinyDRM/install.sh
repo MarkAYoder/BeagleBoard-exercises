39746994f7376153bc0f5af813609c9307710541
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-10-03 15:44:50 -0400

    Moved LED to P9_16

--- displays/ili9341/tinyDRM/install.sh
+++ displays/ili9341/tinyDRM/install.sh
@@ -1,8 +1,7 @@
+# Here's where the original file came from
+# wget https://github.com/beagleboard/bb.org-overlays/raw/master/src/arm/BB-LCD-ADAFRUIT-18-SPI1-00A0.dts
 
-wget https://github.com/beagleboard/bb.org-overlays/raw/master/src/arm/BB-LCD-ADAFRUIT-18-SPI1-00A0.dts
-
-mv BB-LCD-ADAFRUIT-18-SPI1-00A0.dts BB-LCD-ADAFRUIT-28-SPI1-00A0.dts
-
+# mv BB-LCD-ADAFRUIT-18-SPI1-00A0.dts BB-LCD-ADAFRUIT-28-SPI1-00A0.dts
 
 # Edit /boot/uEnv.txt add:
 ###Additional custom capes
