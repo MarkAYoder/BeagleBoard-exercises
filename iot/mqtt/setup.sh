11d064d5ac55c475c177d6793168fae8c95d635f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-02-29 15:25:19 -0500

    Added i2c confg-pin

--- iot/mqtt/setup.sh
+++ iot/mqtt/setup.sh
@@ -5,3 +5,7 @@ mosquitto_sub -d -t hello/world
 # Publishes data
 
 mosquitto_pub -d -t hello/world -m "Hello, MQTT. This is my first message."
+
+# This is for reading the tmp101 sensor on i2c bus 1
+config-pin P9_24 i2c
+config-pin P9_26 i2c
