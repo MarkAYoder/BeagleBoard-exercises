8469d0b4189b3733f7fac0000847e3f49aaec11c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-03-01 22:17:08 +0000

    Combined apt installs

--- iot/mqtt/install.sh
+++ iot/mqtt/install.sh
@@ -1,5 +1,4 @@
 # From: http://michaelconnors.net/article/462/hello-mqtt
 # https://www.vultr.com/docs/how-to-install-mosquitto-mqtt-broker-server-on-ubuntu-16-04
 
-sudo apt install mosquitto
-sudo apt install mosquitto-clients
+time sudo apt install mosquitto mosquitto-clients
