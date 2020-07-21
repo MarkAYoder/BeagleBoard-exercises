d5e800f2fbc4dab41f16db883d22f03f2633259c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-07-21 15:10:59 -0400

    Changed channel name

--- iot/slack/post.sh
+++ iot/slack/post.sh
@@ -1,4 +1,5 @@
 # From: https://www.cloudsavvyit.com/289/how-to-send-a-message-to-slack-from-a-bash-script/
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World 2!"}' \
-https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World 5!"}' \
+https://hooks.slack.com/services/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
+echo
