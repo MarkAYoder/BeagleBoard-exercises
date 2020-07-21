590e012555899c93095084b6a57212e7bf8e88ec
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-07-21 14:44:02 -0400

    Added posting to slack from bash

---
+++ iot/slack/post.sh
@@ -0,0 +1,4 @@
+# From: https://www.cloudsavvyit.com/289/how-to-send-a-message-to-slack-from-a-bash-script/
+
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World 2!"}' \
+https://hooks.slack.com/services/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
