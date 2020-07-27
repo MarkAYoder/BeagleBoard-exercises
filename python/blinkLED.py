2b49d4d31ac4030a408a150182bf62680afc8597
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-07-27 16:05:39 -0400

    Added LED

--- python/blinkLED.py
+++ python/blinkLED.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-GPIO.setup("P9_14", GPIO.OUT)
+LED="P9_11"
+
+GPIO.setup(LED, GPIO.OUT)
 
 while True:
-    GPIO.output("P9_14", GPIO.HIGH)
+    GPIO.output(LED, GPIO.HIGH)
     time.sleep(0.5)
-    GPIO.output("P9_14", GPIO.LOW)
+    GPIO.output(LED, GPIO.LOW)
     time.sleep(0.5)
