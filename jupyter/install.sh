0f3173ba60cceb41763be80f1f3ed0aecad03643
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-07-29 16:50:25 -0400

    Updated install.sh, added gpio.ipynb

--- jupyter/install.sh
+++ jupyter/install.sh
@@ -1,6 +1,3 @@
 # From: http://jupyter.org/install.html
 
-sudo apt update
-sudo apt install python3-pip
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install jupyter
+pip install notebook
