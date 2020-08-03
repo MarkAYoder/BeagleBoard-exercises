7ab0c79bfba8e379533cee96c5753e6527a1aed4
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-08-03 19:09:33 -0400

    Added /dev/remoteproc

--- setup/remoteproc.sh
+++ setup/remoteproc.sh
@@ -9,3 +9,8 @@ cd /lib/firmware
 touch $FW
 chown root:remoteproc $FW
 chmod g+w $FW
+
+# Set up remoteproc links
+cd /dev/remoteproc/
+ln -s /sys/class/remoteproc/remoteproc1 pruss-core0
+ln -s /sys/class/remoteproc/remoteproc2 pruss-core1
