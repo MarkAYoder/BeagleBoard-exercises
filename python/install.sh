87ded06ec3d4b6eba19b94d925b9e3ba6a1c703c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-08-05 10:24:45 -0400

    BBIO is already installed

--- python/install.sh
+++ python/install.sh
@@ -1,8 +1,1 @@
-# Installing Adafruit_BBIO
-sudo apt install python3-pip
-git clone https://github.com/MarkAYoder/adafruit-beaglebone-io-python.git
-cd adafruit-beaglebone-io-python
-python3 setup.py install
-
-# From: http://www.scipy.org/install.html
-sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+# Adafruit_BBIO is already installed.
