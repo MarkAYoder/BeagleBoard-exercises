66a4ad5a3640adb0dfaa30dc3a7bbec8d2e0ea28
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-08-06 19:39:59 -0400

    Moved pwm to a module

--- python/PWMmay.py
+++ python/PWMmay.py
@@ -2,16 +2,16 @@
 
 import glob
 
-NULL=0
-module = 0
-sysfs  = 1
-index  = 2
-muxmode= 3
-path   = 4
-name   = 5
-chip   = 6
-addr   = 7
-key    = 8
+t_NULL=0
+t_module = 0
+t_sysfs  = 1
+t_index  = 2
+t_muxmode= 3
+t_path   = 4
+t_name   = 5
+t_chip   = 6
+t_addr   = 7
+t_key    = 8
 
 pwm_table = [
   [ "ehrpwm2", 6, 1, 4, "ehrpwm.2:1", "EHRPWM2B", "48304000", "48304200", "P8_13"],
@@ -46,22 +46,41 @@ pwm_table = [
 #   [ NULL, 0, 0, 0, NULL, NULL, NULL, NULL, NULL ]
 ]
 
-print(pwm_table[2][key])
-print(len(pwm_table))
+# print(pwm_table[2][t_key])
+# print(len(pwm_table))
 
 def get_pwm_key(channel):
     for i in range(len(pwm_table)):
-        if pwm_table[i][key] == channel:
+        if pwm_table[i][t_key] == channel:
             return pwm_table[i]
 
 def get_pwm_path(channel):
     x = get_pwm_key(channel)
     if x != None:
-        pwmPath =  glob.glob("/sys/devices/platform/ocp/" + x[chip] + ".epwmss/" 
-                + x[addr] + ".pwm/pwm/*")[0] + "/export"
-        print(pwmPath)
-        # print(glob.glob(pwmPath))
-        
-print(get_pwm_key("P9_14"))
+        pwmPath =  glob.glob("/sys/devices/platform/ocp/" + x[t_chip] + ".epwmss/" 
+                + x[t_addr] + ".pwm/pwm/*")[0]
+        # print(pwmPath)
+        return [pwmPath, x[t_index]]
 
-print(get_pwm_path("P9_14"))
+#PWM.start(channel, duty, freq=2000, polarity=0)
+
+def start(channel, duty):
+    path = get_pwm_path(channel)
+    
+    # export
+    try:
+        file1 = open(path[0] + "/export", 'w')
+        file1.write(str(path[1])) 
+        file1.close()
+    except:
+        pass
+
+    unit = path[-1]
+    
+def stop(channel):
+    path = get_pwm_path(channel)
+   
+    # unexport
+    file1 = open(path[0] + "/unexport", 'w')
+    file1.write(str(path[1])) 
+    file1.close()
---
+++ python/PWMtest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import glob
+import PWMmay
+
+# print(PWMmay.get_pwm_key("P9_14"))
+
+# print(PWMmay.get_pwm_path("P9_14"))
+
+print(PWMmay.start("P9_14", 50))
+
+# print(PWMmay.stop("P9_14"))
