ec6e051cf6eb1143c3be1f41d6925449e6a17ed2
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-08-10 10:41:31 -0400

    Added c version

--- gpiod/toggle.c
+++ gpiod/toggle.c
@@ -1,33 +1,56 @@
-// From: https://developer.toradex.com/knowledge-base/libgpiod
-// gcc -gpiod toggle.c -o toggle
+// From: https://github.com/starnight/libgpiod-example/blob/master/libgpiod-led/main.c
 #include <gpiod.h>
+#include <stdio.h>
+#include <unistd.h>
 
-void main() {
-//Define GPIO chip and line structs:
-	struct gpiod_chip *output_chip;
-	struct gpiod_line *output_line;
+#ifndef	CONSUMER
+#define	CONSUMER	"Consumer"
+#endif
 
-// Configure a GPIO as output:
-	/* open the GPIO bank */
-	int bank = 1;
-	output_chip = gpiod_chip_open_by_number(bank);
- 
-	/* open the GPIO line */
-	int line = 18;
-	output_line = gpiod_chip_get_line(output_chip, line);
- 
-	/* config as output and set a description */
-	gpiod_line_request_output(output_line, "gpio-test",
-		GPIOD_LINE_ACTIVE_STATE_HIGH);
+int main(int argc, char **argv)
+{
+	char *chipname = "gpiochip1";
+	unsigned int line_num = 18;	// GPIO Pin #23
+	unsigned int val;
+	struct gpiod_chip *chip;
+	struct gpiod_line *line;
+	int i, ret;
 
-// Toggle a GPIO:
-	while(1) {
-	    /* Clear */
-	    int line_value = 0;
-	    gpiod_line_set_value(output_line, line_value);
-	 
-	    /* Set */
-	    line_value = 1;
-	    gpiod_line_set_value(output_line, line_value);
+	chip = gpiod_chip_open_by_name(chipname);
+	if (!chip) {
+		perror("Open chip failed\n");
+		goto end;
 	}
+
+	line = gpiod_chip_get_line(chip, line_num);
+	if (!line) {
+		perror("Get line failed\n");
+		goto close_chip;
+	}
+
+	ret = gpiod_line_request_output(line, CONSUMER, 0);
+	if (ret < 0) {
+		perror("Request line as output failed\n");
+		goto release_line;
+	}
+
+	/* Blink 20 times */
+	val = 0;
+	while(1) {
+		ret = gpiod_line_set_value(line, val);
+		if (ret < 0) {
+			perror("Set line output failed\n");
+			goto release_line;
+		}
+		// printf("Output %u on line #%u\n", val, line_num);
+		// sleep(1);
+		val = !val;
+	}
+
+release_line:
+	gpiod_line_release(line);
+close_chip:
+	gpiod_chip_close(chip);
+end:
+	return 0;
 }
