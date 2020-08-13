10b5bfb3577cfd141116868694ec9835943c3162
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-08-13 15:10:40 -0400

    Added numpyPlay

---
+++ python/numpyPlay.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# From: https://docs.scipy.org/doc/scipy/reference/tutorial/general.html
+# from scipy import misc
+# import matplotlib.pyplot as plt
+
+# face = misc.face()
+# plt.imshow(face)
+# plt.show()
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
