e00ededd60db9ed8c8bb50122ce505316b74d897
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-09-10 12:55:48 -0400

    Switched to SP1

--- displays/ili9341/fb/on.sh
+++ displays/ili9341/fb/on.sh
@@ -5,15 +5,15 @@
 export LED=51         # P9_16
 
 # This is for the Black SPI 0
-export RESET=12     # RESET - P9_20
-export DC=13        # D/C   - P9_19
-export CS=5         # CS    - P9_17
-export BUS=0        # SPI bus 0
+# export RESET=12     # RESET - P9_20
+# export DC=13        # D/C   - P9_19
+# export CS=5         # CS    - P9_17
+# export BUS=0        # SPI bus 0
 # This is for the Black SPI 1
-# export RESET=117    # RESET - P9_25
-# export DC=115       # D/C   - P9_27
-# export CS=113       # CS    - P9_28
-# export BUS=1        # SPI bus 1
+export RESET=117    # RESET - P9_25
+export DC=115       # D/C   - P9_27
+export CS=113       # CS    - P9_28
+export BUS=1        # SPI bus 1
 # This is for the AI SPI 2 & 3
 # export RESET=177    # RESET - P9_25
 # export DC=111       # D/C   - P9_27
@@ -35,20 +35,20 @@ sudo bash << EOF
 
     # Set the pinmuxes for the display
     # Black SPI 0
-    config-pin P9_19 gpio   # D/C
-    config-pin P9_20 gpio   # RESET
-    config-pin P9_18 spi    # spi 0_d1 MOSI
-    config-pin P9_21 spi    # spi 0_d0 MISO
-    config-pin P9_22 spi_sclk # spi 0_sclk
-    config-pin P9_17 spi_cs # spi 0_cs0
+    # config-pin P9_19 gpio   # D/C
+    # config-pin P9_20 gpio   # RESET
+    # config-pin P9_18 spi    # spi 0_d1 MOSI
+    # config-pin P9_21 spi    # spi 0_d0 MISO
+    # config-pin P9_22 spi_sclk # spi 0_sclk
+    # config-pin P9_17 spi_cs # spi 0_cs0
     
     # Black SPI 1
-    # config-pin P9_27 gpio   # D/C
-    # config-pin P9_25 gpio   # RESET
-    # config-pin P9_30 spi    # spi 1_d1 MOSI
-    # config-pin P9_29 spi    # spi 1_d0 MISO
-    # config-pin P9_31 spi_sclk # spi 1_sclk
-    # config-pin P9_28 spi_cs # spi 1_cs0
+    config-pin P9_27 gpio   # D/C
+    config-pin P9_25 gpio   # RESET
+    config-pin P9_30 spi    # spi 1_d1 MOSI
+    config-pin P9_29 spi    # spi 1_d0 MISO
+    config-pin P9_31 spi_sclk # spi 1_sclk
+    config-pin P9_28 spi_cs # spi 1_cs0
 
     # LED pin, turn on
     ./LCD-backlight.py
