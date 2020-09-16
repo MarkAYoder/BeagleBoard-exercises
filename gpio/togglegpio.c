f4b688d5874a1bb71f2848609d118ea6ed531bb9
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-09-16 15:53:56 -0400

    Fixed gpio number

--- gpio/togglegpio.c
+++ gpio/togglegpio.c
@@ -22,7 +22,7 @@ int main(int argc, char** argv)
 	//Integer to keep track of whether we want on or off
 	int toggle = 0;
 	int onOffTime;	// Time in micro sec to keep the signal on/off
-	int gpio = 48;
+	int gpio = 60;
 	int gpio_fd;
 
 	if (argc < 2) {
