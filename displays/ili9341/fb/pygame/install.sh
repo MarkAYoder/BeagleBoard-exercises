ebc3ed62f97ebcef39fa84fb0f3144aee24273ab
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-09-30 09:22:06 -0400

    Simplified

--- displays/ili9341/fb/pygame/install.sh
+++ displays/ili9341/fb/pygame/install.sh
@@ -1,16 +1,4 @@
 # From: https://www.pygame.org/wiki/GettingStarted
-# I don't know if all of these are needed.
-# sudo apt get python3-numpy python3-pip python3-dev 
-# sudo apt install mercurial libav-tools libsdl-ttf2.0-dev  # 100M, 8m23
 
-# libav-tools -> ffmpeg
-
-# sudo apt install libsmpeg-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev # 18.1M, 1m29
-# sudo apt install libfreetype6-dev libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev     # 13.3M, 2m13
-# sudo python3 -m pip install pygame      # 9m18
-
-# sudo pip3 install requests  pillow      # 6m22
-
-
-# # This may be all that's needed:
-sudo apt install python3-pygame python3-tk
+sudo apt install python3-pygame python3-tk libopenjp2-7
+sudo pip3 install requests  pillow      # 6m22
