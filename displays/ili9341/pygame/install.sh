1cef55b6f54653cc146c61ea8be91ffb777bc0c1
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-10-05 14:24:03 -0400

    Added fb.modes so pygame will work.

--- displays/ili9341/pygame/install.sh
+++ displays/ili9341/pygame/install.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 # From: https://www.pygame.org/wiki/GettingStarted
 
-sudo apt install python3-pygame python3-tk libopenjp2-7
-sudo pip3 install requests  pillow      # 6m22
+# apt install python3-pygame python3-tk libopenjp2-7
+# pip3 install requests  pillow      # 6m22
+
+echo "mode \"320x240\"
+    geometry 320 240 320 240 16
+    timings 0 0 0 0 0 0 0
+    rgba 5/11,6/5,5/0,0/0
+endmode
+" >> /etc/fb.modes
