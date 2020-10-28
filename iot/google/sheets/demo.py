3d218c816a3fd2ef99b06edb98586508d7689cbb
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-10-28 09:45:55 -0400

    Working again

--- iot/google/sheets/demo.py
+++ iot/google/sheets/demo.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# From: https://developers.google.com/sheets/api/quickstart/python
 # Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,47 +15,55 @@
 
 # [START sheets_quickstart]
 from __future__ import print_function
+import pickle
+import os.path
 from googleapiclient.discovery import build
-from httplib2 import Http
-from oauth2client import file, client, tools
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 import time, sys
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
-SPREADSHEET_ID = '1hGMHMLwiG3zEDM19zJehpLjTDbVi8LOjVKhD8R0dBO0'
-RANGE_NAME = 'A2'
+SAMPLE_SPREADSHEET_ID = '1hGMHMLwiG3zEDM19zJehpLjTDbVi8LOjVKhD8R0dBO0'
+SAMPLE_RANGE_NAME = 'A2:E'
 
 def main():
     """Shows basic usage of the Sheets API.
-    Writes values to a sample spreadsheet.
+    Prints values from a sample spreadsheet.
     """
-    store = file.Storage('tokenPython.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('sheets', 'v4', http=creds.authorize(Http()))
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
 
     # Call the Sheets API
-    # Compute a timestamp and pass the first two arguments
+    sheet = service.spreadsheets()
     values = [ [time.time()/60/60/24+ 25569 - 4/24, sys.argv[1], sys.argv[2]]]
-    body = { 'values': values }
-    result = service.spreadsheets().values().append(spreadsheetId=SPREADSHEET_ID,
-                            range=RANGE_NAME,
-                            #  How the input data should be interpreted.
-                            valueInputOption='USER_ENTERED',
-                            # How the input data should be inserted.
-                            # insertDataOption='INSERT_ROWS'
-                            body=body
-                            ).execute()
-    
-    updates = result.get('updates', [])
-    # print(updates)
-
-    if not updates:
-        print('Not updated')
+    body = {'values': values}
+    result = sheet.values().append(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME,
+                                valueInputOption='USER_ENTERED', 
+                                body=body
+                                ).execute()
+    print(result)
 
 if __name__ == '__main__':
     main()
