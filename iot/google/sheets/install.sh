09be50e3caf7b73de9cd3223d032a0c66bf9d9fc
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-11-02 10:02:10 -0500

    Commented out JavaScript install

--- iot/google/sheets/install.sh
+++ iot/google/sheets/install.sh
@@ -1,6 +1,7 @@
 # npm install googleapis i2c-bus
-npm install googleapis@39 --save
+# npm install googleapis@39 --save
 
 pip3 install --upgrade google-api-python-client oauth2client
 
 pip3 install -r requirements.txt
+
