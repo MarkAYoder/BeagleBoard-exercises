69b9553f654873210ffcf8cc353bdc1062aa8080
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-11-04 15:20:05 -0500

    Added sudo's

--- displays/st7735/install.sh
+++ displays/st7735/install.sh
@@ -1,15 +1,13 @@
-export FRAMEBUFFER=/dev/fb0
-
-apt install fbi
+sudo apt install fbi
  
 wget https://kernel.org/theme/images/logos/tux.png
 wget http://www.rose-hulman.edu/InstituteBrandResources/RH_Graphic_Secondary.zip
 
-fbi -noverbose -T 1 -a tux.png
-fbi -t 5 -blend 1000 -noverbose -T 1 -a Matthias.jpg Malachi.jpg Alan.jpg Louis.jpg
+sudo fbi -noverbose -T 1 -a tux.png
+sudo fbi -t 5 -blend 1000 -noverbose -T 1 -a Matthias.jpg Malachi.jpg Alan.jpg Louis.jpg
 # This worked
 
-apt install mplayer
+sudo apt install mplayer
  
 wget http://hubblesource.stsci.edu/sources/video/clips/details/images/hst_1.mpg
 wget http://www.koeniglich.de/pics/RedsNightmare.mpg
@@ -18,6 +16,6 @@ SDL_VIDEODRIVER=fbcon SDL_FBDEV=/dev/fb0 mplayer --vf-add=rotate=2 -vo sdl -fram
 
 # If you get an error about the size being wrong install the following.
 # From: https://github.com/notro/fbtft/wiki/Framebuffer-use
-apt install fbset
+sudo apt install fbset
 fbset
 # Take the output of fbset and append it to /etc/fd.modes
