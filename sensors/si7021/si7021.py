aac3587cbe178fe00188cb4f5a5bccf468303a41
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-12-31 10:23:10 -0500

    Put temp and humid on same line

--- sensors/si7021/si7021.py
+++ sensors/si7021/si7021.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 # Prints the temperature and humidity for s si7021 using the iio interface
+# Be sure to run setup.sh first
+
 BUS="i2c-1"
 ADDR="0x40"
 PATH="/sys/class/i2c-adapter/" + BUS + "/1-0040/iio:device1/"
 
-print(PATH)
+# print(PATH)
 fd = open(PATH + "in_temp_raw")
-print(str(float(fd.read().replace('\n', ''))/100) + "°")
+temp = str(float(fd.read().replace('\n', ''))/100)
 fd.close()
 
 fd = open(PATH + "in_humidityrelative_raw")
-print(str(float(fd.read().replace('\n', ''))/100) + "%")
+humid = str(float(fd.read().replace('\n', ''))/100)
 fd.close()
+
+print(temp + "°, " + humid + "%")
