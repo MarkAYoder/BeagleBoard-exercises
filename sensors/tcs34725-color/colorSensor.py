163221238fd8b94edb8ea6fc2324c5c79753cc5d
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2022-09-08 15:07:15 -0400

    Renamed PATH to IIOPATH

--- sensors/tcs34725-color/colorSensor.py
+++ sensors/tcs34725-color/colorSensor.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python
 import os
 import time
-ms = 250    # Read time in ms
+ms = 200    # Read time in ms
 
-PATH = '/sys/bus/i2c/devices/i2c-2/2-0029/iio:device1'
+IIOPATH = '/sys/bus/i2c/devices/i2c-2/2-0029/iio:device1'
 PWMPATH = '/dev/bone/pwm'
 
-if (not os.path.exists(PATH)):
-    print (PATH+': Not found')
+if (not os.path.exists(IIOPATH)):
+    print (IIOPATH+': Not found. Run setup.sh')
     os.exit()
 
-print("Hi")
-
+print("red  green  blue")
 data = [0,0,0]
 
 pwmr = open(PWMPATH+"/1/a/duty_cycle", "w")
 pwmg = open(PWMPATH+"/0/b/duty_cycle", "w")
 pwmb = open(PWMPATH+"/0/a/duty_cycle", "w")
 
-fr = open(PATH+"/in_intensity_red_raw",   "r")
-fg = open(PATH+"/in_intensity_green_raw", "r")
-fb = open(PATH+"/in_intensity_blue_raw",  "r")
+fr = open(IIOPATH+"/in_intensity_red_raw",   "r")
+fg = open(IIOPATH+"/in_intensity_green_raw", "r")
+fb = open(IIOPATH+"/in_intensity_blue_raw",  "r")
 while True:
     fr.seek(0)
     fg.seek(0)
@@ -28,7 +27,7 @@ while True:
     data[0] = fr.read()[:-1]
     data[1] = fg.read()[:-1]
     data[2] = fb.read()[:-1]
-    print(" ".join(data))
+    print("  ".join(data)+"       ", end='\r')
 
     pwmr.seek(0)
     pwmr.write(data[0])
