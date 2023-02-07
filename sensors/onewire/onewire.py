698c361191e799fb90c0c72da0efeadd098f7fae
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2023-02-07 13:06:50 -0500

    Updated 1-wire to use /sys/class/hwmon

--- sensors/onewire/onewire.py
+++ sensors/onewire/onewire.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import time
 
-w1="/sys/bus/w1/devices/28-00000114ef1b/w1_slave"
+w1="/sys/class/hwmon/hwmon0/device/temperature"
 
 while True:
     raw = open(w1, "r").read()
-    print "Temperature is "+str(float(raw.split("t=")[-1])/1000)+" degrees"
+    print("Temperature is "+str(float(raw)/1000)+" degrees")
     time.sleep(1)
