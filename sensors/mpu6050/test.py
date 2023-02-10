2f33615a987f1304368e35d2eb0cf8d147cef2fb
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2023-02-10 14:35:35 -0500

    Added ^C handler

--- sensors/mpu6050/test.py
+++ sensors/mpu6050/test.py
@@ -5,8 +5,12 @@ mpu="/sys/class/i2c-adapter/i2c-2/2-0068/iio:device1/"
 
 rawX = open(mpu+"in_accel_x_raw", "r")
 
-while True:
-    rawX.seek(0)
-    print("accs_x_raw "+str(float(rawX.read())/1000)+"      ", end='\r')
-    time.sleep(1)
+try:
+    while True:
+        rawX.seek(0)
+        print("accs_x_raw "+str(float(rawX.read())/1000)+"      ", end='\r')
+        time.sleep(1)
 
+except KeyboardInterrupt:
+    print("\nQuitting")
+    rawX.close()
