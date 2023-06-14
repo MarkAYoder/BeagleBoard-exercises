a63ff640e5abd8ce6521f626ee32ac86bfef9448
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2023-06-14 10:53:28 -0400

    Converted to gpiod

--- iot/google/sheets/flashUp.py
+++ iot/google/sheets/flashUp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Pings google and turns on a RGB light according to the response time.
 
-import Adafruit_BBIO.GPIO as GPIO
+import gpiod
 import subprocess
 import time
 import re
@@ -13,24 +13,23 @@ ms = 15000          # Repeat time in ms.
 thresh = 40.0      # If time is above this, turn on warning light
 hist = 10* [thresh]  # Keep track of the last 10 values
 current = 0        # Insert next value here 
-red   = 'P9_11'
-green = 'P9_17'
-blue  = 'P9_13'
+
+LED_CHIP = 'gpiochip0'
+red   = 30 # 'P9_11'
+green =  5 # 'P9_17'
+blue  = 31 # 'P9_13'
+LED_LINE_OFFSET = [red, green, blue]
+
+chip = gpiod.Chip(LED_CHIP)
+lines = chip.get_lines(LED_LINE_OFFSET)
+lines.request(consumer='flashup.py', type=gpiod.LINE_REQ_DIR_OUT)
 
 # Turn all LEDs off
 def allOff():
     print("allOff")
-    GPIO.output(red,   0)
-    GPIO.output(green, 0)
-    GPIO.output(blue,  0)
+    lines.set_values([0, 0, 0])
 
-GPIO.setup(red,  GPIO.OUT)
-GPIO.setup(green,GPIO.OUT)
-GPIO.setup(blue, GPIO.OUT)
-
-GPIO.output(red,   1)
-GPIO.output(green, 0)
-GPIO.output(blue,  1)
+lines.set_values([1, 0, 1])
 
 while True:
     hour=dt.datetime.now().hour
@@ -51,19 +50,15 @@ while True:
                 current=0
             print('ping: time = %5.2f, average = %5.2f}' % (timems, average))
             if timems > 1.1*average:
-                GPIO.output(red,   1)
-                GPIO.output(green, 1)
-                GPIO.output(blue,  0)
+                lines.set_values([1, 1, 0])
             else:
-                GPIO.output(red,   0)
-                GPIO.output(green, 1)
-                GPIO.output(blue,  0)
+                lines.set_values([0, 1, 0])
+
                 
         except subprocess.CalledProcessError as err:
             print('ERROR:', err)
-            GPIO.output(red,   1)
-            GPIO.output(green, 0)
-            GPIO.output(blue,  0)
+            lines.set_values([1, 0, 0])
+
             
     else:
         print('Good night')
