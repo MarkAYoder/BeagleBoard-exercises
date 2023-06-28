1e0630217eef37842ed7ed269b3f3b65341c524b
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2023-06-28 18:05:23 +0000

    Switched tmp114 to stts22h

--- play/temperature/stts22h.c
+++ play/temperature/stts22h.c
@@ -2,9 +2,9 @@
 /*
  * Digital temperature sensor
  * Copyright (c) 2021 Puranjay Mohan <puranjay12@gmail.com>
- * Converted from TMP117 to TMP114 by Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
+ * Converted from TMP117 to STTS22H by Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
  *
- * Driver for the Texas Instruments TMP114 Temperature Sensor
+ * Driver for the Texas Instruments STTS22H Temperature Sensor
  * (7-bit I2C slave address (0x4B))
  */
 
@@ -19,37 +19,37 @@
 
 #include <linux/iio/iio.h>
 
-#define TMP114_REG_DEVICE_ID	0x1
-#define TMP114_REG_HIGH_LIM		0x2
-#define TMP114_REG_LOW_LIM		0x3
-#define TMP114_REG_CFGR			0x4
-#define TMP114_REG_STATUS		0x5
-#define TMP114_REG_TEMP_L		0x6
-#define TMP114_REG_TEMP_H		0x7
+#define STTS22H_REG_DEVICE_ID	0x1
+#define STTS22H_REG_HIGH_LIM		0x2
+#define STTS22H_REG_LOW_LIM		0x3
+#define STTS22H_REG_CFGR			0x4
+#define STTS22H_REG_STATUS		0x5
+#define STTS22H_REG_TEMP_L		0x6
+#define STTS22H_REG_TEMP_H		0x7
 
-#define TMP114_RESOLUTION_10UC	78125
+#define STTS22H_RESOLUTION_10UC	78125
 #define MICRODEGREE_PER_10MILLIDEGREE	10000
 
-#define TMP114_DEVICE_ID		0xA0
+#define STTS22H_DEVICE_ID		0xA0
 
-struct tmp114_data {
+struct stts22h_data {
 	struct i2c_client *client;
 };
 
-static int tmp114_read_raw(struct iio_dev *indio_dev,
+static int stts22h_read_raw(struct iio_dev *indio_dev,
 			   struct iio_chan_spec const *channel, int *val,
 			   int *val2, long mask)
 {
-	struct tmp114_data *data = iio_priv(indio_dev);
+	struct stts22h_data *data = iio_priv(indio_dev);
 	s32 hi;		// Upper byte of temp
 	s32 lo;
 
 	switch (mask) {
 	case IIO_CHAN_INFO_PROCESSED:
-		hi = i2c_smbus_read_byte_data(data->client, TMP114_REG_TEMP_H);
+		hi = i2c_smbus_read_byte_data(data->client, STTS22H_REG_TEMP_H);
 		if (hi < 0)
 			return hi;
-		lo = i2c_smbus_read_byte_data(data->client, TMP114_REG_TEMP_L);
+		lo = i2c_smbus_read_byte_data(data->client, STTS22H_REG_TEMP_L);
 		if (lo < 0)
 			return lo;
 
@@ -61,8 +61,8 @@ static int tmp114_read_raw(struct iio_dev *indio_dev,
 		 * Conversion from 10s of uC to mC
 		 * as IIO reports temperature in mC
 		 */
-		*val = TMP114_RESOLUTION_10UC / MICRODEGREE_PER_10MILLIDEGREE;
-		*val2 = (TMP114_RESOLUTION_10UC %
+		*val = STTS22H_RESOLUTION_10UC / MICRODEGREE_PER_10MILLIDEGREE;
+		*val2 = (STTS22H_RESOLUTION_10UC %
 					MICRODEGREE_PER_10MILLIDEGREE) * 100;
 
 		return IIO_VAL_INT_PLUS_MICRO;
@@ -72,7 +72,7 @@ static int tmp114_read_raw(struct iio_dev *indio_dev,
 	}
 }
 
-static int tmp114_write_raw(struct iio_dev *indio_dev, struct iio_chan_spec
+static int stts22h_write_raw(struct iio_dev *indio_dev, struct iio_chan_spec
 			    const *channel, int val, int val2, long mask)
 {
 	switch (mask) {
@@ -81,7 +81,7 @@ static int tmp114_write_raw(struct iio_dev *indio_dev, struct iio_chan_spec
 	}
 }
 
-static const struct iio_chan_spec tmp114_channels[] = {
+static const struct iio_chan_spec stts22h_channels[] = {
 	{
 		.type = IIO_TEMP,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_PROCESSED) |
@@ -89,25 +89,25 @@ static const struct iio_chan_spec tmp114_channels[] = {
 	},
 };
 
-static const struct iio_info tmp114_info = {
-	.read_raw = tmp114_read_raw,
-	.write_raw = tmp114_write_raw,
+static const struct iio_info stts22h_info = {
+	.read_raw = stts22h_read_raw,
+	.write_raw = stts22h_write_raw,
 };
 
-static int tmp114_identify(struct i2c_client *client)
+static int stts22h_identify(struct i2c_client *client)
 {
 	// const struct i2c_device_id *id;
 	unsigned long match_data;
 	int dev_id;
 
-	dev_id = i2c_smbus_read_byte_data(client, TMP114_REG_DEVICE_ID);
+	dev_id = i2c_smbus_read_byte_data(client, STTS22H_REG_DEVICE_ID);
 	if (dev_id < 0)
 		return dev_id;
 
-	dev_info(&client->dev, "tmp114_identify id (0x%x)\n", dev_id);
+	dev_info(&client->dev, "stts22h_identify id (0x%x)\n", dev_id);
 
 	switch (dev_id) {
-	case TMP114_DEVICE_ID:
+	case STTS22H_DEVICE_ID:
 		return dev_id;
 	}
 
@@ -127,22 +127,22 @@ static int tmp114_identify(struct i2c_client *client)
 	return -ENODEV;
 }
 
-static int tmp114_probe(struct i2c_client *client)
+static int stts22h_probe(struct i2c_client *client)
 {
-	struct tmp114_data *data;
+	struct stts22h_data *data;
 	struct iio_dev *indio_dev;
 	int ret, dev_id;
 	
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_WORD_DATA))
 		return -EOPNOTSUPP;
 
-	ret = tmp114_identify(client);
+	ret = stts22h_identify(client);
 	if (ret < 0)
 		return ret;
 
 	dev_id = ret;
 
-	dev_info(&client->dev, "tmp114_probe id (0x%x)\n", dev_id);
+	dev_info(&client->dev, "stts22h_probe id (0x%x)\n", dev_id);
 
 	indio_dev = devm_iio_device_alloc(&client->dev, sizeof(*data));
 	if (!indio_dev)
@@ -152,12 +152,12 @@ static int tmp114_probe(struct i2c_client *client)
 	data->client = client;
 
 	indio_dev->modes = INDIO_DIRECT_MODE;
-	indio_dev->info = &tmp114_info;
+	indio_dev->info = &stts22h_info;
 
 	switch (dev_id) {
-	case TMP114_DEVICE_ID:
-		indio_dev->channels = tmp114_channels;
-		indio_dev->num_channels = ARRAY_SIZE(tmp114_channels);
+	case STTS22H_DEVICE_ID:
+		indio_dev->channels = stts22h_channels;
+		indio_dev->num_channels = ARRAY_SIZE(stts22h_channels);
 		indio_dev->name = "stts22h";
 		break;
 	}
@@ -165,27 +165,27 @@ static int tmp114_probe(struct i2c_client *client)
 	return devm_iio_device_register(&client->dev, indio_dev);
 }
 
-static const struct of_device_id tmp114_of_match[] = {
-	{ .compatible = "st,stts22h", .data = (void *)TMP114_DEVICE_ID },
+static const struct of_device_id stts22h_of_match[] = {
+	{ .compatible = "st,stts22h", .data = (void *)STTS22H_DEVICE_ID },
 	{ }
 };
-MODULE_DEVICE_TABLE(of, tmp114_of_match);
+MODULE_DEVICE_TABLE(of, stts22h_of_match);
 
-static const struct i2c_device_id tmp114_id[] = {
-	{ "stts22h", TMP114_DEVICE_ID },
+static const struct i2c_device_id stts22h_id[] = {
+	{ "stts22h", STTS22H_DEVICE_ID },
 	{ }
 };
-MODULE_DEVICE_TABLE(i2c, tmp114_id);
+MODULE_DEVICE_TABLE(i2c, stts22h_id);
 
-static struct i2c_driver tmp114_driver = {
+static struct i2c_driver stts22h_driver = {
 	.driver = {
 		.name	= "stts22h",
-		.of_match_table = tmp114_of_match,
+		.of_match_table = stts22h_of_match,
 	},
-	.probe_new	= tmp114_probe,
-	.id_table	= tmp114_id,
+	.probe_new	= stts22h_probe,
+	.id_table	= stts22h_id,
 };
-module_i2c_driver(tmp114_driver);
+module_i2c_driver(stts22h_driver);
 
 MODULE_AUTHOR("Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>");
 MODULE_DESCRIPTION("ST stts22h Temperature sensor driver");
