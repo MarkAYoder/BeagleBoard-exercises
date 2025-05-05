cb847c9d808dc95ddbadca9662291076e92b72b5
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2025-05-05 15:50:32 -0400

    Turns on blue LED when less than 1000 miles away.

--- iot/nasa/blue.py
+++ iot/nasa/blue.py
@@ -7,21 +7,7 @@ import urllib.request
 import time
 from haversine import haversine, Unit
 
-# Get the current location of the ISS
-# The API returns the current location of the ISS in JSON format
 url = 'http://api.open-notify.org/iss-now.json'
-# response = urllib.request.urlopen(url)
-# result = json.loads(response.read())
-# # print(result)
-
-# location = result['iss_position']
-# iss = (float(location['latitude']), float(location['longitude']))
-# print('Current Location : ', iss)
-
-# # Get the distance from the ISS to Brazil Indiana
-# brazil = (39.525000, -87.127500)
-# distance = haversine(iss, brazil, unit=Unit.MILES)
-# print('Distance from ISS to Brazil : ', int(distance), 'miles')
 
 LED_CHIP = 'gpiochip1'
 blue   = 18 # 'P9_19'
@@ -32,13 +18,35 @@ chip = gpiod.Chip(LED_CHIP)
 lines = chip.get_lines(LED_LINE_OFFSET)
 lines.request(consumer='blue.py', type=gpiod.LINE_REQ_DIR_OUT)
 
-# Turn all LEDs off
-def allOff():
-    print("allOff")
-    lines.set_values([0])  # All off
-
 # Turn on blue LED
 print("blue on")
-lines.set_values([1])     # blue
-time.sleep(5)
+lines.set_values([1])     # blue LED on
 
+while True:
+    try:
+        # Get the current location of the ISS
+        response = urllib.request.urlopen(url)
+        result = json.loads(response.read())
+        # print(result)
+
+        location = result['iss_position']
+        iss = (float(location['latitude']), float(location['longitude']))
+        print('Current Location : ', iss)
+
+        # Get the distance from the ISS to Brazil Indiana
+        brazil = (39.525000, -87.127500)
+        # haversine returns distance in miles between two points
+        distance = haversine(iss, brazil, unit=Unit.MILES)
+        print('Distance from ISS to Brazil : ', int(distance), 'miles')
+
+        if distance < 1000:
+            print("Distance < 1000 miles")
+            lines.set_values([1])
+        else:
+            print("Distance > 1000 miles")
+            lines.set_values([0])
+
+        time.sleep(60)  # Wait for 60 seconds before checking again
+
+    except Exception as err:
+        print('ERROR:', err)
